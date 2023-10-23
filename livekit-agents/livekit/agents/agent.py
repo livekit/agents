6152e6bc7cb8ddd9caf0f7ec0a72f38725c52341
Neil Dwyer <neildwyer1991@gmail.com>
2023-10-23 14:41:54 -0700

    make agents work with decorator pattern instead of the decorator pattern

--- livekit-agents/livekit/agents/agent.py
+++ livekit-agents/livekit/agents/agent.py
@@ -12,20 +12,38 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import livekit
+import logging
+from dataclasses import dataclass
 from typing import Callable, Awaitable
+import livekit.rtc as rtc
 
-Should_Process_CB = Callable[[livekit.TrackPublication, livekit.Participant], Awaitable[bool]]
+Event_Fn = Callable[..., Awaitable[None]]
 
 class Agent:
-    """This Agent class is a convencience class on top of a LocalParticipant and Room that handles boilerplate
+    """This Agent class is a convenience class on top of a LocalParticipant and Room that handles boilerplate
        code for subscribing to tracks and handling participants joining and leaving the room. You can use this
        class without compromise because the LocalParticipant and Room are exposed as public attributes: self.participant
        and self.room.
 
        See /examples for usage examples.
     """
-    def __init__(self, *_, participant: livekit.LocalParticipant, room: livekit.Room):
+
+    @dataclass
+    class OnVideoTrackEvent:
+        track: rtc.VideoTrack
+        participant: rtc.RemoteParticipant
+
+    @dataclass
+    class OnAudioTrackEvent:
+        track: rtc.AudioTrack
+        participant: rtc.RemoteParticipant
+
+    @dataclass
+    class OnTrackAvailableEvent:
+        publication: rtc.TrackPublication
+        participant: rtc.RemoteParticipant
+
+    def __init__(self, *_, participant: rtc.LocalParticipant, room: rtc.Room):
         self.participant = participant
         self.room = room
         self.room.on("participant_connected", self._on_participant_connected_or_disconnected)
@@ -38,9 +56,15 @@ class Agent:
         self.should_process_cb = None
 
         self._handle_existing_tracks()
-        self.audio_streams = []
-        self.video_streams = []
         self.stream_tasks = set()
+
+        # handlers
+        self._on_audio_track_handler: Callable[[Agent.OnAudioTrackEvent], None] = \
+            lambda: logging.warning("a handler was not provided for 'on_audio_track'")
+        self._on_video_track_handler: Callable[[Agent.OnVideoTrackEvent], None] = \
+            lambda: logging.warning("a handler was not provided for 'on_video_track'")
+        self._on_track_available_handler: Callable[[Agent.OnTrackAvailableEvent], bool] = \
+            lambda: logging.warning("a handler was not provided for 'on_track_availale'")
 
     async def cleanup(self):
         await self.room.disconnect()
@@ -51,39 +75,25 @@ class Agent:
                 publication = self.participants[participantKey].tracks[publicationKey]
                 self._on_track_published(publication, self.participants[participantKey])
 
-    def on_video_track(self, track: livekit.RemoteVideoTrack, participant: livekit.Participant):
-        pass
+    def on_video_track(self, handler: Callable[[OnVideoTrackEvent], None]):
+        self._on_video_track_handler = handler
 
-    def on_audio_track(self, track: livekit.RemoteAudioTrack, participant: livekit.Participant):
-        pass
+    def on_audio_track(self, handler: Callable[[OnAudioTrackEvent], None]):
+        self._on_audio_track_handler = handler
 
-    def should_process(self, track: livekit.TrackPublication, participant: livekit.Participant) -> bool:
-        pass
+    def on_track_available(self, handler: Callable[[OnTrackAvailableEvent], bool]):
+        self._on_track_available_handler = handler
 
-    def on_participants_changed(self, participants: [livekit.Participant]):
-        pass
-
-    def _on_participant_connected_or_disconnected(self, *args):
-        self.participants = self.room.participants
-        self.on_participants_changed(self.participants)
-
-    def _on_track_published(self, publication: livekit.RemoteTrackPublication, participant: livekit.Participant):
+    def _on_track_published(self, publication: rtc.RemoteTrackPublication, participant: rtc.Participant):
         # Don't do anything for our own tracks
         if participant.sid == self.participant.sid:
             return
 
-        if self.should_process(publication, participant):
+        if self._on_track_available_handler(publication, participant):
             publication.set_subscribed(True)
 
-    def _on_track_subscribed(self, track: livekit.Track, publication: livekit.RemoteTrackPublication, participant: livekit.RemoteParticipant):
+    def _on_track_subscribed(self, track: rtc.Track, publication: rtc.RemoteTrackPublication, participant: rtc.RemoteParticipant):
         if publication.kind == 1:
-            self.on_audio_track(track, participant)
+            self._on_audio_track_handler(Agent.OnAudioTrackEvent(track, participant))
         elif publication.kind == 2:
-            self.on_video_track(track, participant)
-
-    def _on_track_unsubscribed(self, track: livekit.Track, publication: livekit.RemoteTrackPublication, participant: livekit.RemoteParticipant):
-        self.audio_streams = [stream for stream in self.audio_streams if stream.track.sid != publication.track_sid]
-        self.video_streams = [stream for stream in self.video_streams if stream.track.sid != publication.track_sid]
-
-    def _on_data_received(self, data: bytearray, kind, participant: livekit.RemoteParticipant):
-        print(data, self.participant.identity)
+            self._on_video_track_handler(Agent.OnVideoTrackEvent(track, participant))
