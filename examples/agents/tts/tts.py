bba5f0297cc9cde9e26752ba924734ee7c0d6db3
Neil Dwyer <neildwyer1991@gmail.com>
2023-11-06 18:32:08 -0800

    TTS agent

--- examples/agents/tts/tts.py
+++ examples/agents/tts/tts.py
@@ -4,7 +4,6 @@ from livekit import rtc
 from livekit.plugins.vad import VAD, VADPlugin
 from livekit.plugins.elevenlabs import ElevenLabsTTSPlugin
 from livekit import agents
-from typing import AsyncIterator
 
 SAMPLE_RATE = 48000
 NUM_CHANNELS = 1
@@ -18,9 +17,6 @@ async def tts_agent(ctx: agents.JobContext):
     options.source = rtc.TrackSource.SOURCE_MICROPHONE
     await ctx.room.local_participant.publish_track(track, options)
 
-    text_queue = asyncio.Queue()
-    text_queue_iterator = agents.utils.AsyncQueueIterator(text_queue)
-
     @ctx.room.on("data_received")
     def on_data_received(data: bytes, participant: rtc.RemoteParticipant, kind):
         payload = json.loads(data.decode('utf-8'))
@@ -28,9 +24,13 @@ async def tts_agent(ctx: agents.JobContext):
             return
 
         text = payload["text"]
-        text_queue.put_nowait(text)
+        iterator = tts.process(agents.utils.AsyncIteratorList([text]))
 
-    async for frame_iter in tts.start(text_queue_iterator).unwrap():
-        async for frame in frame_iter:
-            resampled = frame.remix_and_resample(SAMPLE_RATE, NUM_CHANNELS)
-            await source.capture_frame(resampled)
+        async def speak():
+            async for frame_iter in iterator:
+                async for frame in frame_iter:
+                    resampled = frame.remix_and_resample(
+                        SAMPLE_RATE, NUM_CHANNELS)
+                    await source.capture_frame(resampled)
+
+        asyncio.create_task(speak())
