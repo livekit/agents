752713a080901e2c745671d6f38657ea033b9b99
Neil Dwyer <neildwyer1991@gmail.com>
2023-11-14 12:36:14 -0800

    Dockerfile for running agents (#35)
    * Dockerfile for running agents
    
    * Audioread in the plugin

--- livekit-plugins/livekit-plugins-openai/setup.py
+++ livekit-plugins/livekit-plugins-openai/setup.py
@@ -54,11 +54,11 @@ setuptools.setup(
     install_requires=[
         "livekit >= 0.5.0",
         "livekit-plugins-core",
-        "torch >= 2.0.0",
+        "torch >= 2.1.0",
         "numpy >= 1.24.0",
         "openai >= 0.28.0",
         "openai-whisper",
-        "audioread",
+        "audioread >= 3.0.0",
     ],
     package_data={},
     project_urls={
