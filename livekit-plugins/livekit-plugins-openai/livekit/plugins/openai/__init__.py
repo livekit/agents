08e03afbbb517f781a5b4a5b191e8ffd689a2926
Neil Dwyer <neildwyer1991@gmail.com>
2023-11-23 12:41:57 -0800

    fix

--- livekit-plugins/livekit-plugins-openai/livekit/plugins/openai/__init__.py
+++ livekit-plugins/livekit-plugins-openai/livekit/plugins/openai/__init__.py
@@ -11,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 from .whisper_api_transcriber import WhisperAPITranscriber
 from .tts import TTSPlugin
 from .chatgpt import ChatGPTPlugin, ChatGPTMessage, ChatGPTMessageRole
