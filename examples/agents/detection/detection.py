da3e657e94c1687b64ab956be3ccec522c818418
Neil Dwyer <neildwyer1991@gmail.com>
2024-01-04 15:38:12 -0800

    Remove unused

--- examples/agents/detection/detection.py
+++ examples/agents/detection/detection.py
@@ -12,11 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import asyncio
 import json
 import logging
 import time
-from enum import Enum
 
 from livekit import agents, rtc
 from livekit.plugins.directai import Detector
