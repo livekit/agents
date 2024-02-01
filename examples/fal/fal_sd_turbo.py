92d41542cd04a67fd67caf136837121604e0ea5b
Neil Dwyer <neildwyer1991@gmail.com>
2024-02-01 10:22:14 -0800

    Fal AI Plugin and Demo (#136)
    * Fal AI Plugin and Demo
    
    * iterating on fal plugin
    
    * iterating
    
    * iterating
    
    * Fal example
    
    * Move fal code into example + add incorrect response
    
    * Updates to fal
    
    * fix aclose impl
    
    * cr

---
+++ examples/fal/fal_sd_turbo.py
@@ -0,0 +1,215 @@
+# Copyright 2024 LiveKit, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import asyncio
+import io
+import logging
+import os
+import time
+from dataclasses import dataclass
+from typing import Optional, Set
+
+import aiohttp
+import msgpack
+from livekit import rtc
+from PIL import Image
+
+FAL_URL = "wss://fal.run/fal-ai/sd-turbo-real-time-high-fps-msgpack-a10g/ws"
+
+
+class FalSDTurbo:
+    """Fal SDXL Plugin
+
+    Requires FAL_KEY_ID and FAL_KEY_SECRET environment variables to be set.
+    """
+
+    def __init__(
+        self,
+        *,
+        key_id: Optional[str] = None,
+        key_secret: Optional[str] = None,
+    ):
+        self._key_id = key_id or os.environ.get("FAL_KEY_ID")
+        self._key_secret = key_secret or os.environ.get("FAL_KEY_SECRET")
+
+    def stream(self) -> "SDTurboHighFPSStream":
+        return SDTurboHighFPSStream(key_id=self._key_id, key_secret=self._key_secret)
+
+
+def _task_done_cb(task: asyncio.Task, set: Set[asyncio.Task]) -> None:
+    set.discard(task)
+    if task.cancelled():
+        logging.info("task cancelled: %s", task)
+        return
+
+    if task.exception():
+        logging.error("task exception: %s", task, exc_info=task.exception())
+        return
+
+
+class SDTurboHighFPSStream:
+    @dataclass
+    class Input:
+        data: bytes
+        prompt: str
+        strength: float
+        width: int
+        height: int
+
+    def __init__(self, key_id: str, key_secret: str):
+        self._key_id = key_id
+        self._key_secret = key_secret
+        self._session = aiohttp.ClientSession()
+        self._input_queue = asyncio.Queue[self.Input]()
+        self._output_queue = asyncio.Queue[rtc.VideoFrame]()
+        self._in_flight_requests = 0
+        self._tasks = set()
+        self._closed = False
+        self._latency = 0
+        self._send_time_queue = asyncio.Queue[float]()
+        self._run_task = asyncio.create_task(self._run())
+        self._run_task.add_done_callback(lambda t: _task_done_cb(t, self._tasks))
+
+    @property
+    def latency(self) -> float:
+        return self._latency
+
+    def push_frame(self, frame: rtc.VideoFrame, prompt: str, strength: float) -> None:
+        if self._closed:
+            raise ValueError("cannot push to a closed stream")
+
+        argb_frame = rtc.ArgbFrame.create(
+            format=rtc.VideoFormatType.FORMAT_RGBA,
+            width=frame.buffer.width,
+            height=frame.buffer.height,
+        )
+        frame.buffer.to_argb(dst=argb_frame)
+        self._input_queue.put_nowait(
+            self.Input(
+                data=argb_frame.data,
+                prompt=prompt,
+                strength=strength,
+                width=frame.buffer.width,
+                height=frame.buffer.height,
+            )
+        )
+
+    async def aclose(self) -> None:
+        self._run_task.cancel()
+        await self._run_task
+        self._closed = True
+
+    async def _run(self):
+        retry_count = 0
+        max_retry = 32
+        while True:
+            try:
+                ws = await self._get_connected_ws()
+                retry_count = 0
+                receive_task = asyncio.create_task(self._ws_receiver(ws))
+                receive_task.add_done_callback(lambda t: _task_done_cb(t, self._tasks))
+                in_flight_requests = 0
+                while not ws.closed:
+                    input = await self._input_queue.get()
+                    image_rgba = Image.frombytes(
+                        "RGBA", (input.width, input.height), input.data
+                    )
+                    image_rgb = image_rgba.convert("RGB")
+                    # center crop to 512x512
+                    crop_x = (image_rgb.width - 512) // 2
+                    crop_y = (image_rgb.height - 512) // 2
+                    image_rgb = image_rgb.crop(
+                        (crop_x, crop_y, crop_x + 512, crop_y + 512)
+                    )
+                    jpg_img = io.BytesIO()
+                    image_rgb.save(jpg_img, format="JPEG")
+                    payload = {
+                        "prompt": input.prompt,
+                        "num_inference_steps": 3,
+                        "strength": input.strength,
+                        "image": jpg_img.getvalue(),
+                    }
+                    packed = msgpack.packb(payload)
+                    if in_flight_requests > 10:
+                        logging.warning(
+                            f"Sending video faster than it is being processed, skipping frame. In-flight requests: {in_flight_requests}"
+                        )
+                        continue
+                    self._in_flight_requests += 1
+                    self._send_time_queue.put_nowait(time.time())
+                    await ws.send_bytes(packed)
+            except asyncio.CancelledError:
+                if ws:
+                    await ws.close()
+                    if receive_task:
+                        await asyncio.shield(receive_task)
+                break
+            except Exception as e:
+                if retry_count > max_retry and max_retry > 0:
+                    logging.error(f"failed to connect to FalAI: {e} - {retry_count}")
+                    break
+
+                logging.warning(f"failed to connect to FalAI: {e} - retrying")
+                await asyncio.sleep(1)
+
+    async def _ws_receiver(self, ws):
+        while True:
+            ws_message = await ws.receive()
+            send_time = await self._send_time_queue.get()
+            self._latency = time.time() - send_time
+            if ws_message.type == aiohttp.WSMsgType.TEXT:
+                logging.info(f"Received text message from fal ai: {ws_message.data}")
+                continue
+
+            if ws_message.type == aiohttp.WSMsgType.CLOSED:
+                logging.info("Fal AI websocket connection closed")
+                return
+
+            if ws_message.type == aiohttp.WSMsgType.ERROR:
+                logging.error("Fal AI websocket connection error")
+                return
+
+            if ws_message.type != aiohttp.WSMsgType.BINARY:
+                logging.warning(f"Received unexpected message type: {ws_message}")
+                return
+
+            data = ws_message.data
+            output = msgpack.unpackb(data)
+            byte_stream = io.BytesIO(output["image"])
+            img = Image.open(byte_stream)
+            img_rgba = img.convert("RGBA")
+            (r, g, b, a) = img_rgba.split()
+            img_argb = Image.merge("RGBA", (b, g, r, a))
+            argb_frame = rtc.ArgbFrame.create(
+                rtc.VideoFormatType.FORMAT_ARGB, img.width, img.height
+            )
+            argb_frame.data[:] = img_argb.tobytes()
+            video_frame = rtc.VideoFrame(argb_frame.to_i420())
+            self._in_flight_requests -= 1
+            await self._output_queue.put(video_frame)
+
+    async def _get_connected_ws(self):
+        creds = f"{self._key_id}:{self._key_secret}"
+        return await self._session.ws_connect(
+            FAL_URL, headers={"Authorization": f"Key {creds}"}
+        )
+
+    async def __anext__(self) -> rtc.ArgbFrame:
+        if self._closed and self._output_queue.empty():
+            raise StopAsyncIteration
+
+        return await self._output_queue.get()
+
+    def __aiter__(self) -> "SDTurboHighFPSStream":
+        return self
---
+++ examples/fal/game_state.py
@@ -0,0 +1,149 @@
+# Copyright 2024 LiveKit, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import asyncio
+import os
+import random
+from typing import Callable
+from enum import Enum
+
+import openai
+from livekit import agents
+
+CELEBRITIES = [
+    "Tom Cruise",
+    "Nicholas Cage",
+    "Beyoncé",
+    "Dwayne 'The Rock' Johnson",
+    "Will Smith",
+    "Taylor Swift",
+    "Scarlett Johansson",
+    "Leonardo DiCaprio",
+    "Oprah Winfrey",
+    "Justin Bieber",
+    "Lady Gaga",
+    "Robert Downey Junior",
+    "Adele",
+    "Chris Evans",
+    "Kim Kardashian",
+    "Brad Pitt",
+    "Shakira",
+    "Vin Diesel",
+    "Jennifer Aniston",
+    "Kanye West",
+]
+
+GAME_STATE = Enum("GAME_STATE", "PRE_GAME PLAYING")
+
+PRE_GAME_PROMPT = "You are a game running agent in the pregame phase.\
+                Your job is to listen to the user's input and start the game when the user expresses intent to start playing. \
+                When the user has expressed that their intent to start playing (by using natural language), \
+                your only job is to return the string 'start'. Do not return any other strings. Just return 'start'\
+                when the user has expressed intent to start playing."
+
+PLAYING_PROMPT_TEMPLATE = (
+    "You are an agent that can return two types of responses: A correct guess or an incorrect guess.\
+    If the guess is correct, return the 'correct' string. Don't return anything else if the guess is correct.\
+    If the guess is incorrect, return a short response that conveys that the guess was incorrect and give\
+    a useful hint about what the answer is without revealing the name.\
+    Do not return any other strings. Return 'correct' if the user has guessed the name %s correctly.\
+    The spelling may be off so don't worry too much about that. Be generous."
+)
+
+
+class GameState:
+    def __init__(
+        self,
+        ctx: agents.JobContext,
+        send_message: Callable[[str], None],
+    ):
+        self._ctx = ctx
+        self._client = openai.AsyncOpenAI(api_key=os.environ["OPENAI_API_KEY"])
+        self._game_state = GAME_STATE.PRE_GAME
+        self._send_message = send_message
+        self._input_queue = asyncio.Queue()
+        self._celeb_index = 0
+        self._shuffle_celebrities()
+        self._ctx.create_task(self._run())
+
+    @property
+    def current_celebrity(self):
+        return CELEBRITIES[self._celeb_index]
+
+    @property
+    def game_state(self):
+        return self._game_state
+
+    @game_state.setter
+    def game_state(self, new_state: GAME_STATE):
+        self._flush_input_queue()
+        self._game_state = new_state
+
+    def add_user_input(self, user_input: str):
+        self._input_queue.put_nowait(user_input)
+
+    async def _run(self):
+        while True:
+            user_input = await self._input_queue.get()
+            if self._game_state == GAME_STATE.PRE_GAME:
+                await self._handle_pre_game_input(user_input)
+            elif self._game_state == GAME_STATE.PLAYING:
+                await self._handle_playing_input(user_input)
+
+    async def _handle_pre_game_input(self, user_input: str):
+        res = await self._client.chat.completions.create(
+            model="gpt-3.5-turbo-1106",
+            n=1,
+            stream=False,
+            messages=[
+                {"role": "system", "content": PRE_GAME_PROMPT},
+                {"role": "user", "content": user_input},
+            ],
+        )
+        text = res.choices[0].message.content
+        print("pre game text: ", text)
+        if text == "start":
+            self.game_state = GAME_STATE.PLAYING
+            self._send_message(
+                "Great! Let's start playing! Give the face swap engine some time to warm up."
+            )
+        elif text == "wait":
+            pass
+
+    async def _handle_playing_input(self, user_input: str):
+        res = await self._client.chat.completions.create(
+            model="gpt-3.5-turbo-1106",
+            n=1,
+            stream=False,
+            messages=[
+                {
+                    "role": "system",
+                    "content": PLAYING_PROMPT_TEMPLATE % self.current_celebrity,
+                },
+                {"role": "user", "content": user_input},
+            ],
+        )
+        text = res.choices[0].message.content
+        if text == "correct":
+            self._send_message(f"Correct! It was {self.current_celebrity}!")
+            self._celeb_index = (self._celeb_index + 1) % len(CELEBRITIES)
+        else:
+            self._send_message(text)
+
+    def _shuffle_celebrities(self):
+        random.shuffle(CELEBRITIES)
+
+    def _flush_input_queue(self):
+        while not self._input_queue.empty():
+            self._input_queue.get_nowait()
