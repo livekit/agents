9ef24bd394c7c10bd3e0eea63280b6581213dd7b
TasseDeCafe <44320355+TasseDeCafe@users.noreply.github.com>
2024-02-05 06:20:53 +0700

    Properly close connection with elevenlabs when completing stream

--- livekit-plugins/livekit-plugins-elevenlabs/livekit/plugins/elevenlabs/tts.py
+++ livekit-plugins/livekit-plugins-elevenlabs/livekit/plugins/elevenlabs/tts.py
@@ -242,6 +242,7 @@ class SynthesizeStream(tts.SynthesizeStream):
                             tts.SynthesisEvent(type=tts.SynthesisEventType.FINISHED)
                         )
                         break
+                break
 
             except asyncio.CancelledError:
                 if ws:
