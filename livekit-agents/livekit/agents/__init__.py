243989ff11d41b05f45d2e1736d07825343c4335
Neil Dwyer <neildwyer1991@gmail.com>
2024-02-29 14:55:15 -0800

    fix

--- livekit-agents/livekit/agents/__init__.py
+++ livekit-agents/livekit/agents/__init__.py
@@ -22,7 +22,7 @@ from .worker import (
     run_app,
 )
 from .plugin import Plugin
-from .utils import AudioBuffer, merge_frames, AsyncIterableQueue
+from .utils import AudioBuffer, merge_frames
 from .job_request import AutoSubscribe, AutoDisconnect, JobRequest
 from .job_context import JobContext
 
@@ -45,7 +45,6 @@ __all__ = [
     "JobType",
     "AudioBuffer",
     "merge_frames",
-    "AsyncIterableQueue",
     "stt",
     "vad",
     "tts",
