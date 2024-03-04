bfe6d6bd811e34ede370e327e1c634f794cc682e
Neil Dwyer <neildwyer1991@gmail.com>
2024-03-04 15:32:56 -0800

    misc

--- tests/test_stt.py
+++ tests/test_stt.py
@@ -45,67 +45,80 @@ def read_wav_file(filename: str) -> rtc.AudioFrame:
         )
 
 
-async def test_recognize():
-    # stts = [deepgram.STT(), google.STT(), openai.STT()]
-    stts = [deepgram.STT(), openai.STT()]
+# async def test_recognize():
+#     # stts = [deepgram.STT(), google.STT(), openai.STT()]
+#     stts = [deepgram.STT(), openai.STT()]
+#     inputs = [
+#         (read_wav_file(TEST_AUDIO_FILEPATH), TEST_AUDIO_TRANSCRIPT),
+#         (await read_mp3_file(TEST_AUDIO_FILEPATH_2), TEST_AUDIO_TRANSCRIPT_2),
+#     ]
+
+#     async def recognize(stt: agents.stt.STT, frame: rtc.AudioFrame, expected: str):
+#         event = await stt.recognize(buffer=frame)
+#         text = event.alternatives[0].text
+#         assert SequenceMatcher(None, text, expected).ratio() > 0.9
+#         assert event.type == agents.stt.SpeechEventType.FINAL_TRANSCRIPT
+
+#     async with asyncio.TaskGroup() as group:
+#         for input, expected in inputs:
+#             for stt in stts:
+#                 group.create_task(recognize(stt, input, expected))
+
+
+async def _test_stream(
+    provider: agents.stt.STT, frame: rtc.AudioFrame, expected_text: str
+):
+    # divide data into chunks of 10ms
+    chunk_size = frame.sample_rate // 100
+    frames = []
+    for i in range(0, len(frame.data), chunk_size):
+        data = frame.data[i : i + chunk_size]
+        frames.append(
+            rtc.AudioFrame(
+                data=data.tobytes() + b"\0\0" * (chunk_size - len(data)),
+                num_channels=frame.num_channels,
+                samples_per_channel=chunk_size,
+                sample_rate=frame.sample_rate,
+            )
+        )
+
+    stream = provider.stream()
+
+    async def stream_input():
+        for frame in frames:
+            stream.push_frame(frame)
+            await asyncio.sleep(0.01)  # one frame is 10ms
+
+    async def stream(stt: agents.stt.STT):
+        # STT Should start with a START_OF_SPEECH event
+        start_event = await anext(stream)
+        assert start_event.type == agents.stt.SpeechEventType.START_OF_SPEECH
+
+        async for event in stream:
+            if event.type == agents.stt.SpeechEventType.END_OF_SPEECH:
+                text = event.alternatives[0].text
+                assert SequenceMatcher(None, text, expected_text).ratio() > 0.8
+
+                await stream.aclose()
+                break
+
+    await stream(provider)
+
+
+async def test_stream():
     inputs = [
         (read_wav_file(TEST_AUDIO_FILEPATH), TEST_AUDIO_TRANSCRIPT),
         (await read_mp3_file(TEST_AUDIO_FILEPATH_2), TEST_AUDIO_TRANSCRIPT_2),
     ]
 
-    async def recognize(stt: agents.stt.STT, frame: rtc.AudioFrame, expected: str):
-        event = await stt.recognize(buffer=frame)
-        text = event.alternatives[0].text
-        assert SequenceMatcher(None, text, expected).ratio() > 0.9
-        assert event.type == agents.stt.SpeechEventType.FINAL_TRANSCRIPT
+    silero_vad = silero.VAD()
+    stts = [
+        deepgram.STT(min_silence_duration=1000),
+        # google.STT(),
+        # agents.stt.StreamAdapter(openai.STT(), silero_vad.stream()),
+    ]
 
     async with asyncio.TaskGroup() as group:
-        for input, expected in inputs:
-            for stt in stts:
-                group.create_task(recognize(stt, input, expected))
-
-
-# async def test_stream():
-#     silero_vad = silero.VAD()
-#     stts = [
-#         deepgram.STT(min_silence_duration=1000),
-#         # google.STT(),
-#         agents.stt.StreamAdapter(openai.STT(), silero_vad.stream()),
-#     ]
-#     frame = read_wav_file(TEST_AUDIO_FILEPATH)
-
-#     # divide data into chunks of 10ms
-#     chunk_size = frame.sample_rate // 100
-#     frames = []
-#     for i in range(0, len(frame.data), chunk_size):
-#         data = frame.data[i : i + chunk_size]
-#         frames.append(
-#             rtc.AudioFrame(
-#                 data=data.tobytes() + b"\0\0" * (chunk_size - len(data)),
-#                 num_channels=frame.num_channels,
-#                 samples_per_channel=chunk_size,
-#                 sample_rate=frame.sample_rate,
-#             )
-#         )
-
-#     async def stream(stt: agents.stt.STT):
-#         stream = stt.stream()
-#         for frame in frames:
-#             stream.push_frame(frame)
-#             await asyncio.sleep(0.01)  # one frame is 10ms
-
-#         # STT Should start with a START_OF_SPEECH event
-#         start_event = await anext(stream)
-#         assert start_event.type == agents.stt.SpeechEventType.START_OF_SPEECH
-
-#         async for event in stream:
-#             if event.type == agents.stt.SpeechEventType.END_OF_SPEECH:
-#                 text = event.alternatives[0].text
-#                 assert SequenceMatcher(None, text, TEST_AUDIO_TRANSCRIPT).ratio() > 0.8
-
-#                 await stream.aclose()
-#                 break
-
-#     async with asyncio.TaskGroup() as group:
-#         for stt in stts:
-#             group.create_task(stream(stt))
+        for provider in stts:
+            for input, expected in inputs:
+                group.create_task(_test_stream(provider, input, expected))
