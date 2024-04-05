20ddf1dd6b9bb29f8631fac83597696acdb4ee77
Neil Dwyer <neildwyer1991@gmail.com>
2024-04-05 16:33:39 -0700

    simple color example

---
+++ examples/simple-color/agent.py
@@ -0,0 +1,44 @@
+import asyncio
+import logging
+
+from livekit import rtc
+from livekit.agents import (
+    JobContext,
+    JobRequest,
+    WorkerOptions,
+    cli,
+)
+
+WIDTH = 640
+HEIGHT = 480
+
+# change this color in dev mode and the agent will automatically update
+COLOR = bytes([0, 255, 0, 255])
+
+
+async def entrypoint(job: JobContext):
+    room = job.room
+    source = rtc.VideoSource(WIDTH, HEIGHT)
+    track = rtc.LocalVideoTrack.create_video_track("single-color", source)
+    options = rtc.TrackPublishOptions(source=rtc.TrackSource.SOURCE_CAMERA)
+    publication = await room.local_participant.publish_track(track, options)
+    logging.info("published track", extra={"track_sid": publication.sid})
+
+    async def _draw_color():
+        argb_frame = bytearray(WIDTH * HEIGHT * 4)
+        while True:
+            await asyncio.sleep(0.1)  # 100ms
+
+            argb_frame[:] = COLOR * WIDTH * HEIGHT
+            frame = rtc.VideoFrame(WIDTH, HEIGHT, rtc.VideoBufferType.RGBA, argb_frame)
+            source.capture_frame(frame)
+
+    asyncio.create_task(_draw_color())
+
+
+async def request_fnc(req: JobRequest) -> None:
+    await req.accept(entrypoint)
+
+
+if __name__ == "__main__":
+    cli.run_app(WorkerOptions(request_fnc=request_fnc))
