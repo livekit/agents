ac35692d905939d2da3d7ab3fc2abc7913200e94
David Zhao <dz@livekit.io>
2024-04-05 17:03:51 -0700

    Lock protocol version to 0.3.1 (#216)

--- livekit-agents/setup.py
+++ livekit-agents/setup.py
@@ -52,7 +52,7 @@ setuptools.setup(
         "click~=8.1.0",
         "livekit>=0.9.0",
         "livekit-api>=0.4.1",
-        "livekit-protocol>=0.3.0",
+        "livekit-protocol~=0.3.0",
         "websockets>=12,<13",
     ],
     extras_require={
