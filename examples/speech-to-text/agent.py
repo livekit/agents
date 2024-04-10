4c735d23fb24c5d18f4ee94d55b52d89959b1cde
Neil Dwyer <neildwyer1991@gmail.com>
2024-04-10 15:29:18 -0700

    Use a queue

--- examples/speech-to-text/agent.py
+++ examples/speech-to-text/agent.py
@@ -13,14 +13,12 @@ from livekit.plugins.deepgram import STT
 
 async def entrypoint(job: JobContext):
     logging.info("starting tts example agent")
-    stt = STT()
-    stt_stream = stt.stream()
 
-    audio_stream_future = asyncio.Future[rtc.AudioStream]()
+    stream_queue = asyncio.Queue[rtc.AudioStream]()
 
     def on_track_subscribed(track: rtc.Track, *_):
         if track.kind == rtc.TrackKind.KIND_AUDIO:
-            audio_stream_future.set_result(rtc.AudioStream(track))
+            stream_queue.put_nowait(rtc.AudioStream(track))
 
     job.room.on("track_subscribed", on_track_subscribed)
 
@@ -30,19 +28,23 @@ async def entrypoint(job: JobContext):
             # call the on_track_subscribed callback
             if track_pub.track is None:
                 continue
-            audio_stream_future.set_result(rtc.AudioStream(track_pub.track))
+            stream_queue.put_nowait(rtc.AudioStream(track_pub.track))
 
     async def process_track(audio_stream: rtc.AudioStream):
         async for audio_frame_event in audio_stream:
             stt_stream.push_frame(audio_frame_event.frame)
 
-    async def process_stt():
+    async def process_stt(stt_stream: agents.stt.STTStream):
         async for stt_event in stt_stream:
             if stt_event.type == agents.stt.SpeechEventType.FINAL_TRANSCRIPT:
                 logging.info("Got transcript: %s", stt_event.alternatives[0].text)
 
-    audio_stream = await audio_stream_future
-    await asyncio.gather(process_track(audio_stream), process_stt())
+    tasks = []
+    while True:
+        stream = await stream_queue.get()
+        stt = STT()
+        stt_stream = stt.stream()
+        tasks.append(asyncio.gather(process_track(stream), process_stt(stt_stream)))
 
 
 async def request_fnc(req: JobRequest) -> None:
