427d0dbc5175aeb88ff5e3f25021e8e1739f86eb
Neil Dwyer <neildwyer1991@gmail.com>
2024-04-11 10:38:20 -0700

    stt example

--- examples/speech-to-text/agent.py
+++ examples/speech-to-text/agent.py
@@ -13,12 +13,24 @@ from livekit.plugins.deepgram import STT
 
 async def entrypoint(job: JobContext):
     logging.info("starting tts example agent")
+    tasks = []
 
-    stream_queue = asyncio.Queue[rtc.AudioStream]()
+    async def process_track(audio_stream: rtc.AudioStream):
+        stt = STT()
+        stt_stream = stt.stream()
+        stt_task = asyncio.create_task(process_stt(stt_stream))
+        async for audio_frame_event in audio_stream:
+            stt_stream.push_frame(audio_frame_event.frame)
+        await stt_task
+
+    async def process_stt(stt_stream: agents.stt.STTStream):
+        async for stt_event in stt_stream:
+            if stt_event.type == agents.stt.SpeechEventType.FINAL_TRANSCRIPT:
+                logging.info("Got transcript: %s", stt_event.alternatives[0].text)
 
     def on_track_subscribed(track: rtc.Track, *_):
         if track.kind == rtc.TrackKind.KIND_AUDIO:
-            stream_queue.put_nowait(rtc.AudioStream(track))
+            tasks.append(asyncio.create_task(process_track(rtc.AudioStream(track))))
 
     job.room.on("track_subscribed", on_track_subscribed)
 
@@ -28,23 +40,8 @@ async def entrypoint(job: JobContext):
             # call the on_track_subscribed callback
             if track_pub.track is None:
                 continue
-            stream_queue.put_nowait(rtc.AudioStream(track_pub.track))
 
-    async def process_track(audio_stream: rtc.AudioStream):
-        async for audio_frame_event in audio_stream:
-            stt_stream.push_frame(audio_frame_event.frame)
-
-    async def process_stt(stt_stream: agents.stt.STTStream):
-        async for stt_event in stt_stream:
-            if stt_event.type == agents.stt.SpeechEventType.FINAL_TRANSCRIPT:
-                logging.info("Got transcript: %s", stt_event.alternatives[0].text)
-
-    tasks = []
-    while True:
-        stream = await stream_queue.get()
-        stt = STT()
-        stt_stream = stt.stream()
-        tasks.append(asyncio.gather(process_track(stream), process_stt(stt_stream)))
+            tasks.append(asyncio.create_task(process_track(rtc.AudioStream(track))))
 
 
 async def request_fnc(req: JobRequest) -> None:
