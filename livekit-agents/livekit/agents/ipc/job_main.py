ca9280aabdc12218abd11805a06d12ba32d2f916
Neil Dwyer <neildwyer1991@gmail.com>
2024-04-16 15:51:00 -0700

    Log exceptions when agent process throws

--- livekit-agents/livekit/agents/ipc/job_main.py
+++ livekit-agents/livekit/agents/ipc/job_main.py
@@ -4,6 +4,7 @@ import asyncio
 import contextlib
 import logging
 import os
+import traceback
 
 from livekit import rtc
 
@@ -23,9 +24,13 @@ class LogHandler(logging.Handler):
 
     def emit(self, record: logging.LogRecord) -> None:
         try:
+            msg = record.getMessage()
+            if record.exc_info:
+                type, value, tb = record.exc_info
+                msg += "\n" + "".join(traceback.format_exception(type, value, tb))
             ipc_enc.write_msg(
                 self._writer,
-                protocol.Log(level=record.levelno, message=record.getMessage()),
+                protocol.Log(level=record.levelno, message=msg),
             )
         except Exception as e:
             print(f"failed to write log: {e}")
