bdd7405de7099d4ccaae222d3acb45ac65feda0c
Théo Monnom <theo.monnom@outlook.com>
2024-05-08 19:16:15 +0200

    basic tokenizer + hyphenator & word tokenizer impl. (#275)
    * wip
    
    * nltk
    
    * test
    
    * fix cut index
    
    * Update token_stream.py
    
    * add tests for hyphenate_word

---
+++ livekit-agents/livekit/agents/tokenize/_basic_word.py
@@ -0,0 +1,18 @@
+def split_words(text: str, ignore_punctuation: bool = True) -> list[str]:
+    # fmt: off
+    punctuations = [".", ",", "!", "?", ";", ":", "'", '"', "(", ")", "[", "]", "{", "}", "<", ">"]
+    # fmt: on
+
+    if ignore_punctuation:
+        for p in punctuations:
+            # TODO(theomonnom): Ignore acronyms
+            text = text.replace(p, "")
+
+    words = text.split(" ")
+    new_words = []
+    for word in words:
+        if not word:
+            continue  # ignore empty
+        new_words.append(word)
+
+    return new_words
