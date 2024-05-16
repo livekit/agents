d426ef337edbd245020e174b8c52ce0bd1929cd6
Théo Monnom <theo.8bits@gmail.com>
2024-05-16 13:38:49 -0700

    Update tts.py

--- livekit-plugins/livekit-plugins-elevenlabs/livekit/plugins/elevenlabs/tts.py
+++ livekit-plugins/livekit-plugins-elevenlabs/livekit/plugins/elevenlabs/tts.py
@@ -149,7 +149,7 @@ class ChunkedStream(tts.ChunkedStream):
                     ),
                 ),
             ) as resp:
-                # avoid very small frame. chunk by 10ms 16bits
+                # avoid very small frames. chunk by 10ms 16bits
                 bytes_per_frame = (self._opts.sample_rate // 100) * 2
                 buf = bytearray()
                 async for data, _ in resp.content.iter_chunks():
