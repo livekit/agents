b793222167c6a339a6439f8375a75d72a6ed0db7
Théo Monnom <theo.8bits@gmail.com>
2024-05-18 15:05:15 -0700

    Update deepgram_stt.py

--- examples/speech-to-text/deepgram_stt.py
+++ examples/speech-to-text/deepgram_stt.py
@@ -17,11 +17,13 @@ async def _forward_transcription(
     stt_stream: stt.SpeechStream,
     stt_forwarder: transcription.STTSegmentsForwarder,
 ):
+    """Forward the transcription to the client and log the transcript in the console"""
     async for ev in stt_stream:
         stt_forwarder.update(ev)
         if ev.type == stt.SpeechEventType.INTERIM_TRANSCRIPT:
             print(ev.alternatives[0].text, end="")
         elif ev.type == stt.SpeechEventType.FINAL_TRANSCRIPT:
+            print("\n")
             print(" -> ", ev.alternatives[0].text)
 
 
