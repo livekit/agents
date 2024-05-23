d5ba5abd88ff5f4b89b30f5b4027f836609ddd2f
Neil Dwyer <neildwyer1991@gmail.com>
2024-05-23 08:55:53 -0700

    Bump versions (#319)
    * Bump versions
    
    * r.txt

--- livekit-plugins/livekit-plugins-openai/setup.py
+++ livekit-plugins/livekit-plugins-openai/setup.py
@@ -49,7 +49,7 @@ setuptools.setup(
     python_requires=">=3.9.0",
     install_requires=[
         "livekit ~= 0.11",
-        "livekit-agents~=0.6.0",
+        "livekit-agents~=0.7.dev0",
         "openai >= 1.0.0",
         "requests >= 2, < 3",
     ],
