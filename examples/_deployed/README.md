812b7745f4519c203faac942b257c165a6ffbc75
Théo Monnom <theo.monnom@outlook.com>
2024-05-29 20:56:49 +0200

    add chat input to Kitt (#329)
    * wip
    
    * remove assistant from prompt, & use pcm_44100
    
    * Update kitt.py

---
+++ examples/_deployed/README.md
@@ -0,0 +1,1 @@
+# The source code of our deployed examples
---
+++ examples/_deployed/kitt/kitt.py
@@ -0,0 +1,66 @@
+import asyncio
+import copy
+import logging
+
+from livekit import rtc
+from livekit.agents import JobContext, JobRequest, WorkerOptions, cli
+from livekit.agents.llm import (
+    ChatContext,
+    ChatMessage,
+    ChatRole,
+)
+from livekit.agents.voice_assistant import VoiceAssistant
+from livekit.plugins import deepgram, elevenlabs, openai, silero
+
+
+async def entrypoint(ctx: JobContext):
+    initial_ctx = ChatContext(
+        messages=[
+            ChatMessage(
+                role=ChatRole.SYSTEM,
+                text=(
+                    "You are a funny bot created by LiveKit. Your interface with users will be voice. "
+                    "You should use short and concise responses, and avoiding usage of unpronouncable punctuation."
+                ),
+            )
+        ]
+    )
+
+    gpt = openai.LLM()
+    assistant = VoiceAssistant(
+        vad=silero.VAD(),
+        stt=deepgram.STT(),
+        llm=gpt,
+        tts=elevenlabs.TTS(encoding="pcm_44100"),
+        chat_ctx=initial_ctx,
+    )
+
+    chat = rtc.ChatManager(ctx.room)
+
+    async def _answer_from_text(text: str):
+        chat_ctx = copy.deepcopy(assistant.chat_context)
+        chat_ctx.messages.append(ChatMessage(role=ChatRole.USER, text=text))
+
+        stream = await gpt.chat(chat_ctx)
+        await assistant.say(stream)
+
+    @chat.on("message_received")
+    def on_chat_received(msg: rtc.ChatMessage):
+        if not msg.message:
+            return
+
+        asyncio.create_task(_answer_from_text(msg.message))
+
+    assistant.start(ctx.room)
+
+    await asyncio.sleep(3)
+    await assistant.say("Hey, how can I help you today?", allow_interruptions=True)
+
+
+async def request_fnc(req: JobRequest) -> None:
+    logging.info("received request %s", req)
+    await req.accept(entrypoint)
+
+
+if __name__ == "__main__":
+    cli.run_app(WorkerOptions(request_fnc))
