a7e5614c8b0eeb319665b380b2823401b8185d5c
David Zhao <dz@livekit.io>
2024-06-07 16:04:47 -0700

    py39 compat

--- livekit-plugins/livekit-plugins-openai/livekit/plugins/openai/utils.py
+++ livekit-plugins/livekit-plugins-openai/livekit/plugins/openai/utils.py
@@ -1,7 +1,8 @@
 import os
+from typing import Optional
 
 
-def get_base_url(base_url: str | None) -> str:
-    if base_url is None:
+def get_base_url(base_url: Optional[str]) -> str:
+    if not base_url:
         base_url = os.getenv("OPENAI_BASE_URL", "https://api.openai.com/v1")
     return base_url
