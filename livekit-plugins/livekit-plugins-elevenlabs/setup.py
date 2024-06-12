a4457e14c29f46bb7f7d0a1a90700de43bdead4d
Théo Monnom <theo.8bits@gmail.com>
2024-06-12 22:02:00 +0200

    11labs should be minor

--- livekit-plugins/livekit-plugins-elevenlabs/setup.py
+++ livekit-plugins/livekit-plugins-elevenlabs/setup.py
@@ -50,7 +50,7 @@ setuptools.setup(
     packages=setuptools.find_namespace_packages(include=["livekit.*"]),
     python_requires=">=3.9.0",
     install_requires=[
-        "livekit-agents[codecs]~=0.7",
+        "livekit-agents[codecs]>=0.7.2",
     ],
     package_data={
         "livekit.plugins.elevenlabs": ["py.typed"],
