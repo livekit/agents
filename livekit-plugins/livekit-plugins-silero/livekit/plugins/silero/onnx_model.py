40eb6833b4b9d56d4f2238a5ddc79aaab5be1b4c
Théo Monnom <theo.8bits@gmail.com>
2024-06-18 19:54:07 +0200

    Update onnx_model.py

--- livekit-plugins/livekit-plugins-silero/livekit/plugins/silero/onnx_model.py
+++ livekit-plugins/livekit-plugins-silero/livekit/plugins/silero/onnx_model.py
@@ -3,7 +3,7 @@ import importlib.resources
 from contextlib import ExitStack
 
 import numpy as np
-import onnxruntime
+import onnxruntime # type: ignore
 
 _resource_files = ExitStack()
 atexit.register(_resource_files.close)
