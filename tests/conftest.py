b2241d477e2e44971ab7213875760fbaed3940dd
Neil Dwyer <neildwyer1991@gmail.com>
2024-06-21 17:10:15 -0700

    more typing work

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,6 +4,6 @@ from livekit.agents import utils
 
 @pytest.fixture
 def job_process(event_loop):
-    utils.http_context.new_session_ctx()
+    utils.http_context._new_session_ctx()
     yield
     event_loop.run_until_complete(utils.http_context.close_http_ctx())
