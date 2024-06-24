3f87846160667db82b6cf4c296efcf70c582f699
Neil Dwyer <neildwyer1991@gmail.com>
2024-06-24 10:55:11 -0700

    cooking

--- livekit-agents/livekit/agents/utils/__init__.py
+++ livekit-agents/livekit/agents/utils/__init__.py
@@ -1,7 +1,12 @@
 from . import codecs, images
 from .event_emitter import EventEmitter
 from .exp_filter import ExpFilter
-from .http_context import _noop, http_session  # type: ignore
+from .http_context import (
+    _close_http_ctx,
+    _new_session_ctx,
+    _noop,
+    http_session,
+)
 from .log import log_exceptions
 from .misc import AudioBuffer, merge_frames, time_ms
 from .moving_average import MovingAverage
