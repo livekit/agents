ca227e8282fb2baa4eebed41d6eb5624a0e794b2
Neil Dwyer <neildwyer1991@gmail.com>
2024-07-23 12:50:24 -0700

    fix type

--- livekit-plugins/livekit-plugins-google/livekit/plugins/google/stt.py
+++ livekit-plugins/livekit-plugins-google/livekit/plugins/google/stt.py
@@ -131,9 +131,9 @@ class STT(stt.STT):
 
     async def recognize(
         self,
-        *,
         buffer: utils.AudioBuffer,
-        language: str | None = None,
+        *,
+        language: SpeechLanguages | str | None = None,
     ) -> stt.SpeechEvent:
         config = self._sanitize_options(language=language)
         frame = agents.utils.merge_frames(buffer)
