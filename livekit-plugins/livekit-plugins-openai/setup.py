f9be298f21681eac6e68a9a3ae6fb578d9daf906
Théo Monnom <theo.monnom@outlook.com>
2024-07-24 21:49:22 +0200

    bump dev version (#510)

--- livekit-plugins/livekit-plugins-openai/setup.py
+++ livekit-plugins/livekit-plugins-openai/setup.py
@@ -47,7 +47,7 @@ setuptools.setup(
     license="Apache-2.0",
     packages=setuptools.find_namespace_packages(include=["livekit.*"]),
     python_requires=">=3.9.0",
-    install_requires=["livekit-agents[codecs, images]>=0.7.2", "openai ~= 1.35"],
+    install_requires=["livekit-agents[codecs, images]>=0.8.0.dev0", "openai ~= 1.35"],
     package_data={"livekit.plugins.openai": ["py.typed"]},
     project_urls={
         "Documentation": "https://docs.livekit.io",
