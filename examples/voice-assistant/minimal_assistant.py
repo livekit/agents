9a72cccdd91c11af32c06e3255b17628c03a6041
David Zhao <dz@livekit.io>
2024-08-02 18:38:16 -0700

    Include chat handling with minimal_assistant (#568)

--- examples/voice-assistant/minimal_assistant.py
+++ examples/voice-assistant/minimal_assistant.py
@@ -1,6 +1,7 @@
 import asyncio
 
 from dotenv import load_dotenv
+from livekit import rtc
 from livekit.agents import AutoSubscribe, JobContext, WorkerOptions, cli, llm
 from livekit.agents.voice_assistant import VoiceAssistant
 from livekit.plugins import deepgram, openai, silero
@@ -19,14 +20,30 @@ async def entrypoint(ctx: JobContext):
 
     await ctx.connect(auto_subscribe=AutoSubscribe.AUDIO_ONLY)
 
+    llm_plugin = openai.LLM()
     assistant = VoiceAssistant(
         vad=silero.VAD.load(),
         stt=deepgram.STT(),
-        llm=openai.LLM(),
+        llm=llm_plugin,
         tts=openai.TTS(),
         chat_ctx=initial_ctx,
     )
     assistant.start(ctx.room)
+
+    # listen to incoming chat messages, only required if you'd like the agent to
+    # answer incoming messages from Chat
+    chat = rtc.ChatManager(ctx.room)
+
+    async def answer_from_text(txt: str):
+        chat_ctx = assistant.chat_ctx.copy()
+        chat_ctx.append(role="user", text=txt)
+        stream = llm_plugin.chat(chat_ctx=chat_ctx)
+        await assistant.say(stream)
+
+    @chat.on("message_received")
+    def on_chat_received(msg: rtc.ChatMessage):
+        if msg.message:
+            asyncio.create_task(answer_from_text(msg.message))
 
     await asyncio.sleep(1)
     await assistant.say("Hey, how can I help you today?", allow_interruptions=True)
