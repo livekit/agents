b59cf32cae1d9fa2e82f6d2a13b748227acb0cb7
Neil Dwyer <neildwyer1991@gmail.com>
2024-08-07 12:55:18 -0700

    misc

--- livekit-plugins/livekit-plugins-openai/livekit/plugins/openai/__init__.py
+++ livekit-plugins/livekit-plugins-openai/livekit/plugins/openai/__init__.py
@@ -12,6 +12,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from .assistant_llm import (
+    AssistantCreateOptions,
+    AssistantLLM,
+    AssistantLLMStream,
+    AssistantLoadOptions,
+)
 from .embeddings import EmbeddingData, create_embeddings
 from .llm import LLM, LLMStream
 from .models import TTSModels, TTSVoices, WhisperModels
@@ -24,6 +30,10 @@ __all__ = [
     "TTS",
     "LLM",
     "LLMStream",
+    "AssistantCreateOptions",
+    "AssistantLoadOptions",
+    "AssistantLLM",
+    "AssistantLLMStream",
     "WhisperModels",
     "TTSModels",
     "TTSVoices",
--- livekit-plugins/livekit-plugins-openai/livekit/plugins/openai/assistant_llm.py
+++ livekit-plugins/livekit-plugins-openai/livekit/plugins/openai/assistant_llm.py
@@ -15,18 +15,17 @@
 from __future__ import annotations
 
 import asyncio
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from typing import Any, MutableSet
 
+import httpx
 from livekit.agents import llm
 
 from openai import AsyncAssistantEventHandler, AsyncClient
-from openai.types.beta.assistant import Assistant
-from openai.types.beta.thread import Thread
 from openai.types.beta.threads import Text, TextDelta
 from openai.types.beta.threads.runs import ToolCall, ToolCallDelta
 
-from .models import ChatModels
+from .models import AssistantTools, ChatModels
 from .utils import build_oai_message
 
 DEFAULT_MODEL = "gpt-4o"
@@ -38,20 +37,59 @@ class LLMOptions:
     model: str | ChatModels
 
 
+@dataclass
+class AssistantCreateOptions:
+    name: str
+    instructions: str
+    model: ChatModels
+    tools: list[AssistantTools] = field(default_factory=list)
+
+
+@dataclass
+class AssistantLoadOptions:
+    assistant_id: str
+    thread_id: str
+
+
 class AssistantLLM(llm.LLM):
     def __init__(
         self,
         *,
-        client: AsyncClient,
-        assistant: Assistant,
-        thread: Thread,
-        instructions: str | None = None,
+        assistant_opts: AssistantCreateOptions | AssistantLoadOptions,
+        client: AsyncClient | None = None,
+        api_key: str | None = None,
+        base_url: str | None = None,
     ) -> None:
-        self._client = client
-        self._assistant = assistant
-        self._thread = thread
-        self._instructions = instructions
+        self._client = client or AsyncClient(
+            api_key=api_key,
+            base_url=base_url,
+            http_client=httpx.AsyncClient(
+                timeout=5.0,
+                follow_redirects=True,
+                limits=httpx.Limits(
+                    max_connections=1000,
+                    max_keepalive_connections=100,
+                    keepalive_expiry=120,
+                ),
+            ),
+        )
+        self._assistant_opts = assistant_opts
         self._running_fncs: MutableSet[asyncio.Task[Any]] = set()
+
+        self._sync_openai_task = asyncio.create_task(self._sync_openai())
+
+    async def _sync_openai(self) -> AssistantLoadOptions:
+        if isinstance(self._assistant_opts, AssistantCreateOptions):
+            assistant = await self._client.beta.assistants.create(
+                model=self._assistant_opts.model,
+                name=self._assistant_opts.name,
+                instructions=self._assistant_opts.instructions,
+                tools=[{"type": t} for t in self._assistant_opts.tools],
+            )
+            thread = await self._client.beta.threads.create()
+            return AssistantLoadOptions(assistant_id=assistant.id, thread_id=thread.id)
+        else:
+            return self._assistant_opts
 
     def chat(
         self,
@@ -63,12 +101,11 @@ class AssistantLLM(llm.LLM):
         parallel_tool_calls: bool | None = None,
     ):
         return AssistantLLMStream(
+            cache_key=id(self),
+            sync_openai_task=self._sync_openai_task,
             client=self._client,
-            thread=self._thread,
-            assistant=self._assistant,
             chat_ctx=chat_ctx,
             fnc_ctx=fnc_ctx,
-            instructions=self._instructions,
         )
 
 
@@ -77,12 +114,19 @@ class AssistantLLMStream(llm.LLMStream):
         def __init__(
             self, output_queue: asyncio.Queue[llm.ChatChunk | Exception | None]
         ):
+            super().__init__()
             self._output_queue = output_queue
 
-        async def on_text_created(self, text: Text) -> None:
-            print("NEIL text", text.value)
-
         async def on_text_delta(self, delta: TextDelta, snapshot: Text):
+            self._output_queue.put_nowait(
+                llm.ChatChunk(
+                    choices=[
+                        llm.Choice(
+                            delta=llm.ChoiceDelta(role="assistant", content=delta.value)
+                        )
+                    ]
+                )
+            )
             print("NEIL text delta", delta.value, snapshot.value)
 
         async def on_tool_call_created(self, tool_call: ToolCall):
@@ -95,46 +139,50 @@ class AssistantLLMStream(llm.LLMStream):
     def __init__(
         self,
         *,
+        cache_key: int,
         client: AsyncClient,
-        thread: Thread,
-        assistant: Assistant,
+        sync_openai_task: asyncio.Task[AssistantLoadOptions],
         chat_ctx: llm.ChatContext,
         fnc_ctx: llm.FunctionContext | None,
-        instructions: str | None,
     ) -> None:
         super().__init__(chat_ctx=chat_ctx, fnc_ctx=fnc_ctx)
-        self._instructions = instructions
+        self._cache_key = cache_key
         self._client = client
-        self._thread = thread
-        self._assistant = assistant
+        self._thread_id: str | None = None
+        self._assistant_id: str | None = None
 
         # current function call that we're waiting for full completion (args are streamed)
         self._tool_call_id: str | None = None
         self._fnc_name: str | None = None
         self._fnc_raw_arguments: str | None = None
-        self._create_stream_task = asyncio.create_task(self._create_stream())
         self._output_queue = asyncio.Queue[llm.ChatChunk | Exception | None]()
+        self._create_stream_task = asyncio.create_task(self._create_stream())
+        self._sync_openai_task = sync_openai_task
 
     async def _sync_thread(self) -> None:
+        load_options = await self._sync_openai_task
         for msg in self._chat_ctx.messages:
+            if msg.role == "assistant":
+                continue
             if MESSAGE_THREAD_KEY not in msg._metadata:
                 msg._metadata[MESSAGE_THREAD_KEY] = set[str]()
-            if self._thread.id not in msg._metadata[MESSAGE_THREAD_KEY]:
-                converted_msg = build_oai_message(msg)
+            if self._thread_id not in msg._metadata[MESSAGE_THREAD_KEY]:
+                converted_msg = build_oai_message(msg, self._cache_key)
                 await self._client.beta.threads.messages.create(
-                    thread_id=self._thread.id,
+                    thread_id=load_options.thread_id,
                     role=msg.role,
                     content=converted_msg["content"],
                 )
-                msg._metadata[MESSAGE_THREAD_KEY].add(self._thread.id)
+                msg._metadata[MESSAGE_THREAD_KEY].add(self._thread_id)
 
     async def _create_stream(self) -> None:
         try:
             await self._sync_thread()
+            load_options = await self._sync_openai_task
+
             async with self._client.beta.threads.runs.stream(
-                thread_id=self._thread.id,
-                assistant_id=self._assistant.id,
-                instructions=self._instructions,
+                thread_id=load_options.thread_id,
+                assistant_id=load_options.assistant_id,
                 event_handler=AssistantLLMStream.EventHandler(self._output_queue),
             ) as stream:
                 await stream.until_done()
