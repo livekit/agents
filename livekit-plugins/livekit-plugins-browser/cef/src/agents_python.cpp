81a9bec12fc558c488f34c4d0ef1058b79cbf7e8
Théo Monnom <theo.8bits@gmail.com>
2024-08-12 04:46:17 +0200

    livekit-plugins-browser: initial dev renderer (#616)

--- livekit-plugins/livekit-plugins-browser/cef/src/agents_python.cpp
+++ livekit-plugins/livekit-plugins-browser/cef/src/agents_python.cpp
@@ -3,25 +3,50 @@
 #include "app.hpp"
 #include "include/internal/cef_mac.h"
 
+#include <pybind11/pybind11.h>
+#include <pybind11/functional.h>
+
 namespace py = pybind11;
 
-BrowserImpl::BrowserImpl() {
-
+BrowserApp::BrowserApp(const AppOptions& options) : options_(options) {
+  app_ = new AgentApp(options_.dev_mode, options_.initialized_callback);
 }
 
+std::shared_ptr<BrowserImpl> BrowserApp::CreateBrowser(
+    const std::string& url,
+    const BrowserOptions& options) {
 
-void BrowserImpl::start() {
-    py::print("BrowserImpl::start()");
-    AgentApp::run();
+  app_->CreateBrowser(url, options.framerate, options.created_callback);
+  return nullptr;//std::make_shared<BrowserImpl>();
 }
 
+int BrowserApp::Run() {
+  return RunAgentApp(app_);
+}
 
-PYBIND11_MODULE(lkcef_python, m)
-{
+BrowserImpl::BrowserImpl() {}
+
+void BrowserImpl::SetSize(int width, int height) {}
+
+PYBIND11_MODULE(lkcef_python, m) {
+  // Isn't that fucking cool? llm using browsers
   m.doc() = "Chromium Embedded Framework (CEF) for LiveKit Agents";
-  
-    
+
+  py::class_<AppOptions>(m, "AppOptions")
+      .def(py::init())
+      .def_readwrite("dev_mode", &AppOptions::dev_mode)
+      .def_readwrite("initialized_callback", &AppOptions::initialized_callback);
+
+  py::class_<BrowserOptions>(m, "BrowserOptions")
+      .def(py::init())
+      .def_readwrite("framerate", &BrowserOptions::framerate)
+      .def_readwrite("created_callback", &BrowserOptions::created_callback);
+
+  py::class_<BrowserApp>(m, "BrowserApp")
+      .def(py::init<const AppOptions&>())
+      .def("create_browser", &BrowserApp::CreateBrowser)
+      .def("run", &BrowserApp::Run);
+
   py::class_<BrowserImpl>(m, "BrowserImpl")
-    .def(py::init<>())
-    .def("start", &BrowserImpl::start);
+      .def("set_size", &BrowserImpl::SetSize);
 }
--- livekit-plugins/livekit-plugins-browser/cef/src/app.hpp
+++ livekit-plugins/livekit-plugins-browser/cef/src/app.hpp
@@ -1,6 +1,8 @@
 #ifndef LKCEF_APP_HPP
 #define LKCEF_APP_HPP
 
+#include "dev_renderer.hpp"
+#include "handler.hpp"
 #include "include/cef_app.h"
 #include "include/cef_base.h"
 #include "include/cef_browser_process_handler.h"
@@ -8,20 +10,38 @@
 #include "include/internal/cef_ptr.h"
 
 class AgentApp : public CefApp, public CefBrowserProcessHandler {
-public:
-  AgentApp();
+ public:
+  AgentApp(bool dev_mode, std::function<void()> initialized_callback);
 
   CefRefPtr<CefBrowserProcessHandler> GetBrowserProcessHandler() override {
     return this;
   }
 
+  void OnBeforeCommandLineProcessing(
+      const CefString& process_type,
+      CefRefPtr<CefCommandLine> command_line) override;
+
   void OnContextInitialized() override;
+
   CefRefPtr<CefClient> GetDefaultClient() override;
 
-  static int run();
+  CefRefPtr<BrowserHandle> CreateBrowser(
+      const std::string& url,
+      int framerate,
+      std::function<void()> created_callback);
 
-private:
+  int Run();
+
+ private:
   IMPLEMENT_REFCOUNTING(AgentApp);
+
+  CefRefPtr<AgentHandler> client_;
+  CefRefPtr<DevRenderer> dev_renderer_;
+
+  bool dev_mode_;
+  std::function<void()> initialized_callback_;
 };
 
-#endif // LKCEF_APP_HPP
+int RunAgentApp(CefRefPtr<AgentApp> app);
+
+#endif  // LKCEF_APP_HPP
