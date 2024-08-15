7eb83e27ddc353c3e9f924e00fe416983cbc86ad
aoife cassidy <aoife@livekit.io>
2024-08-15 14:42:30 -0700

    openai: allow sending user IDs
    Fixes #612

--- livekit-plugins/livekit-plugins-openai/livekit/plugins/openai/llm.py
+++ livekit-plugins/livekit-plugins-openai/livekit/plugins/openai/llm.py
@@ -200,6 +200,7 @@ class LLM(llm.LLM):
         temperature: float | None = None,
         n: int | None = 1,
         parallel_tool_calls: bool | None = None,
+        user: str | None = None,
     ) -> "LLMStream":
         opts: dict[str, Any] = dict()
         if fnc_ctx and len(fnc_ctx.ai_functions) > 0:
@@ -219,6 +220,7 @@ class LLM(llm.LLM):
             n=n,
             temperature=temperature,
             stream=True,
+            user=user,
             **opts,
         )
 
