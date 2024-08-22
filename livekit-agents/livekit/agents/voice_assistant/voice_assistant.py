45da07ba346d8a6ca2f07b20e29955c52b6549df
lukasIO <mail@lukasseiler.de>
2024-08-22 14:20:53 +0200

    Add VoiceAssistantState

--- livekit-agents/livekit/agents/voice_assistant/voice_assistant.py
+++ livekit-agents/livekit/agents/voice_assistant/voice_assistant.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 import asyncio
 import contextvars
 import time
+from enum import Enum
 from dataclasses import dataclass
 from typing import Any, AsyncIterable, Awaitable, Callable, Literal, Optional, Union
 
@@ -32,6 +33,13 @@ EventTypes = Literal[
     "agent_speech_interrupted",
     "function_calls_collected",
     "function_calls_finished",
+]
+
+VoiceAssistantState = Literal[
+    "initializing",
+    "listening",
+    "thinking",
+    "speaking"
 ]
 
 
@@ -301,6 +309,10 @@ class VoiceAssistant(utils.EventEmitter[EventTypes]):
         new_handle.initialize(source=source, synthesis_handle=synthesis_handle)
         self._add_speech_for_playout(new_handle)
 
+    async def set_state(self, state: VoiceAssistantState):
+        """Set the current state of the voice assistant"""
+        await self._room.local_participant.set_attributes({'agent.state': state })
+
     async def aclose(self) -> None:
         """Close the voice assistant"""
         if not self._started:
@@ -329,10 +341,11 @@ class VoiceAssistant(utils.EventEmitter[EventTypes]):
             transcription=self._opts.transcription.user_transcription,
         )
 
-        def _on_start_of_speech(ev: vad.VADEvent) -> None:
+        async def _on_start_of_speech(ev: vad.VADEvent) -> None:
             self._plotter.plot_event("user_started_speaking")
             self.emit("user_started_speaking")
             self._deferred_validation.on_human_start_of_speech(ev)
+            await self.set_state('listening')
 
         def _on_vad_updated(ev: vad.VADEvent) -> None:
             if not self._track_published_fut.done():
@@ -354,11 +367,13 @@ class VoiceAssistant(utils.EventEmitter[EventTypes]):
             if ev.speech_duration >= self._opts.int_speech_duration:
                 self._interrupt_if_possible()
 
-        def _on_end_of_speech(ev: vad.VADEvent) -> None:
+        async def _on_end_of_speech(ev: vad.VADEvent) -> None:
             self._plotter.plot_event("user_stopped_speaking")
             self.emit("user_stopped_speaking")
             self._deferred_validation.on_human_end_of_speech(ev)
             self._last_end_of_speech_time = time.time()
+            await self.set_state("thinking")
+
 
         def _on_interim_transcript(ev: stt.SpeechEvent) -> None:
             self._transcribed_interim_text = ev.alternatives[0].text
@@ -393,6 +408,7 @@ class VoiceAssistant(utils.EventEmitter[EventTypes]):
         if self._opts.plotting:
             await self._plotter.start()
 
+        await self.set_state('initializing');
         audio_source = rtc.AudioSource(self._tts.sample_rate, self._tts.num_channels)
         track = rtc.LocalAudioTrack.create_audio_track("assistant_voice", audio_source)
         self._agent_publication = await self._room.local_participant.publish_track(
@@ -407,13 +423,16 @@ class VoiceAssistant(utils.EventEmitter[EventTypes]):
             tts=self._tts,
         )
 
-        def _on_playout_started() -> None:
+        async def _on_playout_started() -> None:
             self._plotter.plot_event("agent_started_speaking")
             self.emit("agent_started_speaking")
+            await self.set_state('speaking')
 
-        def _on_playout_stopped(interrupted: bool) -> None:
+        async def _on_playout_stopped(interrupted: bool) -> None:
             self._plotter.plot_event("agent_stopped_speaking")
             self.emit("agent_stopped_speaking")
+            await self.set_state('listening')
+
 
         agent_playout.on("playout_started", _on_playout_started)
         agent_playout.on("playout_stopped", _on_playout_stopped)
