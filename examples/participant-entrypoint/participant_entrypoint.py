13400e2d3e8b9b592ffb408958903ea151418577
Neil Dwyer <neildwyer1991@gmail.com>
2024-08-28 14:15:05 -0700

    add tasks before connect

--- examples/participant-entrypoint/participant_entrypoint.py
+++ examples/participant-entrypoint/participant_entrypoint.py
@@ -3,6 +3,8 @@ import logging
 
 from livekit import rtc
 from livekit.agents import AutoSubscribe, JobContext, WorkerOptions, cli
+from dotenv import load_dotenv
+load_dotenv()
 
 logger = logging.getLogger("my-worker")
 logger.setLevel(logging.INFO)
@@ -10,8 +12,7 @@ logger.setLevel(logging.INFO)
 
 async def entrypoint(ctx: JobContext):
     logger.info("starting entrypoint")
-    await ctx.connect(auto_subscribe=AutoSubscribe.SUBSCRIBE_ALL)
-    logger.info("connected to the room")
+
 
     async def participant_task_1(p: rtc.RemoteParticipant):
         # You can filter out participants you are not interested in
@@ -28,11 +29,14 @@ async def entrypoint(ctx: JobContext):
 
     async def participant_task_2(p: rtc.RemoteParticipant):
         # multiple tasks can be run concurrently for each participant
+        logger.info(f"participant task 2 starting for {p.identity}")
         await asyncio.sleep(10)
 
     ctx.add_participant_entrypoint(entrypoint_fnc=participant_task_1)
     ctx.add_participant_entrypoint(entrypoint_fnc=participant_task_2)
 
+    await ctx.connect(auto_subscribe=AutoSubscribe.SUBSCRIBE_ALL)
+    logger.info("connected to the room")
 
 if __name__ == "__main__":
     cli.run_app(WorkerOptions(entrypoint_fnc=entrypoint))
