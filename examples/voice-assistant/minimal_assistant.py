27d0ac5120e12e7d3139edaa18fb74103bfcf618
David Zhao <dz@livekit.io>
2024-09-02 16:14:28 -0700

    Update examples to use participant_entrypoint

--- examples/voice-assistant/minimal_assistant.py
+++ examples/voice-assistant/minimal_assistant.py
@@ -1,15 +1,30 @@
 import asyncio
+import logging
 
 from dotenv import load_dotenv
 from livekit import rtc
-from livekit.agents import AutoSubscribe, JobContext, WorkerOptions, cli, llm
+from livekit.agents import (
+    AutoSubscribe,
+    JobContext,
+    JobProcess,
+    WorkerOptions,
+    cli,
+    llm,
+)
 from livekit.agents.voice_assistant import VoiceAssistant
 from livekit.plugins import deepgram, openai, silero
 
 load_dotenv()
+logger = logging.getLogger("myagent")
 
 
-async def entrypoint(ctx: JobContext):
+def prewarm_process(proc: JobProcess):
+    # preload silero VAD in memory to speed up session start
+    proc.userdata["vad"] = silero.VAD.load()
+
+
+async def handle_participant(ctx: JobContext, participant: rtc.RemoteParticipant):
+    logger.info(f"handling participant {participant.identity}")
     initial_ctx = llm.ChatContext().append(
         role="system",
         text=(
@@ -17,18 +32,16 @@ async def entrypoint(ctx: JobContext):
             "You should use short and concise responses, and avoiding usage of unpronouncable punctuation."
         ),
     )
-
-    await ctx.connect(auto_subscribe=AutoSubscribe.AUDIO_ONLY)
-
     assistant = VoiceAssistant(
-        vad=silero.VAD.load(),
+        vad=ctx.proc.userdata["vad"],
         stt=deepgram.STT(),
         llm=openai.LLM(),
         tts=openai.TTS(),
         chat_ctx=initial_ctx,
     )
-    assistant.start(ctx.room)
+    assistant.start(ctx.room, participant=participant)
 
+    # START_CHAT_HANDLING
     # listen to incoming chat messages, only required if you'd like the agent to
     # answer incoming messages from Chat
     chat = rtc.ChatManager(ctx.room)
@@ -44,8 +57,25 @@ async def entrypoint(ctx: JobContext):
         if msg.message:
             asyncio.create_task(answer_from_text(msg.message))
 
+    # END_CHAT_HANDLING
+
     await assistant.say("Hey, how can I help you today?", allow_interruptions=True)
 
 
+async def entrypoint(ctx: JobContext):
+    logger.info(f"joining room {ctx.job.room.name}")
+
+    # spawn a task to handle when the participant that joins
+    ctx.add_participant_entrypoint(handle_participant)
+
+    # now connect to the room
+    await ctx.connect(auto_subscribe=AutoSubscribe.AUDIO_ONLY)
+
+
 if __name__ == "__main__":
-    cli.run_app(WorkerOptions(entrypoint_fnc=entrypoint))
+    cli.run_app(
+        WorkerOptions(
+            entrypoint_fnc=entrypoint,
+            prewarm_fnc=prewarm_process,
+        ),
+    )
