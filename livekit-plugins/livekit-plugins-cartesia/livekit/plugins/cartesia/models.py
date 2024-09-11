81b47bb954aa51a5c741127a24524488d2cc5298
Ben Cherry <bcherry@gmail.com>
2024-09-11 11:06:07 -0700

    Add support for cartesia voice control (#740)
    Co-authored-by: Théo Monnom <theo.8bits@gmail.com>

--- livekit-plugins/livekit-plugins-cartesia/livekit/plugins/cartesia/models.py
+++ livekit-plugins/livekit-plugins-cartesia/livekit/plugins/cartesia/models.py
@@ -8,7 +8,34 @@ TTSEncoding = Literal[
     # "pcm_alaw",
 ]
 
-
 TTSModels = Literal["sonic-english", "sonic-multilingual"]
 TTSLanguages = Literal["en", "es", "fr", "de", "pt", "zh", "ja"]
 TTSDefaultVoiceId = "c2ac25f9-ecc4-4f56-9095-651354df60c0"
+TTSVoiceSpeed = Literal["fastest", "fast", "normal", "slow", "slowest"]
+TTSVoiceEmotion = Literal[
+    "anger:lowest",
+    "anger:low",
+    "anger",
+    "anger:high",
+    "anger:highest",
+    "positivity:lowest",
+    "positivity:low",
+    "positivity",
+    "positivity:high",
+    "positivity:highest",
+    "surprise:lowest",
+    "surprise:low",
+    "surprise",
+    "surprise:high",
+    "surprise:highest",
+    "sadness:lowest",
+    "sadness:low",
+    "sadness",
+    "sadness:high",
+    "sadness:highest",
+    "curiosity:lowest",
+    "curiosity:low",
+    "curiosity",
+    "curiosity:high",
+    "curiosity:highest",
+]
