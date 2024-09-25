d3e13a985d52fde6b2be519acdeaf95cffdd0b1c
Jax <anyetiangong@qq.com>
2024-09-26 01:08:38 +0800

    fix the problem of infinite loop when agent speech is interrupted (#790)

---
+++ .changeset/moody-doors-poke.md
@@ -0,0 +1,5 @@
+---
+"livekit-agents": patch
+---
+
+fix VoiceAssisstant being stuck when interrupting before user speech is committed
--- livekit-agents/livekit/agents/voice_assistant/voice_assistant.py
+++ livekit-agents/livekit/agents/voice_assistant/voice_assistant.py
@@ -640,6 +640,9 @@ class VoiceAssistant(utils.EventEmitter[EventTypes]):
             )
 
             _commit_user_question_if_needed()
+            
+            if speech_handle.interrupted:
+                break
 
         _commit_user_question_if_needed()
 
