17f50d1ed76edcb3f94b11b13aebccce1e516561
Théo Monnom <theo.monnom@outlook.com>
2024-10-29 21:12:43 -0700

    noisy logs

--- livekit-agents/livekit/agents/cli/log.py
+++ livekit-agents/livekit/agents/cli/log.py
@@ -20,6 +20,13 @@ NOISY_LOGGERS = [
     "watchfiles",
 ]
 
+def _silence_noisy_loggers() -> None:
+    for noisy_logger in NOISY_LOGGERS:
+        logger = logging.getLogger(noisy_logger)
+        if logger.level == logging.NOTSET:
+            logger.setLevel(logging.WARN)
+
+
 # skip default LogRecord attributes
 # http://docs.python.org/library/logging.html#logrecord-attributes
 _RESERVED_ATTRS: Tuple[str, ...] = (
@@ -208,11 +215,8 @@ def setup_logging(log_level: str, devmode: bool) -> None:
     root = logging.getLogger()
     root.addHandler(handler)
     root.setLevel(log_level)
-
-    for noisy_logger in NOISY_LOGGERS:
-        logger = logging.getLogger(noisy_logger)
-        if logger.level == logging.NOTSET:
-            logger.setLevel(logging.WARN)
+    
+    silence_noisy_loggers()
 
     from ..log import logger
 
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,6 @@
 import pytest
 from livekit.agents import utils
+from livekit.agents.cli import log
 
 
 @pytest.fixture
@@ -7,3 +8,8 @@ def job_process(event_loop):
     utils.http_context._new_session_ctx()
     yield
     event_loop.run_until_complete(utils.http_context._close_http_ctx())
+
+
+@pytest.fixture(autouse=True)
+def configure_test():
+    log._silence_noisy_loggers()
