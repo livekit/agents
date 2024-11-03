c623506aa5adeecb284f06cc95be2415821cc722
Jayesh <jayeshparmar9829@gmail.com>
2024-11-03 09:01:03 +0530

    init

--- livekit-plugins/livekit-plugins-google/livekit/plugins/google/__init__.py
+++ livekit-plugins/livekit-plugins-google/livekit/plugins/google/__init__.py
@@ -14,9 +14,10 @@
 
 from .stt import STT, SpeechStream
 from .tts import TTS
+from .llm import LLM
 from .version import __version__
 
-__all__ = ["STT", "TTS", "SpeechStream", "__version__"]
+__all__ = ["LLM","STT", "TTS", "SpeechStream", "__version__"]
 
 from livekit.agents import Plugin
 
