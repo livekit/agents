01fa5b3370e80c7f0d804d64a4e15904aebd8cb2
Jayesh <jayeshparmar9829@gmail.com>
2024-11-08 17:21:40 +0530

    handles function call error

--- examples/voice-pipeline-agent/function_calling_weather.py
+++ examples/voice-pipeline-agent/function_calling_weather.py
@@ -42,7 +42,7 @@ class AssistantFnc(llm.FunctionContext):
                     # response from the function call is returned to the LLM
                     return f"The weather in {location} is {weather_data}."
                 else:
-                    raise f"Failed to get weather data, status code: {response.status}"
+                    return f"Failed to get weather data, status code: {response.status}"
 
 
 def prewarm_process(proc: JobProcess):
