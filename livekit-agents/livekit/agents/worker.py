af21edbd73ed6cade5fb6534461202f38a071e35
David Zhao <dz@livekit.io>
2024-11-13 01:16:23 -0800

    fix typecheck

--- livekit-agents/livekit/agents/worker.py
+++ livekit-agents/livekit/agents/worker.py
@@ -599,7 +599,10 @@ class Worker(utils.EventEmitter[EventTypes]):
             availability_resp.availability.participant_identity = args.identity
             availability_resp.availability.participant_name = args.name
             availability_resp.availability.participant_metadata = args.metadata
-            availability_resp.availability.participant_attributes = args.attributes
+            if args.attributes:
+                availability_resp.availability.participant_attributes.update(
+                    args.attributes
+                )
             await self._queue_msg(availability_resp)
 
             wait_assignment = asyncio.Future[agent.JobAssignment]()
