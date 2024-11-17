32f25afc7940250360249a9d386ea24d14785dc6
Julian Bright <brightsparc@gmail.com>
2024-11-17 14:14:06 +1100

    Update to set client_options explicitly

--- livekit-plugins/livekit-plugins-google/livekit/plugins/google/stt.py
+++ livekit-plugins/livekit-plugins-google/livekit/plugins/google/stt.py
@@ -129,21 +129,25 @@ class STT(stt.STT):
     def _ensure_client(self) -> SpeechAsyncClient:
         # Add support for passing a specific location that matches recognizer
         # see: https://cloud.google.com/speech-to-text/v2/docs/speech-to-text-supported-languages
-        args = {}
+        client_options = None
         if self._location != "global":
-            args["client_options"] = ClientOptions(
+            client_options = ClientOptions(
                 api_endpoint=f"{self._location}-speech.googleapis.com"
             )
         if self._credentials_info:
             self._client = SpeechAsyncClient.from_service_account_info(
-                self._credentials_info, **args
+                self._credentials_info,
+                client_options=client_options,
             )
         elif self._credentials_file:
             self._client = SpeechAsyncClient.from_service_account_file(
-                self._credentials_file, **args
+                self._credentials_file,
+                client_options=client_options,
             )
         else:
-            self._client = SpeechAsyncClient(**args)
+            self._client = SpeechAsyncClient(
+                client_options=client_options,
+            )
         assert self._client is not None
         return self._client
 
