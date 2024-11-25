fcc1e9742bc91f1bfd0f207fcb2b2b0ece80fbe3
jayesh <jayeshparmar9829@gmail.com>
2024-11-25 21:25:52 +0530

    updates

--- livekit-agents/livekit/agents/llm/fallback_adapter.py
+++ livekit-agents/livekit/agents/llm/fallback_adapter.py
@@ -119,7 +119,7 @@ class FallbackLLMStream(LLMStream):
                 temperature=self._temperature,
                 n=self._n,
                 parallel_tool_calls=self._parallel_tool_calls,
-                tool_choice=self._tool_choice,
+                tool_choice=self._tool_choice if self._tool_choice else None,
                 conn_options=dataclasses.replace(
                     self._conn_options,
                     max_retry=self._fallback_adapter._max_retry_per_llm,
