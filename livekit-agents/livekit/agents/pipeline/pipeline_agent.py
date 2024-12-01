6d471716edf6143d851f95db35ae2d35e33fefeb
narendra-bluebash <narendra.dangi@bluebash.co>
2024-12-01 19:01:02 +0530

    fix bug related to emit error msg

--- livekit-agents/livekit/agents/pipeline/pipeline_agent.py
+++ livekit-agents/livekit/agents/pipeline/pipeline_agent.py
@@ -56,6 +56,7 @@ EventTypes = Literal[
     "function_calls_collected",
     "function_calls_finished",
     "metrics_collected",
+    "error_message_collected",
 ]
 
 _CallContextVar = contextvars.ContextVar["AgentCallContext"](
@@ -929,6 +930,9 @@ class VoicePipelineAgent(utils.EventEmitter[EventTypes]):
                         continue
 
                     yield content
+
+            except Exception as e:
+                self.emit("error_message_collected", e)
             finally:
                 await stream.aclose()
 
