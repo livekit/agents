041e4a6b3944453a1dcaff99b61cc10e2c268dfc
jayesh <jayeshparmar9829@gmail.com>
2024-12-11 17:56:41 +0530

    send spaces with words

--- livekit-plugins/livekit-plugins-deepgram/livekit/plugins/deepgram/tts.py
+++ livekit-plugins/livekit-plugins-deepgram/livekit/plugins/deepgram/tts.py
@@ -245,7 +245,7 @@ class SynthesizeStream(tts.SynthesizeStream):
         async def _run_segments(ws: aiohttp.ClientWebSocketResponse):
             async for word_stream in self._segments_ch:
                 async for word in word_stream:
-                    speak_msg = {"type": "Speak", "text": word.token}
+                    speak_msg = {"type": "Speak", "text": f"{word.token} "}
                     await ws.send_str(json.dumps(speak_msg))
 
                 # Always flush after a segment
