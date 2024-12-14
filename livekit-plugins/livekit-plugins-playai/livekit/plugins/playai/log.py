be7310cf91fc195eed1ab2d0d926566b0009020d
David Zhao <dz@livekit.io>
2024-12-14 01:29:53 -0800

    fix

--- livekit-plugins/livekit-plugins-playai/livekit/plugins/playai/log.py
+++ livekit-plugins/livekit-plugins-playai/livekit/plugins/playai/log.py
@@ -1,4 +1,3 @@
 import logging
 
 logger = logging.getLogger("livekit.plugins.playai")
-
--- livekit-plugins/livekit-plugins-playai/livekit/plugins/playai/tts.py
+++ livekit-plugins/livekit-plugins-playai/livekit/plugins/playai/tts.py
@@ -1,9 +1,9 @@
 from __future__ import annotations
 
 import asyncio
+import logging
 import os
 from dataclasses import dataclass
-import logging
 
 from livekit import rtc
 from livekit.agents import (
