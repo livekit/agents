93794e2681a140fb4f5a97d76953ab5c09db3cbd
David Zhao <dz@livekit.io>
2024-12-17 12:12:21 -0800

    Revert "changeset"
    This reverts commit 20f0278b3ed40118a86717ef37cdb1e6d95bb4ed.

--- .changeset/old-humans-fly.md
+++
@@ -1,6 +0,0 @@
----
-"livekit-plugins-openai": patch
-"livekit-agents": patch
----
-
-update default realtime model to gpt-4o-realtime-preview-2024-12-17
--- livekit-agents/livekit/agents/utils/codecs/__init__.py
+++ livekit-agents/livekit/agents/utils/codecs/__init__.py
@@ -12,12 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from .decoder import AudioContainerStreamDecoder, AudioRawStreamDecoder, StreamBuffer
+from .decoder import AudioStreamDecoder, StreamBuffer
 from .mp3 import Mp3StreamDecoder
 
-__all__ = [
-    "Mp3StreamDecoder",
-    "AudioContainerStreamDecoder",
-    "AudioRawStreamDecoder",
-    "StreamBuffer",
-]
+__all__ = ["Mp3StreamDecoder", "AudioStreamDecoder", "StreamBuffer"]
