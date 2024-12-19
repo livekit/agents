49855e8932b5a2ee82ebf9f2436d9c6aad9e94bd
tinalenguyen <thetinalenguyen@gmail.com>
2024-12-18 22:10:42 -0500

    Ruff formatted and checked

--- examples/conversation_persistor.py
+++ examples/conversation_persistor.py
@@ -1,37 +1,41 @@
-from datetime import datetime
+import asyncio
+import logging
 from dataclasses import dataclass
+from datetime import datetime
+from typing import Union
 
 import aiofiles
-import asyncio
+from dotenv import load_dotenv
+from livekit.agents import (
+    AutoSubscribe,
+    JobContext,
+    WorkerOptions,
+    cli,
+    multimodal,
+    utils,
+)
+from livekit.agents.llm import ChatMessage
+from livekit.agents.multimodal.multimodal_agent import EventTypes
+from livekit.plugins import openai
 
-from typing import Union
 
 @dataclass
 class EventLog:
-    eventname: str | None 
+    eventname: str | None
     """name of recorded event"""
-    time: str = datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
+    time: str = datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")[:-3]
     """time the event is recorded"""
+
 
 @dataclass
 class TranscriptionLog:
-    role: str | None    
+    role: str | None
     """role of the speaker"""
     transcription: str | None
     """transcription of speech"""
-    time: str = datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
+    time: str = datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")[:-3]
     """time the event is recorded"""
 
-from livekit.agents import (
-    multimodal, 
-    utils,
-    JobContext,
-    AutoSubscribe,
-    WorkerOptions,
-    cli
-)
-from livekit.agents.multimodal.multimodal_agent import EventTypes
-from livekit.agents.llm import ChatMessage
 
 class ConversationPersistor(utils.EventEmitter[EventTypes]):
     def __init__(
@@ -39,7 +43,7 @@ class ConversationPersistor(utils.EventEmitter[EventTypes]):
         *,
         model: multimodal.MultimodalAgent | None,
         log: str | None,
-        transcriptions_only: bool = False
+        transcriptions_only: bool = False,
     ):
         """
         Initializes a ConversationPersistor instance which records the events and transcriptions of a MultimodalAgent.
@@ -52,7 +56,7 @@ class ConversationPersistor(utils.EventEmitter[EventTypes]):
             agent_transcriptions (arr): list of agent transcriptions
             events (arr): list of all events
             log_q (asyncio.Queue): a queue of EventLog and TranscriptionLog
-    
+
         """
         super().__init__()
 
@@ -65,36 +69,36 @@ class ConversationPersistor(utils.EventEmitter[EventTypes]):
         self._events = []
 
         self._log_q = asyncio.Queue[Union[EventLog, TranscriptionLog, None]]()
-        
+
         self._main_task = asyncio.create_task(self._main_atask())
         self.main_task()
 
     @property
     def log(self) -> str | None:
-        return self._log 
+        return self._log
 
     @property
     def model(self) -> multimodal.MultimodalAgent | None:
-        return self._model 
-    
+        return self._model
+
     @property
     def user_transcriptions(self) -> dict:
         return self._user_transcriptions
-    
+
     @property
     def agent_transcriptions(self) -> dict:
         return self._agent_transcriptions
-    
+
     @property
     def events(self) -> dict:
         return self._events
 
     @log.setter
     def log(self, newlog: str | None) -> None:
-        self._log = newlog 
-    
+        self._log = newlog
+
     async def _main_atask(self) -> None:
-    # Writes to file asynchronously 
+        # Writes to file asynchronously
         while True:
             log = await self._log_q.get()
 
@@ -102,25 +106,27 @@ class ConversationPersistor(utils.EventEmitter[EventTypes]):
                 break
 
             async with aiofiles.open(self._log, "a") as file:
-                if type(log) == EventLog and not self._transcriptions_only:
+                if type(log) is EventLog and not self._transcriptions_only:
                     self._events.append(log)
                     await file.write("\n" + log.time + " " + log.eventname)
-                    
-                if type(log) == TranscriptionLog:
+
+                if type(log) is TranscriptionLog:
                     if log.role == "user":
                         self._user_transcriptions.append(log)
                     else:
                         self._agent_transcriptions.append(log)
 
-                    await file.write("\n" + log.time + " " + log.role + " " + log.transcription)
+                    await file.write(
+                        "\n" + log.time + " " + log.role + " " + log.transcription
+                    )
 
     async def aclose(self) -> None:
-    # Exits 
+        # Exits
         self._log_q.put_nowait(None)
         await self._main_task
 
     def main_task(self) -> None:
-    # Listens for emitted MultimodalAgent events
+        # Listens for emitted MultimodalAgent events
         @self._model.on("user_started_speaking")
         def _user_started_speaking():
             event = EventLog(eventname="user_started_speaking")
@@ -130,15 +136,18 @@ class ConversationPersistor(utils.EventEmitter[EventTypes]):
         def _user_stopped_speaking():
             event = EventLog(eventname="user_stopped_speaking")
             self._log_q.put_nowait(event)
-        
+
         @self._model.on("agent_started_speaking")
         def _agent_started_speaking():
             event = EventLog(eventname="agent_started_speaking")
             self._log_q.put_nowait(event)
-        
+
         @self._model.on("agent_stopped_speaking")
         def _agent_stopped_speaking():
-            transcription = TranscriptionLog(role="agent", transcription=(self._model._playing_handle._tr_fwd.played_text)[1:])
+            transcription = TranscriptionLog(
+                role="agent",
+                transcription=(self._model._playing_handle._tr_fwd.played_text)[1:],
+            )
             self._log_q.put_nowait(transcription)
 
             event = EventLog(eventname="agent_stopped_speaking")
@@ -146,7 +155,9 @@ class ConversationPersistor(utils.EventEmitter[EventTypes]):
 
         @self._model.on("user_speech_committed")
         def _user_speech_committed(user_msg: ChatMessage):
-            transcription = TranscriptionLog(role="user", transcription=user_msg.content)
+            transcription = TranscriptionLog(
+                role="user", transcription=user_msg.content
+            )
             self._log_q.put_nowait(transcription)
 
             event = EventLog(eventname="user_speech_committed")
@@ -172,11 +183,6 @@ class ConversationPersistor(utils.EventEmitter[EventTypes]):
             event = EventLog(eventname="function_calls_finished")
             self._log_q.put_nowait(event)
 
-
-from livekit.plugins import openai
-from dotenv import load_dotenv
-import logging
-
 load_dotenv()
 
 logger = logging.getLogger("my-worker")
@@ -185,21 +191,22 @@ logger.setLevel(logging.INFO)
 
 async def entrypoint(ctx: JobContext):
     agent = multimodal.MultimodalAgent(
-            model=openai.realtime.RealtimeModel(
-                voice="alloy",
-                temperature=0.8,
-                instructions="You are a helpful assistant.",
-                turn_detection=openai.realtime.ServerVadOptions(
-                    threshold=0.6, prefix_padding_ms=200, silence_duration_ms=500
-                ),
+        model=openai.realtime.RealtimeModel(
+            voice="alloy",
+            temperature=0.8,
+            instructions="You are a helpful assistant.",
+            turn_detection=openai.realtime.ServerVadOptions(
+                threshold=0.6, prefix_padding_ms=200, silence_duration_ms=500
             ),
-        )
+        ),
+    )
 
     cp = ConversationPersistor(model=agent, log="log.txt")
 
     await ctx.connect(auto_subscribe=AutoSubscribe.AUDIO_ONLY)
     participant = await ctx.wait_for_participant()
     agent.start(ctx.room, participant)
+
 
 if __name__ == "__main__":
     cli.run_app(WorkerOptions(entrypoint_fnc=entrypoint))
