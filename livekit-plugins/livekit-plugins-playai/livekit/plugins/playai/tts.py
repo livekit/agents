be2af05399cba4a65d783aa324e626ba73ba60f8
jayesh <jayeshparmar9829@gmail.com>
2024-12-23 16:52:14 +0530

    updates

--- livekit-plugins/livekit-plugins-playai/livekit/plugins/playai/tts.py
+++ livekit-plugins/livekit-plugins-playai/livekit/plugins/playai/tts.py
@@ -1,9 +1,8 @@
 from __future__ import annotations
 
 import asyncio
-import logging
 import os
-from dataclasses import dataclass
+from dataclasses import dataclass, fields
 
 from livekit import rtc
 from livekit.agents import (
@@ -17,23 +16,17 @@ from livekit.agents import (
 from pyht import AsyncClient as PlayHTAsyncClient  # type: ignore
 from pyht.client import Format, Language, TTSOptions  # type: ignore
 
-from .models import TTSModel
+from .log import logger
+from .models import TTSModel, format_mapping
+
+NUM_CHANNELS = 1
 
 
 @dataclass
-class _TTSOptions:
-    voice: str
-    format: Format
-    sample_rate: int
+class _Options:
     model: TTSModel | str
-    speed: float | None
-    language: Language
-    temperature: float | None
-    top_p: float | None
-    text_guidance: float | None
-    voice_guidance: float | None
-    style_guidance: float | None
-    repetition_penalty: float | None
+    tts_options: TTSOptions
+    word_tokenizer: tokenize.WordTokenizer
 
 
 class TTS(tts.TTS):
@@ -42,72 +35,63 @@ class TTS(tts.TTS):
         *,
         api_key: str | None = None,
         user_id: str | None = None,
-        voice: str = "s3://voice-cloning-zero-shot/775ae416-49bb-4fb6-bd45-740f205d20a1/jennifersaad/manifest.json",
+        voice: str = "s3://voice-cloning-zero-shot/d9ff78ba-d016-47f6-b0ef-dd630f59414e/female-cs/manifest.json",
         language: str = "english",
         sample_rate: int = 24000,
-        speed: float = 1.0,
-        model: TTSModel | str = "Play3.0-mini",
-        temperature: float | None = None,
-        top_p: float | None = None,
-        text_guidance: float | None = None,
-        voice_guidance: float | None = None,
-        style_guidance: float | None = None,
-        repetition_penalty: float | None = None,
+        model: TTSModel | str = "Play3.0-mini-ws",
+        word_tokenizer: tokenize.WordTokenizer = tokenize.basic.WordTokenizer(
+            ignore_punctuation=False
+        ),
+        **kwargs,
     ) -> None:
         """
         Initialize the PlayAI TTS engine.
 
         Args:
-            api_key (str): The PlayAI API key. Can be set via environment variable PLAYAI_API_KEY.
-            user_id (str): The PlayAI user ID. Can be set via environment variable PLAYAI_USER_ID.
-            voice (str): A URL pointing to a Play voice manifest file. (e.g. "s3://voice-cloning-zero-shot/775ae416-49bb-4fb6-bd45-740f205d20a1/jennifersaad/manifest.json").
-            language (str): The language of the text. Default is 'english'.
-            sample_rate (int): The sample rate in Hz. Options are 8000, 16000, 24000, 44100, 48000.
-            speed (float): The speed of the audio. Default is 1.0.
-            model (str): The voice engine to use. Default is "Play3.0-mini".
-            > The following options are inference-time hyperparameters of the text-to-speech model; if unset, the model will use default values chosen by PlayAI.
-            temperature (float): The temperature of the model.
-            top_p (float): The top-p value of the model.
-            text_guidance (float): The text guidance of the model.
-            voice_guidance (float): The voice guidance of the model.
-            style_guidance (float): (Play3.0-mini-http and Play3.0-mini-ws only) The style guidance of the model.
-            repetition_penalty (float): The repetition penalty of the model.
+            api_key (str): PlayAI API key.
+            user_id (str): PlayAI user ID.
+            voice (str): Voice manifest URL.
+            model (str): TTS model.
+            language (str): language.
+            sample_rate (int): sample rate (Hz), A number greater than or equal to 8000, and must be less than or equal to 48000
+            word_tokenizer (tokenize.WordTokenizer): Tokenizer for processing text. Defaults to basic WordTokenizer.
+            **kwargs: Additional options.
         """
+
         super().__init__(
             capabilities=tts.TTSCapabilities(
-                streaming=True,
+                streaming=False,
             ),
             sample_rate=sample_rate,
             num_channels=1,
         )
 
-        api_key = api_key or os.environ.get("PLAYAI_API_KEY")
-        user_id = user_id or os.environ.get("PLAYAI_USER_ID")
+        api_key = api_key or os.environ.get("PLAYHT_API_KEY")
+        user_id = user_id or os.environ.get("PLAYHT_USER_ID")
 
         if not api_key or not user_id:
             raise ValueError(
-                "PlayAI API key and user ID are required, either as arguments or set PLAYAI_API_KEY and PLAYAI_USER_ID environment variables"
+                "PlayHT API key and user ID are required. Set environment variables PLAYHT_API_KEY and PLAYHT_USER_ID or pass them explicitly."
             )
+        _validate_kwargs(kwargs)
+        self._config = TTSOptions(
+            voice=voice,
+            format=format_mapping.get(format, Format.FORMAT_MP3),
+            sample_rate=sample_rate,
+            language=Language(language),
+            **kwargs,
+        )
 
+        self._opts = _Options(
+            model=model,
+            tts_options=self._config,
+            word_tokenizer=word_tokenizer,
+        )
+
+        # Initialize client
         self._client = PlayHTAsyncClient(
             user_id=user_id,
             api_key=api_key,
-        )
-        # suppress verbose websocket logs
-        logging.getLogger("websockets.client").setLevel(logging.INFO)
-        self._opts = _TTSOptions(
-            voice=voice,
-            model=model,
-            format=Format.FORMAT_MP3,  # default for now
-            sample_rate=sample_rate,
-            speed=speed,
-            language=Language(language),
-            temperature=temperature,
-            top_p=top_p,
-            text_guidance=text_guidance,
-            voice_guidance=voice_guidance,
-            style_guidance=style_guidance,
-            repetition_penalty=repetition_penalty,
         )
 
     def update_options(
@@ -116,14 +100,7 @@ class TTS(tts.TTS):
         voice: str | None = None,
         model: TTSModel | str | None = None,
         language: str | None = None,
-        sample_rate: int | None = None,
-        speed: float | None = None,
-        temperature: float | None = None,
-        top_p: float | None = None,
-        text_guidance: float | None = None,
-        voice_guidance: float | None = None,
-        style_guidance: float | None = None,
-        repetition_penalty: float | None = None,
+        **kwargs,
     ) -> None:
         """
         Update the TTS options.
@@ -132,31 +109,18 @@ class TTS(tts.TTS):
             voice (str, optional): The voice to use.
             model (str, optional): The model to use.
             language (str, optional): The language of the text.
-            sample_rate (int, optional): The sample rate of the audio.
-            speed (float, optional): The speed of the audio.
-            temperature (float, optional): The temperature of the model.
-            top_p (float, optional): The top-p value of the model.
-            text_guidance (float, optional): The text guidance of the model.
-            voice_guidance (float, optional): The voice guidance of the model.
-            style_guidance (float, optional): The style guidance of the model.
-            repetition_penalty (float, optional): The repetition penalty of the model.
+            **kwargs: Additional keyword arguments to update the options.
         """
+        _validate_kwargs(kwargs)
         updates = {
             "voice": voice,
             "model": model,
             "language": Language(language) if language else None,
-            "sample_rate": sample_rate,
-            "speed": speed,
-            "temperature": temperature,
-            "top_p": top_p,
-            "text_guidance": text_guidance,
-            "voice_guidance": voice_guidance,
-            "style_guidance": style_guidance,
-            "repetition_penalty": repetition_penalty,
         }
+        updates.update({k: v for k, v in kwargs.items()})
         for k, v in updates.items():
             if v is not None:
-                setattr(self._opts, k, v)
+                setattr(self._config, k, v)
 
     def synthesize(
         self,
@@ -188,34 +152,25 @@ class ChunkedStream(tts.ChunkedStream):
         tts: TTS,
         input_text: str,
         conn_options: APIConnectOptions,
-        opts: _TTSOptions,
+        opts: _Options,
     ) -> None:
         super().__init__(tts=tts, input_text=input_text, conn_options=conn_options)
         self._client = tts._client
         self._opts = opts
+        self._config = self._opts.tts_options
         self._mp3_decoder = utils.codecs.Mp3StreamDecoder()
 
     async def _run(self) -> None:
         request_id = utils.shortuuid()
         bstream = utils.audio.AudioByteStream(
-            sample_rate=self._opts.sample_rate, num_channels=1
+            sample_rate=self._config.sample_rate, num_channels=NUM_CHANNELS
         )
-        tts_options = TTSOptions(
-            voice=self._opts.voice,
-            format=self._opts.format,
-            sample_rate=self._opts.sample_rate,
-            speed=self._opts.speed,
-            language=self._opts.language,
-        )
-
-        # only use websocket streaming
-        voice_engine = self._opts.model + "-ws"
 
         try:
             async for chunk in self._client.tts(
                 text=self._input_text,
-                options=tts_options,
-                voice_engine=voice_engine,
+                options=self._config,
+                voice_engine=self._opts.model,
                 streaming=True,
             ):
                 for frame in self._mp3_decoder.decode_chunk(chunk):
@@ -240,74 +195,89 @@ class SynthesizeStream(tts.SynthesizeStream):
         *,
         tts: TTS,
         conn_options: APIConnectOptions,
-        opts: _TTSOptions,
+        opts: _Options,
     ):
         super().__init__(tts=tts, conn_options=conn_options)
         self._client = tts._client
         self._opts = opts
+        self._config = self._opts.tts_options
+        self._segments_ch = utils.aio.Chan[tokenize.WordStream]()
         self._mp3_decoder = utils.codecs.Mp3StreamDecoder()
-        self._sent_tokenizer_stream = tokenize.basic.SentenceTokenizer(
-            min_sentence_len=8
-        ).stream()
 
     async def _run(self) -> None:
         request_id = utils.shortuuid()
+        segment_id = utils.shortuuid()
         bstream = utils.audio.AudioByteStream(
-            sample_rate=self._opts.sample_rate,
-            num_channels=1,
+            sample_rate=self._config.sample_rate,
+            num_channels=NUM_CHANNELS,
         )
-        tts_options = TTSOptions(
-            voice=self._opts.voice,
-            format=self._opts.format,
-            sample_rate=self._opts.sample_rate,
-            speed=self._opts.speed,
-            language=self._opts.language,
-        )
+        last_frame: rtc.AudioFrame | None = None
 
-        input_task = asyncio.create_task(self._handle_input())
+        def _send_last_frame(*, segment_id: str, is_final: bool) -> None:
+            nonlocal last_frame
+            if last_frame is not None:
+                self._event_ch.send_nowait(
+                    tts.SynthesizedAudio(
+                        request_id=request_id,
+                        segment_id=segment_id,
+                        frame=last_frame,
+                        is_final=is_final,
+                    )
+                )
+                last_frame = None
+
+        input_task = asyncio.create_task(self._tokenize_input())
         try:
             text_stream = await self._create_text_stream()
             async for chunk in self._client.stream_tts_input(
                 text_stream=text_stream,
-                options=tts_options,
-                voice_engine=self._opts.model + "-ws",
+                options=self._config,
+                voice_engine=self._opts.model,
             ):
                 for frame in self._mp3_decoder.decode_chunk(chunk):
                     for frame in bstream.write(frame.data.tobytes()):
-                        self._send_frame(request_id, frame)
+                        _send_last_frame(segment_id=segment_id, is_final=False)
                         last_frame = frame
 
             for frame in bstream.flush():
-                self._send_frame(request_id, frame)
+                _send_last_frame(segment_id=segment_id, is_final=False)
                 last_frame = frame
-            self._send_frame(request_id, last_frame, is_final=True)
+            _send_last_frame(segment_id=segment_id, is_final=True)
         except Exception as e:
             raise APIConnectionError() from e
         finally:
             await utils.aio.gracefully_cancel(input_task)
 
-    async def _handle_input(self):
-        async for data in self._input_ch:
-            if isinstance(data, self._FlushSentinel):
-                self._sent_tokenizer_stream.flush()
-                continue
-            self._sent_tokenizer_stream.push_text(data)
-        self._sent_tokenizer_stream.end_input()
+    @utils.log_exceptions(logger=logger)
+    async def _tokenize_input(self):
+        # Converts incoming text into WordStreams and sends them into _segments_ch
+        word_stream = None
+        async for input in self._input_ch:
+            if isinstance(input, str):
+                if word_stream is None:
+                    word_stream = self._opts.word_tokenizer.stream()
+                    self._segments_ch.send_nowait(word_stream)
+                word_stream.push_text(input)
+            elif isinstance(input, self._FlushSentinel):
+                if word_stream:
+                    word_stream.end_input()
+                word_stream = None
+        self._segments_ch.close()
 
+    @utils.log_exceptions(logger=logger)
     async def _create_text_stream(self):
         async def text_stream():
-            async for data in self._sent_tokenizer_stream:
-                yield data.token
+            async for word_stream in self._segments_ch:
+                async for word in word_stream:
+                    yield word.token
 
         return text_stream()
 
-    def _send_frame(
-        self, request_id: str, frame: rtc.AudioFrame, is_final: bool = False
-    ) -> None:
-        self._event_ch.send_nowait(
-            tts.SynthesizedAudio(
-                request_id=request_id,
-                frame=frame,
-                is_final=is_final,
-            )
+
+def _validate_kwargs(kwargs: dict) -> None:
+    valid_keys = {field.name for field in fields(TTSOptions)}
+    invalid_keys = set(kwargs.keys()) - valid_keys
+    if invalid_keys:
+        raise ValueError(
+            f"Invalid parameters: {invalid_keys}. Allowed parameters: {valid_keys}"
         )
