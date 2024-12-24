b216d22a8d5820378e7ebf86bc48ab166f33d556
jayesh <jayeshparmar9829@gmail.com>
2024-12-25 01:09:09 +0530

    updates

--- livekit-plugins/livekit-plugins-playai/livekit/plugins/playai/models.py
+++ livekit-plugins/livekit-plugins-playai/livekit/plugins/playai/models.py
@@ -3,13 +3,7 @@ from typing import Literal
 from pyht.client import Format  # type: ignore
 
 TTSModel = Literal["Play3.0-mini-ws", "PlayDialog-ws", "Play3.0-mini", "PlayDialog"]
-FORMAT = Literal["raw", "mp3", "wav", "ogg", "flac", "mulaw", "pcm"]
+FORMAT = Literal["mp3"]
 format_mapping = {
-    "raw": Format.FORMAT_RAW,
     "mp3": Format.FORMAT_MP3,
-    "wav": Format.FORMAT_WAV,
-    "ogg": Format.FORMAT_OGG,
-    "flac": Format.FORMAT_FLAC,
-    "mulaw": Format.FORMAT_MULAW,
-    "pcm": Format.FORMAT_PCM,
 }
