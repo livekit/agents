06eee67cefad6f7639f07d4cff768611d247a28e
Martin Kess <martin@purplefish.com>
2024-12-26 10:57:06 -0500

    Add event for partial transcript

--- livekit-agents/livekit/agents/pipeline/pipeline_agent.py
+++ livekit-agents/livekit/agents/pipeline/pipeline_agent.py
@@ -48,6 +48,7 @@ BeforeTTSCallback = Callable[
 
 EventTypes = Literal[
     "user_started_speaking",
+    "user_interim_transcript",
     "user_stopped_speaking",
     "agent_started_speaking",
     "agent_stopped_speaking",
@@ -415,6 +416,7 @@ class VoicePipelineAgent(utils.EventEmitter[EventTypes]):
         Args:
             event: the event to listen to (see EventTypes)
                 - user_started_speaking: the user started speaking
+                - user_partial_transcript: the user transcript was updated
                 - user_stopped_speaking: the user stopped speaking
                 - agent_started_speaking: the agent started speaking
                 - agent_stopped_speaking: the agent stopped speaking
@@ -578,6 +580,7 @@ class VoicePipelineAgent(utils.EventEmitter[EventTypes]):
             self._deferred_validation.on_human_end_of_speech(ev)
 
         def _on_interim_transcript(ev: stt.SpeechEvent) -> None:
+            self.emit("user_interim_transcript", ev.alternatives[0].text)
             self._transcribed_interim_text = ev.alternatives[0].text
 
         def _on_final_transcript(ev: stt.SpeechEvent) -> None:
