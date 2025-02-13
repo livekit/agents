c0d8dc65b9a12ffbc7bbfe5ec62a6e5ab26c5534
Long Chen <longch1024@gmail.com>
2025-02-13 10:24:25 +0800

    refactor transcription sync to be decoupled with agent (#1483)

--- livekit-agents/livekit/agents/transcription/__init__.py
+++ livekit-agents/livekit/agents/transcription/__init__.py
@@ -1,21 +1,8 @@
-from .forwarder import (
-    TranscriptionDataStreamForwarder,
-    TranscriptionForwarder,
-    TranscriptionRoomForwarder,
-    TranscriptionStreamForwarder,
-)
-from .synchronizer import (
-    TranscriptionSyncIO,
-    TranscriptionSyncOptions,
-    TranscriptSegment,
-)
+from ._utils import find_micro_track_id
+from .synchronizer import TextSynchronizer, TextSyncOptions
 
 __all__ = [
-    "TranscriptionSyncIO",
-    "TranscriptionSyncOptions",
-    "TranscriptionForwarder",
-    "TranscriptionRoomForwarder",
-    "TranscriptionStreamForwarder",
-    "TranscriptionDataStreamForwarder",
-    "TranscriptSegment",
+    "TextSynchronizer",
+    "TextSyncOptions",
+    "find_micro_track_id",
 ]
