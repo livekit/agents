c2ac0bd9ff07cd61feabaebb3ab64b651c6013b6
tinalenguyen <thetinalenguyen@gmail.com>
2025-02-24 01:51:17 -0500

    integrating cal api, wip

--- examples/dentist_scheduler.py
+++ examples/dentist_scheduler.py
@@ -2,6 +2,7 @@ import asyncio
 import logging
 import datetime
 import aiohttp
+import os
 from enum import Enum
 from dataclasses import dataclass
 
@@ -23,114 +24,163 @@ from livekit.plugins import openai
 
 @dataclass
 class UserInfo:
-    name: str
+    name: str = ""
     email: NotGivenOr[str] = NOT_GIVEN
     phone: NotGivenOr[str] = NOT_GIVEN
     message: NotGivenOr[str] = NOT_GIVEN
 
 
-class ServicesOffered(Enum):
-    MESSENGER = "messenger"
-    SCHEDULER = "scheduler"
+class APIRequests(Enum):
+    GET_APPTS = "get_appts"
+    GET_APPT = "get_appt"
+    CANCEL = "cancel"
+    RESCHEDULE = "reschedule"
+    SCHEDULE = "schedule"
 
 
 class Receptionist(AgentTask):
-    def __init__(self, userdata: NotGivenOr[UserInfo] = NOT_GIVEN) -> None:
+    def __init__(self) -> None:
         super().__init__(
             instructions="""You are Alloy, a receptionist at the LiveKit Dental Office scheduling dentist appointments for users. Always speak in English.
                             The office is closed on Sundays but operates from 10AM to 12PM and 1PM to 4PM PT otherwise. The services offered are routine checkups and tooth extractions.
-                            The location of the office is 123 LiveKit Lane, California.""",
+                            The location of the office is 123 LiveKit Lane, California. When managing appointments or taking a message, first ask for the user's name then redirect them to the right agent.""",
             llm=openai.realtime.RealtimeModel(voice="alloy"),
         )
-        self.agent.userdata = userdata or None
-        self._userdata_event = asyncio.Event()
 
+    # will be adding a TTS() for variety
     async def on_enter(self) -> None:
         speech_handle = await self.agent.generate_reply(
             instructions="Welcome the user to the LiveKit Dental Office and ask how you can assist."
         )
 
-    async def on_exit(self) -> None:
-        self.agent.say(
-            "Thank you for calling the LiveKit Dental Office, have a wonderful day!"
-        )
-
     @ai_function()
-    async def servicer(self, service_requested: ServicesOffered):
-        """Performs a service for the user, either managing appointments or leaving a message
+    async def appointment(self, service: str):
+        """This function allows for users to schedule, reschedule, or cancel an appointment.
 
         Args:
-            service_requested (ServicesOffered): either "messenger", which allows the user to leave a message, or "scheduler",
-            which allows the user to schedule a new appointment and reschedule or cancel an existing appointment.
+            service (str): Either "schedule", "reschedule", or "cancel"
         """
-        if self.agent.userdata is None:
-            self.agent.say("What is your name and contact information?")
-            await self._userdata_event.wait()
-
-        if service_requested.value == "messenger":
-            return Messenger(), "I'll be transferring you to Shimmer."
-
-        if service_requested.value == "scheduler":
-            # perhaps would be better to also pass schedule/reschedule/cancel to prevent repetitiveness
-            return Scheduler(), "I'll be transferring you to Echo."
+        return Scheduler(service=service), "I'll be transferring you to Echo."
 
     @ai_function()
-    async def get_userdata(
-        self, name: str | None, phone: str | None, email: str | None
-    ) -> None:
-        """Records user data.
+    async def take_message(self):
+        """This function allows for users to leave a message for the office."""
+        return Messenger(), "I'll be transferring you to Shimmer."
+
+    @ai_function()
+    async def get_name(self, name: str | None) -> None:
+        """Records the user's name
 
         Args:
             name (str | None): User's name
-            phone (str | None): User's phone number
-            email (str | None): User's email
         """
-        # to do: confirm that details are correct
-        if name and self.agent.userdata is None:
-            self.agent.userdata = UserInfo(name=name)
-        else:
-            self.agent.userdata.name = name
-        if phone and self.agent.userdata is not None:
-            self.agent.userdata.phone = phone
-        if email and self.agent.userdata is not None:
-            self.agent.userdata.email = email
-        else:
-            self.agent.say("Sorry, could you please repeat your name and contact info?")
-
-        self._userdata_event.set()
+        self.agent.userdata.name = name
+        speech_handle = await self.agent.generate_reply(instructions=f"Thanks {name}!")
 
 
 class Scheduler(AgentTask):
-    def __init__(self) -> None:
+    def __init__(self, *, service: str) -> None:
         super().__init__(
             instructions="You are Echo, a scheduler managing appointments for the LiveKit dental office.",
             llm=openai.realtime.RealtimeModel(voice="echo"),
         )
-        self._payload_queue = asyncio.Queue(dict)
+        self._service_requested = service
 
     async def on_enter(self) -> None:
         speech_handle = await self.agent.generate_reply(
-            instructions="Introduce yourself and ask the user if they'd like to schedule a new appointment, reschedule, or cancel an existing appointment."
+            instructions=f"Introduce yourself and ask {self.agent.userdata.name} to confirm that they would like to {self._service_requested} an appointment. If they are scheduling a new appointment, ask for their email."
         )
 
-    async def send_requests(self) -> None: ...
+    async def send_request(
+        self, *, request: APIRequests, uid: str = "", time: str = "", note: str = ""
+    ) -> dict:
+        headers = {
+            "cal-api-version": "2024-08-13",
+            "Authorization": "Bearer " + os.getenv("CAL_API_KEY"),
+        }
+        async with aiohttp.ClientSession() as session:
+            data = {}
+            if request.value == "get_appts":
+                payload = {"attendeeName": self.agent.userdata.name}
+                async with session.post(
+                    "https://api.cal.com/v2/bookings", params=payload, headers=headers
+                ) as response:
+                    data = await response.json()
 
-    # continuously sends requests from queue?
+            if request.value == "cancel":
+                async with session.post(
+                    "https://api.cal.com/v2/bookings/{uid}/cancel", headers=headers
+                ) as response:
+                    data = await response.json()
 
-    @ai_function
-    async def schedule(self) -> None:
-        """Schedules a new appointment"""
-        ...
+            if request.value == "schedule":
+                attendee_details = {
+                    "name": self.agent.userdata.name,
+                    "email": self.agent.userdata.email,
+                    "timeZone": "America/Los_Angeles",
+                }
+                payload = {
+                    "start": time,
+                    "eventTypeId": 1,
+                    "attendee": attendee_details,
+                    "bookingFieldsResponses": {"notes": note},
+                }
+                async with session.post(
+                    "https://api.cal.com/v2/bookings", json=payload, headers=headers
+                ) as response:
+                    data = await response.json()
 
-    @ai_function
+            else:
+                raise Exception("API Request Failed")
+            return data
+
+    @ai_function()
+    async def schedule(self, description: str, date: str) -> None:
+        """Schedules a new appointment for users.
+        Args:
+            description (str): Reason for scheduling appointment, either "routine checkup" or "tooth extraction"
+            date (str): Date and time for the appointment in ISO 8601 format in UTC timezone.
+        """
+        response = await self.send_request(
+            request=APIRequests.SCHEDULE, time=date, note=description
+        )
+        if response.status == "success":
+            sp = await self.agent.generate_reply(
+                instructions="Tell the user you were able to schedule the appointment successfully."
+            )
+
+    @ai_function()
     async def cancel(self) -> None:
         """Cancels an existing appointment"""
-        ...
+        response = await self.send_request(request=APIRequests.GET_APPTS)
+        if response.data:
+            sp = await self.agent.generate_reply(
+                f"Canceling the appointment found on {response.data.start} for {response.data.title}."
+            )
+            confirmation = await self.send_request(
+                request=APIRequests.CANCEL, uid=response.data.uid
+            )
+            if confirmation.status == "success":
+                return Receptionist(), "You're all set, transferring you back to Alloy."
+        else:
+            sp = await self.agent.generate_reply(
+                "There are no appointments under your name, would you like to schedule one?"
+            )
 
-    @ai_function
+    @ai_function()
     async def reschedule(self) -> None:
         """Reschedules an existing appointment"""
         ...
+        # check for existing appt -> confirm correct appt -> adjust time -> schedule new, cancel old
+
+    @ai_function()
+    async def get_email(self, email: str) -> None:
+        """Records the user's email.
+
+        Args:
+            email (str): The user's email
+        """
+        self.agent.userdata.email = email
 
 
 class Messenger(AgentTask):
@@ -155,11 +205,12 @@ logger.setLevel(logging.INFO)
 
 
 async def entrypoint(ctx: JobContext):
-    agent = PipelineAgent(task=Receptionist(), llm=openai.realtime.RealtimeModel())
+    agent = PipelineAgent(
+        task=Receptionist(), userdata=UserInfo(), llm=openai.realtime.RealtimeModel()
+    )
 
     await ctx.connect(auto_subscribe=AutoSubscribe.AUDIO_ONLY)
-    participant = await ctx.wait_for_participant()
-    agent.start(ctx.room, participant)
+    await agent.start(room=ctx.room)
 
 
 if __name__ == "__main__":
