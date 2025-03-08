da644eb7b8f67818382bcd0caa0aec79d94502c9
Tina Nguyen <72938484+tinalenguyen@users.noreply.github.com>
2025-03-08 01:46:00 -0500

    Update __init__.py

--- examples/dentist-scheduler/tasks/__init__.py
+++ examples/dentist-scheduler/tasks/__init__.py
@@ -2,4 +2,8 @@ from .receptionist_task import Receptionist
 from .scheduler_task import Scheduler
 from .messenger_task import Messenger
 
-all = ["Receptionist", "Scheduler", "Messenger"]
+__all__ = [
+  "Receptionist",
+  "Scheduler",
+  "Messenger"
+]
