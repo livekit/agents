80d398209f2955a14fdf4ee27791d36fda22afe6
Ritesh Aluka <riteshaluka@Riteshs-Work-MacBook.local>
2025-03-08 20:35:32 -0800

    cartesia flag for latency calcs

--- livekit-plugins/livekit-plugins-cartesia/livekit/plugins/cartesia/tts.py
+++ livekit-plugins/livekit-plugins-cartesia/livekit/plugins/cartesia/tts.py
@@ -18,11 +18,13 @@ import asyncio
 import base64
 import json
 import os
+import time
 import weakref
 from dataclasses import dataclass
 from typing import Any, Optional
 
 import aiohttp
+from app_config import AppConfig
 from livekit.agents import (
     APIConnectionError,
     APIConnectOptions,
@@ -297,6 +299,14 @@ class SynthesizeStream(tts.SynthesizeStream):
         request_id = utils.shortuuid()
 
         async def _sentence_stream_task(ws: aiohttp.ClientWebSocketResponse):
+            if (
+                not AppConfig()
+                .get_call_metadata()
+                .get("first_sentence_synthesis_start_time")
+            ):
+                AppConfig().get_call_metadata().update(
+                    {"first_sentence_synthesis_start_time": time.time()}
+                )
             base_pkt = _to_cartesia_options(self._opts)
             async for ev in self._sent_tokenizer_stream:
                 token_pkt = base_pkt.copy()
