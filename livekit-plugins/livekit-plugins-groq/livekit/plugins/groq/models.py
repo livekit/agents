a3cb47c11fd912f609cfad32d1fdcef8b946ef7d
David Zhao <dz@livekit.io>
2025-03-17 12:52:05 -0800

    Groq plugin

---
+++ livekit-plugins/livekit-plugins-groq/livekit/plugins/groq/models.py
@@ -0,0 +1,38 @@
+from typing import Literal
+
+SpeechModels = Literal[
+    "playai-tts",
+    "playai-tts-arabic",
+]
+
+SpeechVoices = Literal[
+    "Aaliyah-PlayAI",
+    "Abigail-PlayAI",
+    "Angelo-PlayAI",
+    "Arthur-PlayAI",
+    "Aurora-PlayAI",
+    "Autumn-PlayAI",
+    "Ayla-Meditation-PlayAI",
+    "Ayla-Advertising-PlayAI",
+    "Bryan-PlayAI",
+    "Chuck-PlayAI",
+    "Darrell-PlayAI",
+    "Dexter-PlayAI",
+    "Donovan-PlayAI",
+    "Eileen-PlayAI",
+    "Eleanor-PlayAI",
+    "Erasmo-PlayAI",
+    "Hudson-PlayAI",
+    "Inara-PlayAI",
+    "Luna-PlayAI",
+    "Nia-PlayAI",
+    "Phoebe-PlayAI",
+    "Ranger-PlayAI",
+    "Sophia-PlayAI",
+    "Waylon-PlayAI",
+    "William-Training-PlayAI",
+    "Nasser-PlayAI",
+    "Khalid-PlayAI",
+    "Amira-PlayAI",
+    "Ahmad-PlayAI",
+]
