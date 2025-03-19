22595343839fb13c651ea35d584d5df2e275d508
smarakp <smarak@trysalient.com>
2025-03-19 09:30:31 -0700

    Adding in better understanding of when to interrupt

--- livekit-agents/livekit/agents/pipeline/agent_output.py
+++ livekit-agents/livekit/agents/pipeline/agent_output.py
@@ -85,10 +85,13 @@ class SynthesisHandle:
             extra={"speech_id": self.speech_id},
         )
         logger.info(f"AGENT INTERRUPTED TEXT: {self.tts_forwarder.played_text}")
-        # AppConfig().call_metadata.update({"agent_has_been_interrupted": True})
+        if (
+            self.tts_forwarder.played_text
+            and self.tts_forwarder.played_text.strip() is not None
+        ):
+            AppConfig().call_metadata.update({"agent_has_been_interrupted": True})
 
         if self._play_handle is not None:
-            logger.info("There is an active play handle, interrupting it")
             self._play_handle.interrupt()
 
         self._interrupt_fut.set_result(None)
