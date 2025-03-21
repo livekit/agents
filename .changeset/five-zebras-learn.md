4cb4eff80ef7edc21427cbb5e823a4221eb4bcca
Pontus Lundin <lundin.codeitez@gmail.com>
2025-03-21 14:37:06 +0100

    Add instructions param and new model/voices to openai TTS (#1691)

---
+++ .changeset/five-zebras-learn.md
@@ -0,0 +1,5 @@
+---
+"livekit-plugins-openai": patch
+---
+
+openai new STT model and voices
--- livekit-plugins/livekit-plugins-openai/livekit/plugins/openai/tts.py
+++ livekit-plugins/livekit-plugins-openai/livekit/plugins/openai/tts.py
@@ -43,6 +43,7 @@ class _TTSOptions:
     model: TTSModels | str
     voice: TTSVoices | str
     speed: float
+    instructions: Optional[str] = None
 
 
 class TTS(tts.TTS):
@@ -52,6 +53,7 @@ class TTS(tts.TTS):
         model: TTSModels | str = "tts-1",
         voice: TTSVoices | str = "alloy",
         speed: float = 1.0,
+        instructions: Optional[str] = None,
         base_url: str | None = None,
         api_key: str | None = None,
         client: openai.AsyncClient | None = None,
@@ -75,6 +77,7 @@ class TTS(tts.TTS):
             model=model,
             voice=voice,
             speed=speed,
+            instructions=instructions,
         )
 
         self._client = client or openai.AsyncClient(
@@ -98,10 +101,12 @@ class TTS(tts.TTS):
         model: TTSModels | str | None,
         voice: TTSVoices | str | None,
         speed: float | None,
+        instructions: Optional[str] = None,
     ) -> None:
         self._opts.model = model or self._opts.model
         self._opts.voice = voice or self._opts.voice
         self._opts.speed = speed or self._opts.speed
+        self._opts.instructions = instructions or self._opts.instructions
 
     @staticmethod
     def create_azure_client(
@@ -177,9 +182,10 @@ class ChunkedStream(tts.ChunkedStream):
         oai_stream = self._client.audio.speech.with_streaming_response.create(
             input=self.input_text,
             model=self._opts.model,
-            voice=self._opts.voice,  # type: ignore
+            voice=self._opts.voice,
             response_format="opus",
             speed=self._opts.speed,
+            instructions=self._opts.instructions,
             timeout=httpx.Timeout(30, connect=self._conn_options.timeout),
         )
 
