6e6c6cc8e093e8df5c68c7d0406263c398246d6e
Long Chen <longch1024@gmail.com>
2025-03-27 08:47:22 +0800

    sync transcription with annotation or speaking rate (#1741)

--- livekit-agents/livekit/agents/voice/transcription/synchronizer.py
+++ livekit-agents/livekit/agents/voice/transcription/synchronizer.py
@@ -4,8 +4,10 @@ import asyncio
 import contextlib
 import functools
 import time
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from typing import Callable
+
+import numpy as np
 
 from livekit import rtc
 
@@ -14,6 +16,7 @@ from ...log import logger
 from ...types import NOT_GIVEN, NotGivenOr
 from ...utils import is_given
 from .. import io
+from ._speaking_rate import SpeakingRateDetector, SpeakingRateStream
 
 STANDARD_SPEECH_RATE = 3.83  # hyphens (syllables) per second
 
@@ -23,42 +26,133 @@ class _TextSyncOptions:
     speed: float
     hyphenate_word: Callable[[str], list[str]]
     split_words: Callable[[str], list[tuple[str, int, int]]]
+    sentence_tokenizer: tokenize.SentenceTokenizer
+    speaking_rate_detector: SpeakingRateDetector
 
 
 @dataclass
-class _TextSegment:
-    text: str
-    start_time: float | None  # When the transcript should start playing
-    end_time: float | None  # When the transcript should be fully played
+class _SpeakingRateData:
+    timestamps: list[float] = field(default_factory=list)
+    """timestamps of the speaking rate"""
+
+    speaking_rate: list[float] = field(default_factory=list)
+    """speed at the timestamp"""
+
+    speak_integrals: list[float] = field(default_factory=list)
+    """accumulated speaking units up to the timestamp"""
+
+    _text_buffer: list[str] = field(default_factory=list)
+
+    def add_by_rate(self, *, timestamp: float, speaking_rate: float) -> None:
+        integral = self.speak_integrals[-1] if self.timestamps else 0
+        dt = timestamp - self.pushed_duration
+        integral += speaking_rate * dt
+
+        self.timestamps.append(timestamp)
+        self.speaking_rate.append(speaking_rate)
+        self.speak_integrals.append(integral)
+
+    def add_by_annotation(
+        self,
+        *,
+        text: str,
+        start_time: float | None,
+        end_time: float | None,
+        text_to_hyphens: Callable[[str], list[str]],
+    ) -> None:
+        if start_time is not None:
+            # calculate the integral of the speaking rate up to the start time
+            integral = self.speak_integrals[-1] if self.timestamps else 0
+
+            dt = start_time - self.pushed_duration
+            full_text = "".join(self._text_buffer)
+            d_hyphens = len(text_to_hyphens(full_text))
+            integral += d_hyphens
+            rate = d_hyphens / dt if dt > 0 else 0
+
+            self.timestamps.append(start_time)
+            self.speaking_rate.append(rate)
+            self.speak_integrals.append(integral)
+            self._text_buffer.clear()
+
+        self._text_buffer.append(text)
+
+        if end_time is not None:
+            self.add_by_annotation(
+                "", start_time=end_time, end_time=None, text_to_hyphens=text_to_hyphens
+            )
+
+    def accumulate_to(self, timestamp: float) -> float:
+        """Get accumulated speaking units up to the given timestamp."""
+        if not self.timestamps:
+            return 0
+
+        idx = np.searchsorted(self.timestamps, timestamp, side="right")
+        if idx == 0:
+            return 0
+
+        integral_t = self.speak_integrals[idx - 1]
+
+        # fill the tail assuming the speaking rate is constant
+        dt = timestamp - self.timestamps[idx - 1]
+        rate = (
+            self.speaking_rate[idx]
+            if idx < len(self.speaking_rate)
+            else self.speaking_rate[idx - 1]
+        )
+        integral_t += rate * dt
+
+        if idx < len(self.timestamps):
+            # if there is a next timestamp, make sure the integral does not exceed the next
+            integral_t = min(integral_t, self.speak_integrals[idx])
+
+        return integral_t
+
+    @property
+    def pushed_duration(self) -> float:
+        return self.timestamps[-1] if self.timestamps else 0
 
 
 @dataclass
-class _AudioSegment:
-    frame: rtc.AudioFrame
-    # TODO(theomonnom): support TTS alignments
+class _AudioData:
+    sr_stream: SpeakingRateStream  # speaking rate estimation
+    pushed_duration: float = 0.0
+    done: bool = False
+    sr_data_est: _SpeakingRateData = field(default_factory=_SpeakingRateData)
+    sr_data_annotated: _SpeakingRateData | None = None  # speaking rate from `start_time`
+
+
+@dataclass
+class _TextData:
+    sentence_stream: tokenize.SentenceStream
+    pushed_text: str = ""
+    done: bool = False
+    forwarded_hyphens: int = 0
 
 
 class _SegmentSynchronizerImpl:
     """Synchronizes one text segment with one audio segment"""
 
     def __init__(self, options: _TextSyncOptions, *, next_in_chain: io.TextOutput) -> None:
-        self._text_ch = utils.aio.Chan[_TextSegment]()
-        self._audio_ch = utils.aio.Chan[_AudioSegment]()
-        self._text_segments: list[_TextSegment] = []
-        self._audio_segments: list[_AudioSegment] = []
-        self._next_in_chain = next_in_chain
-
         self._opts = options
+        self._text_data = _TextData(sentence_stream=self._opts.sentence_tokenizer.stream())
+        self._audio_data = _AudioData(sr_stream=self._opts.speaking_rate_detector.stream())
+
+        self._next_in_chain = next_in_chain
         self._start_wall_time: float | None = None
         self._start_fut: asyncio.Event = asyncio.Event()
 
-        self._speed = STANDARD_SPEECH_RATE * self._opts.speed
+        self._speed = STANDARD_SPEECH_RATE * self._opts.speed  # hyphens per second
+        self._speed_on_speaking_unit: float | None = None  # hyphens per speaking unit
+        # a speaking unit is defined by the speaking rate estimation method, it's a relative unit
+
         self._out_ch = utils.aio.Chan[str]()
         self._close_future = asyncio.Future[None]()
 
         self._main_atask = asyncio.create_task(self._main_task())
         self._main_atask.add_done_callback(lambda _: self._out_ch.close())
         self._capture_atask = asyncio.create_task(self._capture_task())
+        self._speaking_rate_atask = asyncio.create_task(self._speaking_rate_task())
 
         self._playback_completed = False
 
@@ -77,18 +171,16 @@ class _SegmentSynchronizerImpl:
             self._start_wall_time = time.time()
             self._start_fut.set()
 
-        seg = _AudioSegment(frame=frame)
-        self._audio_segments.append(seg)
-        self._audio_ch.send_nowait(seg)
+        self._audio_data.sr_stream.push_frame(frame)
+        self._audio_data.pushed_duration += frame.duration
 
     def end_audio_input(self) -> None:
         if self.closed:
             logger.warning("_SegmentSynchronizerImpl.end_audio_input called after close")
             return
 
-        with contextlib.suppress(utils.aio.ChanClosed):
-            self._audio_ch.close()
-
+        self._audio_data.done = True
+        self._audio_data.sr_stream.end_input()
         self._reestimate_speed()
 
     def push_text(self, text: str) -> None:
@@ -100,39 +192,61 @@ class _SegmentSynchronizerImpl:
         if isinstance(text, io.TimedString):
             start_time = text.start_time or None
             end_time = text.end_time or None
+            if not self._audio_data.sr_data_annotated:
+                self._audio_data.sr_data_annotated = _SpeakingRateData()
 
-        seg = _TextSegment(text=text, start_time=start_time, end_time=end_time)
-        self._text_segments.append(seg)
-        self._text_ch.send_nowait(seg)
+            if start_time is not None or end_time is not None:
+                # flush if we have time annotations
+                self._text_data.sentence_stream.flush()
+
+            # accumulate the actual hyphens if time annotations are present
+            self._audio_data.sr_data_annotated.add_by_annotation(
+                text=text,
+                start_time=start_time,
+                end_time=end_time,
+                text_to_hyphens=self._calc_hyphens,
+            )
+
+        self._text_data.sentence_stream.push_text(text)
+        self._text_data.pushed_text += text
+
+        if start_time is not None or end_time is not None:
+            self._text_data.sentence_stream.flush()
 
     def end_text_input(self) -> None:
         if self.closed:
             logger.warning("_SegmentSynchronizerImpl.end_text_input called after close")
             return
 
-        with contextlib.suppress(utils.aio.ChanClosed):
-            self._text_ch.close()
+        self._text_data.done = True
+        self._text_data.sentence_stream.end_input()
 
         self._reestimate_speed()
 
     def _reestimate_speed(self) -> None:
-        if not self._text_ch.closed or not self._audio_ch.closed:
+        if not self._text_data.done or not self._audio_data.done:
             return
 
-        # calculate the true syllables per second based on the complete text and audio input
-        words = self._opts.split_words("".join(seg.text for seg in self._text_segments))
-        total_syllables = sum(len(self._opts.hyphenate_word(w)) for w, _, _ in words)
-        total_duration = sum(seg.frame.duration for seg in self._audio_segments)
-        self._speed = total_syllables / total_duration
+        pushed_hyphens = len(self._calc_hyphens(self._text_data.pushed_text))
+        # hyphens per second
+        if self._audio_data.pushed_duration > 0:
+            self._speed = pushed_hyphens / self._audio_data.pushed_duration
+
+        # hyphens per speaking unit
+        pushed_speaking_units = self._audio_data.sr_data_est.accumulate_to(
+            self._audio_data.pushed_duration
+        )
+        if pushed_speaking_units > 0:
+            self._speed_on_speaking_unit = pushed_hyphens / pushed_speaking_units
 
     def playback_finished(self, *, playback_position: float, interrupted: bool) -> None:
         if self.closed:
             logger.warning("_SegmentSynchronizerImpl.playback_finished called after close")
             return
 
-        if not self._text_ch.closed or not self._audio_ch.closed:
+        if not self._text_data.done or not self._audio_data.done:
             logger.warning(
-                "_SegmentSynchronizerImpl.playback_finished called before text/audio input closed"
+                "_SegmentSynchronizerImpl.playback_finished called before text/audio input is done"
             )
             return
 
@@ -150,6 +264,13 @@ class _SegmentSynchronizerImpl:
             self._next_in_chain.flush()
 
     @utils.log_exceptions(logger=logger)
+    async def _speaking_rate_task(self) -> None:
+        async for ev in self._audio_data.sr_stream:
+            self._audio_data.sr_data_est.add_by_rate(
+                timestamp=ev.timestamp, speaking_rate=ev.speaking_rate
+            )
+
+    @utils.log_exceptions(logger=logger)
     async def _main_task(self) -> None:
         await self._start_fut.wait()
 
@@ -158,57 +279,59 @@ class _SegmentSynchronizerImpl:
 
         assert self._start_wall_time is not None
 
-        i = 0
-        async for text_seg in self._text_ch:
-            i += 1
-
-            base_start = text_seg.start_time or 0.0
-            elapsed = time.time() - self._start_wall_time
-
-            if (delay_until_start := base_start - elapsed) > 0:
-                await self._sleep_if_not_closed(delay_until_start)
-
-            # check if closed after waiting for the base_start time (it could be several seconds in
-            # extreme cases)
-            # However, if playback *is* completed, we keep going so we can flush out
-            # all remaining text segments without pacing delays.
-            if self.closed and not self._playback_completed:
-                return
-
-            acc_syllables = 0
+        async for text_seg in self._text_data.sentence_stream:
+            sentence = text_seg.token
             text_cursor = 0
-            for word, _, end_pos in self._opts.split_words(text_seg.text):
+            for word, _, end_pos in self._opts.split_words(sentence):
+                if self.closed and not self._playback_completed:
+                    return
+
                 if self._playback_completed:
-                    self._out_ch.send_nowait(text_seg.text[text_cursor:end_pos])
+                    self._out_ch.send_nowait(sentence[text_cursor:end_pos])
+                    text_cursor = end_pos
                     continue
 
-                word_syllables = len(self._opts.hyphenate_word(word))
+                word_hyphens = len(self._opts.hyphenate_word(word))
                 elapsed = time.time() - self._start_wall_time
 
-                # since the speed can change at anytime, calculate the word delay based on
-                # the "target_syllables"
-                target_syllables = round(self._speed * (elapsed - base_start))
-                dt = target_syllables - acc_syllables
-                to_wait_hyphens = max(0.0, word_syllables - dt)
-                delay = to_wait_hyphens / self._speed
+                target_hyphens: float | None = None
+                if self._audio_data.sr_data_annotated:
+                    # use the actual speaking rate
+                    target_hyphens = self._audio_data.sr_data_annotated.accumulate_to(elapsed)
+                elif self._speed_on_speaking_unit:
+                    # use the estimated speed from speaking rate
+                    target_speaking_units = self._audio_data.sr_data_est.accumulate_to(elapsed)
+                    target_hyphens = target_speaking_units * self._speed_on_speaking_unit
 
-                # if the next text segment should have started, flush the word as soon as possible
-                # for this current segment to catch up
-                if (i + 1) < len(self._text_segments):
-                    next_seg = self._text_segments[i + 1]
-                    if next_seg.start_time is not None and next_seg.start_time <= elapsed:
-                        delay = 0
+                if target_hyphens is not None:
+                    dt = np.ceil(target_hyphens) - self._text_data.forwarded_hyphens
+                    delay = max(0.0, word_hyphens - dt) / self._speed
+                else:
+                    delay = word_hyphens / self._speed
 
                 # if playback completed, flush the word as soon as possible
                 if self._playback_completed:
                     delay = 0
 
                 await self._sleep_if_not_closed(delay / 2.0)
-                self._out_ch.send_nowait(text_seg.text[text_cursor:end_pos])
+                self._out_ch.send_nowait(sentence[text_cursor:end_pos])
                 await self._sleep_if_not_closed(delay / 2.0)
 
-                acc_syllables += word_syllables
+                self._text_data.forwarded_hyphens += word_hyphens
                 text_cursor = end_pos
+
+            if text_cursor < len(sentence):
+                # send the remaining text (e.g. new line or spaces)
+                self._out_ch.send_nowait(sentence[text_cursor:])
+
+    def _calc_hyphens(self, text: str) -> list[str]:
+        """Calculate hyphens for text."""
+        hyphens: list[str] = []
+        words: list[tuple[str, int, int]] = self._opts.split_words(text=text)
+        for word, _, _ in words:
+            new = self._opts.hyphenate_word(word)
+            hyphens.extend(new)
+        return hyphens
 
     async def _sleep_if_not_closed(self, delay: float) -> None:
         with contextlib.suppress(asyncio.TimeoutError):
@@ -220,10 +343,10 @@ class _SegmentSynchronizerImpl:
 
         self._close_future.set_result(None)
         self._start_fut.set()  # avoid deadlock of main_task in case it never started
-        # not using end_text_input/end_audio_input, because they may change the speed calculation
-        self._text_ch.close()
-        self._audio_ch.close()
+        await self._text_data.sentence_stream.aclose()
+        await self._audio_data.sr_stream.aclose()
         await self._capture_atask
+        await self._speaking_rate_atask
 
 
 class TranscriptSynchronizer:
@@ -244,6 +367,7 @@ class TranscriptSynchronizer:
         split_words: Callable[[str], list[tuple[str, int, int]]] = functools.partial(
             tokenize.basic.split_words, ignore_punctuation=False
         ),
+        sentence_tokenizer: NotGivenOr[tokenize.SentenceTokenizer] = NOT_GIVEN,
     ) -> None:
         super().__init__()
 
@@ -251,7 +375,13 @@ class TranscriptSynchronizer:
         self._audio_output = _SyncedAudioOutput(self, next_in_chain=next_in_chain_audio)
         self._text_attached, self._audio_attached = True, True
         self._opts = _TextSyncOptions(
-            speed=speed, hyphenate_word=hyphenate_word, split_words=split_words
+            speed=speed,
+            hyphenate_word=hyphenate_word,
+            split_words=split_words,
+            sentence_tokenizer=(
+                sentence_tokenizer or tokenize.basic.SentenceTokenizer(retain_format=True)
+            ),
+            speaking_rate_detector=SpeakingRateDetector(),
         )
         self._enabled = True
         self._closed = False
