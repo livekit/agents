53f801020bbcffe6e3a2eef41717de54c28d6ba2
Jayesh Parmar <60539217+jayeshp19@users.noreply.github.com>
2025-03-31 12:59:49 +0530

    Google plugin fix Imports  (#1753)

--- livekit-plugins/livekit-plugins-google/livekit/plugins/google/beta/realtime/api_proto.py
+++ livekit-plugins/livekit-plugins-google/livekit/plugins/google/beta/realtime/api_proto.py
@@ -5,13 +5,13 @@ from typing import Literal, Union
 
 from google.genai import types
 
-from ..._utils import _build_gemini_ctx, _build_tools
+# from ..._utils import _build_gemini_ctx, _build_tools
 
 LiveAPIModels = Literal["gemini-2.0-flash-exp"]
 
 Voice = Literal["Puck", "Charon", "Kore", "Fenrir", "Aoede"]
 
-__all__ = ["_build_tools", "ClientEvents", "_build_gemini_ctx"]
+# __all__ = ["_build_tools", "ClientEvents", "_build_gemini_ctx"]
 
 ClientEvents = Union[
     types.ContentListUnion,
