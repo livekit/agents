8caa38ee4b529e43ee0835d9c95ee064f2556722
jeradf <jeradfields@gmail.com>
2025-04-04 18:25:37 -0500

    fix model choice

--- livekit-plugins/livekit-plugins-turn-detector/livekit/plugins/turn_detector/eou.py
+++ livekit-plugins/livekit-plugins-turn-detector/livekit/plugins/turn_detector/eou.py
@@ -63,6 +63,7 @@ class _EUORunnerBase(_InferenceRunner):
                 revision=self._model_revision,
                 local_files_only=True,
             )
+            print(f"Loading: {HG_MODEL}|{self._model_revision}")
             self._session = ort.InferenceSession(
                 local_path_onnx, providers=["CPUExecutionProvider"]
             )
@@ -141,8 +142,10 @@ class EOUModel:
         self._executor = (
             inference_executor or get_current_job_context().inference_executor
         )
-
-        self._inference_method = _EUORunnerMultilingual.INFERENCE_METHOD
+        if self._model_type == "multilingual":
+            self._inference_method = _EUORunnerMultilingual.INFERENCE_METHOD
+        else:
+            self._inference_method = _EUORunnerEn.INFERENCE_METHOD
         config_fname = _download_from_hf_hub(
             HG_MODEL,
             "languages.json",
