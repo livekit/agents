ec705e340724b076004cc5910906bed87664ba02
Théo Monnom <theo.8bits@gmail.com>
2025-04-08 18:09:47 +0200

    Update __init__.py

--- livekit-agents/livekit/agents/__init__.py
+++ livekit-agents/livekit/agents/__init__.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from . import cli, ipc, llm, metrics, stt, tokenize, tts, utils, vad  # noqa: F401
 from ._exceptions import (
     APIConnectionError,
     APIError,
