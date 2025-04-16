a8fec5b0430d7dbb803e5126ab155b6ad55898c5
Shubhrakanti Ganguly <shubhrakanti.ganguly@livekit.io>
2025-04-16 14:28:16 -0700

    fix import

--- livekit-plugins/livekit-plugins-openai/livekit/plugins/openai/realtime/realtime_model.py
+++ livekit-plugins/livekit-plugins-openai/livekit/plugins/openai/realtime/realtime_model.py
@@ -16,8 +16,7 @@ import aiohttp
 from pydantic import BaseModel, ValidationError
 
 from livekit import rtc
-from livekit.agents import io, llm, utils
-from livekit.agents._exceptions import APIConnectionError, APIError
+from livekit.agents import APIConnectionError, APIError, io, llm, utils
 from livekit.agents.types import NOT_GIVEN, NotGivenOr
 from livekit.agents.utils import is_given
 from openai.types.beta.realtime import (
