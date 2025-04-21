471f5ee56c762e4b5f95d0f3efa46690d5579dd3
Saatvik Bhatnagar <saatvik19097@iiitd.ac.in>
2025-04-22 04:15:53 +0530

    Add Hume TTS Plugin

---
+++ livekit-plugins/livekit-plugins-hume/livekit/plugins/hume/__init__.py
@@ -0,0 +1,56 @@
+# Copyright 2023 LiveKit, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from __future__ import annotations
+
+__version__ = "1.0.0"
+
+# make imports available
+from hume.tts import (
+    Format,
+    PostedContext,
+    PostedUtterance,
+    PostedUtteranceVoiceWithId,
+    PostedUtteranceVoiceWithName,
+)
+from livekit.agents import Plugin
+
+from .tts import TTS
+
+# all exports
+__all__ = [
+    "TTS",
+    "Format",
+    "PostedUtterance",
+    "PostedContext",
+    "PostedUtteranceVoiceWithName",
+    "PostedUtteranceVoiceWithId",
+]
+
+
+class HumeAIPlugin(Plugin):
+    def __init__(self) -> None:
+        super().__init__(__name__, __version__, __package__)
+
+
+Plugin.register_plugin(HumeAIPlugin())
+
+# Cleanup docs of unexported modules
+_module = dir()
+NOT_IN_ALL = [m for m in _module if m not in __all__]
+
+__pdoc__ = {}
+
+for n in NOT_IN_ALL:
+    __pdoc__[n] = False
