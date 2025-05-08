a86999d3659ef2c7d615e815c8a389f3eefd345e
Théo Monnom <theo.8bits@gmail.com>
2025-05-08 21:55:39 +0200

    Update stream_adapter.py

--- livekit-agents/livekit/agents/tts/stream_adapter.py
+++ livekit-agents/livekit/agents/tts/stream_adapter.py
@@ -7,7 +7,6 @@ from .. import tokenize, utils
 from ..types import DEFAULT_API_CONNECT_OPTIONS, APIConnectOptions
 from .tts import TTS, ChunkedStream, SynthesizedAudio, SynthesizeStream, TTSCapabilities
 
-
 # already a retry mechanism in TTS.synthesize, don't retry in stream adapter
 DEFAULT_STREAM_ADAPTER_API_CONNECT_OPTIONS = APIConnectOptions(
     max_retry=0, timeout=DEFAULT_API_CONNECT_OPTIONS.timeout
