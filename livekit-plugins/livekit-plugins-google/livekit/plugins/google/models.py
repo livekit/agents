764b5ce50008678721f81391739aab296b1c2169
Mateusz Kulpa <mateusz.kulpa@outlook.com>
2025-05-11 13:19:24 +0200

    feat: support cached prompt metrics for Gemini LLM (#2256)

--- livekit-plugins/livekit-plugins-google/livekit/plugins/google/models.py
+++ livekit-plugins/livekit-plugins-google/livekit/plugins/google/models.py
@@ -95,6 +95,8 @@ SpeechLanguages = Literal[
 Gender = Literal["male", "female", "neutral"]
 
 ChatModels = Literal[
+    "gemini-2.5-pro-preview-05-06",
+    "gemini-2.5-flash-preview-04-17",
     "gemini-2.0-flash-001",
     "gemini-2.0-flash-lite-preview-02-05",
     "gemini-2.0-pro-exp-02-05",
