4faabea71d86b3c3bee96bcd90200ac7cd3db20a
Théo Monnom <theo.8bits@gmail.com>
2025-05-31 01:09:50 +0200

    use pyht SDK

--- livekit-plugins/livekit-plugins-playai/livekit/plugins/playai/tts.py
+++ livekit-plugins/livekit-plugins-playai/livekit/plugins/playai/tts.py
@@ -2,43 +2,45 @@ from __future__ import annotations
 
 import asyncio
 import os
-from dataclasses import dataclass, replace
+import weakref
+from dataclasses import dataclass, fields, replace
 
-import aiohttp
+from pyht import AsyncClient as PlayAsyncClient
+from pyht.client import Format, Language, TTSOptions as PlayOptions
 
-from livekit.agents import (
-    APIConnectionError,
-    APIConnectOptions,
-    APIStatusError,
-    APITimeoutError,
-    tts,
-    utils,
+from livekit.agents import APIConnectionError, APIConnectOptions, tokenize, tts, utils
+from livekit.agents.types import (
+    DEFAULT_API_CONNECT_OPTIONS,
+    NOT_GIVEN,
+    NotGivenOr,
 )
-from livekit.agents.types import DEFAULT_API_CONNECT_OPTIONS, NOT_GIVEN, NotGivenOr
 from livekit.agents.utils import is_given
 
+from .log import logger
 from .models import TTSModel
+
+NUM_CHANNELS = 1
 
 
 @dataclass
 class _TTSOptions:
     model: TTSModel | str
-    language: str
-    sample_rate: int
-    voice: str
+    play_options: PlayOptions
+    word_tokenizer: tokenize.WordTokenizer
 
 
 class TTS(tts.TTS):
     def __init__(
         self,
         *,
-        api_key: str | None = None,
-        user_id: str | None = None,
-        model: TTSModel | str = "PlayDialog-turbo",
-        voice: str = "Atlas-PlayAI",
+        api_key: NotGivenOr[str] = NOT_GIVEN,
+        user_id: NotGivenOr[str] = NOT_GIVEN,
+        voice: str = "s3://voice-cloning-zero-shot/d9ff78ba-d016-47f6-b0ef-dd630f59414e/female-cs/manifest.json",
         language: str = "english",
         sample_rate: int = 24000,
-        http_session: aiohttp.ClientSession | None = None,
+        model: TTSModel | str = "Play3.0-mini",
+        word_tokenizer: tokenize.WordTokenizer | None = None,
+        **kwargs,
     ) -> None:
         """
         Initialize the PlayAI TTS engine.
@@ -46,39 +48,52 @@ class TTS(tts.TTS):
         Args:
             api_key (str): PlayAI API key.
             user_id (str): PlayAI user ID.
-            model (TTSModel): TTS model, defaults to "PlayDialog-turbo".
             voice (str): Voice manifest URL.
+            model (TTSModel): TTS model, defaults to "Play3.0-mini".
             language (str): language, defaults to "english".
-            sample_rate (int): sample rate (Hz), A number greater than or equal to 8000,
-                and must be less than or equal to 48000
-        """
+            sample_rate (int): sample rate (Hz), A number greater than or equal to 8000, and must be less than or equal to 48000
+            word_tokenizer (tokenize.WordTokenizer): Tokenizer for processing text. Defaults to basic WordTokenizer.
+            **kwargs: Additional options.
+        """  # noqa: E501
+
         super().__init__(
-            capabilities=tts.TTSCapabilities(streaming=True),
+            capabilities=tts.TTSCapabilities(
+                streaming=True,
+            ),
             sample_rate=sample_rate,
             num_channels=1,
         )
 
-        api_key = api_key or os.environ.get("PLAYHT_API_KEY")
-        user_id = user_id or os.environ.get("PLAYHT_USER_ID")
+        if not word_tokenizer:
+            word_tokenizer = tokenize.basic.WordTokenizer(ignore_punctuation=False)
 
-        if not api_key or not user_id:
+        pyht_api_key = api_key if is_given(api_key) else os.environ.get("PLAYHT_API_KEY")
+        pyht_user_id = user_id if is_given(user_id) else os.environ.get("PLAYHT_USER_ID")
+
+        if not pyht_api_key or not pyht_user_id:
             raise ValueError(
-                "PlayHT API key and user ID are required. Set environment variables PLAYHT_API_KEY "
-                "and PLAYHT_USER_ID or pass them explicitly."
+                "PlayHT API key and user ID are required. Set environment variables PLAYHT_API_KEY and PLAYHT_USER_ID or pass them explicitly."  # noqa: E501
             )
 
-        self._api_key = api_key
-        self._user_id = user_id
+        _validate_kwargs(kwargs)
+
         self._opts = _TTSOptions(
-            voice=voice, model=model, sample_rate=sample_rate, language=language
+            model=model,
+            play_options=PlayOptions(
+                voice=voice,
+                format=Format.FORMAT_OGG,
+                sample_rate=sample_rate,
+                language=Language(language),
+                **kwargs,
+            ),
+            word_tokenizer=word_tokenizer,
         )
-        self._session = http_session
 
-    def _ensure_session(self) -> aiohttp.ClientSession:
-        if not self._session:
-            self._session = utils.http_context.http_session()
-
-        return self._session
+        self._client = PlayAsyncClient(
+            user_id=pyht_user_id,
+            api_key=pyht_api_key,
+        )
+        self._streams = weakref.WeakSet[SynthesizeStream]()
 
     def update_options(
         self,
@@ -86,63 +101,141 @@ class TTS(tts.TTS):
         voice: NotGivenOr[str] = NOT_GIVEN,
         model: NotGivenOr[TTSModel | str] = NOT_GIVEN,
         language: NotGivenOr[str] = NOT_GIVEN,
+        **kwargs,
     ) -> None:
+        """
+        Update the TTS options.
+        """
+        updates = {}
         if is_given(voice):
-            self._opts.voice = voice
+            updates["voice"] = voice
+        if is_given(language):
+            updates["language"] = Language(language)
+        updates.update(kwargs)
+
+        _validate_kwargs(updates)
+
+        for key, value in updates.items():
+            if value is not None:
+                setattr(self._opts.play_options, key, value)
+
         if is_given(model):
             self._opts.model = model
-        if is_given(language):
-            self._opts.language = language
 
     def synthesize(
         self, text: str, *, conn_options: APIConnectOptions = DEFAULT_API_CONNECT_OPTIONS
     ) -> ChunkedStream:
         return ChunkedStream(tts=self, input_text=text, conn_options=conn_options)
 
+    def stream(
+        self, *, conn_options: APIConnectOptions = DEFAULT_API_CONNECT_OPTIONS
+    ) -> SynthesizeStream:
+        stream = SynthesizeStream(tts=self, conn_options=conn_options)
+        self._streams.add(stream)
+        return stream
+
 
 class ChunkedStream(tts.ChunkedStream):
     def __init__(self, *, tts: TTS, input_text: str, conn_options: APIConnectOptions) -> None:
         super().__init__(tts=tts, input_text=input_text, conn_options=conn_options)
-        self._tts: TTS = tts
-        self._opts = replace(tts._opts)
+        self._tts = tts
+        self._opts = replace(self._tts._opts)
 
     async def _run(self, output_emitter: tts.AudioEmitter) -> None:
+        output_emitter.initialize(
+            request_id=utils.shortuuid(),
+            sample_rate=self._opts.play_options.sample_rate,  # type: ignore
+            num_channels=1,
+            mime_type="audio/ogg",
+        )
+
         try:
-            async with self._tts._ensure_session().post(
-                "https://api.play.ht/api/v2/tts/stream",
-                # headers from https://github.com/playht/pyht/blob/master/pyht/client.py
-                headers={
-                    "authorization": f"Bearer {self._tts._api_key}",
-                    "x-user-id": self._tts._user_id,
-                    "accept": "audio/wav",
-                },
-                json={
-                    "text": self._input_text,
-                    "voice": self._opts.voice,
-                    "voice_engine": self._opts.model,
-                    "output_format": "wav",
-                    "sample_rate": self._opts.sample_rate,
-                },
-                timeout=aiohttp.ClientTimeout(total=30, sock_connect=self._conn_options.timeout),
-            ) as resp:
-                resp.raise_for_status()
+            async for chunk in self._tts._client.tts(
+                text=self._input_text,
+                options=self._opts.play_options,
+                voice_engine=self._opts.model,
+                protocol="http",
+                streaming=True,
+            ):
+                output_emitter.push(chunk)
 
-                output_emitter.initialize(
-                    request_id=utils.shortuuid(),
-                    sample_rate=self._opts.sample_rate,
-                    num_channels=1,
-                    mime_type="audio/wav",
-                )
-
-                async for data, _ in resp.content.iter_chunks():
-                    output_emitter.push(data)
-
-                output_emitter.flush()
-        except asyncio.TimeoutError:
-            raise APITimeoutError() from None
-        except aiohttp.ClientResponseError as e:
-            raise APIStatusError(
-                message=e.message, status_code=e.status, request_id=None, body=None
-            ) from None
         except Exception as e:
             raise APIConnectionError() from e
+
+
+class SynthesizeStream(tts.SynthesizeStream):
+    def __init__(
+        self,
+        *,
+        tts: TTS,
+        conn_options: APIConnectOptions,
+    ):
+        super().__init__(tts=tts, conn_options=conn_options)
+        self._tts = tts
+        self._opts = replace(self._tts._opts)
+        self._segments_ch = utils.aio.Chan[tokenize.WordStream]()
+
+    async def _run(self, output_emitter: tts.AudioEmitter) -> None:
+        if self._opts.model == "PlayDialog-turbo":
+            protocol = "http"
+        else:
+            protocol = "ws"
+
+        output_emitter.initialize(
+            request_id=utils.shortuuid(),
+            sample_rate=self._opts.play_options.sample_rate,  # type: ignore
+            num_channels=1,
+            mime_type="audio/ogg",
+            stream=True,
+        )
+        output_emitter.start_segment(segment_id=utils.shortuuid())
+
+        input_task = asyncio.create_task(self._tokenize_input())
+        try:
+            text_stream = await self._create_text_stream()
+
+            async for chunk in self._tts._client.stream_tts_input(
+                text_stream=text_stream,
+                options=self._opts.play_options,
+                voice_engine=self._opts.model,
+                protocol=protocol,
+            ):
+                output_emitter.push(chunk)
+
+        except Exception as e:
+            raise APIConnectionError() from e
+        finally:
+            await utils.aio.gracefully_cancel(input_task)
+
+    @utils.log_exceptions(logger=logger)
+    async def _tokenize_input(self):
+        # Converts incoming text into WordStreams and sends them into _segments_ch
+        word_stream = None
+        async for input in self._input_ch:
+            if isinstance(input, str):
+                if word_stream is None:
+                    word_stream = self._opts.word_tokenizer.stream()
+                    self._segments_ch.send_nowait(word_stream)
+                word_stream.push_text(input)
+            elif isinstance(input, self._FlushSentinel):
+                if word_stream:
+                    word_stream.end_input()
+                word_stream = None
+        self._segments_ch.close()
+
+    @utils.log_exceptions(logger=logger)
+    async def _create_text_stream(self):
+        async def text_stream():
+            async for word_stream in self._segments_ch:
+                async for word in word_stream:
+                    self._mark_started()
+                    yield word.token
+
+        return text_stream()
+
+
+def _validate_kwargs(kwargs: dict) -> None:
+    valid_keys = {field.name for field in fields(PlayOptions)}
+    invalid_keys = set(kwargs.keys()) - valid_keys
+    if invalid_keys:
+        raise ValueError(f"Invalid parameters: {invalid_keys}. Allowed parameters: {valid_keys}")
