667a9896eda4cd3e04e253ecb5c49308b6cb1760
Long Chen <longch1024@gmail.com>
2025-05-30 20:03:58 +0800

    fix agent state for pipeline agent (#2453)

--- livekit-plugins/livekit-plugins-assemblyai/livekit/plugins/assemblyai/stt.py
+++ livekit-plugins/livekit-plugins-assemblyai/livekit/plugins/assemblyai/stt.py
@@ -290,7 +290,7 @@ class SpeechStream(stt.SpeechStream):
 
                 try:
                     done, _ = await asyncio.wait(
-                        [asyncio.gather(*tasks), wait_reconnect_task],
+                        (asyncio.gather(*tasks), wait_reconnect_task),
                         return_when=asyncio.FIRST_COMPLETED,
                     )
                     for task in done:
@@ -320,7 +320,7 @@ class SpeechStream(stt.SpeechStream):
             "end_of_turn_confidence_threshold": self._opts.end_of_turn_confidence_threshold
             if is_given(self._opts.end_of_turn_confidence_threshold)
             else DEFAULT_END_OF_TURN_CONFIDENCE_THRESHOLD,
-            "min_end_of_turn_silence_when_confident": self._opts.min_end_of_turn_silence_when_confident
+            "min_end_of_turn_silence_when_confident": self._opts.min_end_of_turn_silence_when_confident  # noqa: E501
             if is_given(self._opts.min_end_of_turn_silence_when_confident)
             else DEFAULT_MIN_END_OF_TURN_SILENCE_WHEN_CONFIDENT,
             "max_turn_silence": self._opts.max_turn_silence
