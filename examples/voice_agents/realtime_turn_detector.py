c2e09d42664c01f9a2cc12bd4c50c21a817ecb84
David Zhao <dz@livekit.io>
2025-05-31 23:27:55 -0700

    example of using Gemini API with manual turn detection

--- examples/voice_agents/realtime_turn_detector.py
+++ examples/voice_agents/realtime_turn_detector.py
@@ -1,9 +1,10 @@
 import logging
 
 from dotenv import load_dotenv
+from google.genai import types  # noqa: F401
 
 from livekit.agents import Agent, AgentSession, JobContext, JobProcess, WorkerOptions, cli
-from livekit.plugins import deepgram, openai, silero
+from livekit.plugins import deepgram, google, openai, silero  # noqa: F401
 from livekit.plugins.turn_detector.multilingual import MultilingualModel
 
 logger = logging.getLogger("realtime-turn-detector")
@@ -25,6 +26,7 @@ async def entrypoint(ctx: JobContext):
         turn_detection=MultilingualModel(),
         vad=ctx.proc.userdata["vad"],
         stt=deepgram.STT(),
+        # To use OpenAI Realtime API
         llm=openai.realtime.RealtimeModel(
             voice="alloy",
             # it's necessary to turn off turn detection in the OpenAI Realtime API in order to use
@@ -32,6 +34,15 @@ async def entrypoint(ctx: JobContext):
             turn_detection=None,
             input_audio_transcription=None,  # we use Deepgram STT instead
         ),
+        # To use Gemini Live API
+        # llm=google.beta.realtime.RealtimeModel(
+        #     realtime_input_config=types.RealtimeInputConfig(
+        #         automatic_activity_detection=types.AutomaticActivityDetection(
+        #             disabled=True,
+        #         ),
+        #     ),
+        #     input_audio_transcription=None,
+        # ),
     )
     await session.start(agent=Agent(instructions="You are a helpful assistant."), room=ctx.room)
 
