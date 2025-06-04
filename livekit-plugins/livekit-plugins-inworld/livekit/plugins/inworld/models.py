43210fd525ec832e7ebae440d79cf2bb0d9611cd
Michael Solati <mkslt04@gmail.com>
2025-06-04 14:20:10 -0700

    feat(plugin-inworldai): add inworld ai tts plugin

---
+++ livekit-plugins/livekit-plugins-inworld/livekit/plugins/inworld/models.py
@@ -0,0 +1,7 @@
+from inworld_sdk import (
+    TTSLanguageCodes as inworld_tts_language_codes,
+    TTSVoices as inworld_tts_voices,
+)
+
+TTSVoices = inworld_tts_voices
+TTSLanguageCodes = inworld_tts_language_codes
