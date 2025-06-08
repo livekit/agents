8ab0b5356d13412b4e7f636c4f37a24140bc246f
David Zhao <dz@livekit.io>
2025-06-08 01:06:46 -0700

    fixed headers

--- livekit-plugins/livekit-plugins-langchain/livekit/plugins/langchain/version.py
+++ livekit-plugins/livekit-plugins-langchain/livekit/plugins/langchain/version.py
@@ -1,4 +1,4 @@
-# Copyright 2023 LiveKit, Inc.
+# Copyright 2025 LiveKit, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
