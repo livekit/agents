1285c826143353330f7b05cd8ee7b0a4f7ee652a
Denis Malimonov <dacool731@mail.ru>
2025-06-17 18:31:48 +0700

    hot fix

--- livekit-plugins/livekit-plugins-openai/livekit/plugins/openai/realtime/realtime_model.py
+++ livekit-plugins/livekit-plugins-openai/livekit/plugins/openai/realtime/realtime_model.py
@@ -1525,11 +1525,14 @@ class RealtimeSession(
                 text_tokens=usage.get("input_token_details", {}).get("text_tokens", 0),
                 cached_tokens_details=RealtimeModelMetrics.CachedTokenDetails(
                     text_tokens=usage.get("input_token_details", {})
-                        .get("cached_tokens_details", {}).get("text_tokens", 0),
+                    .get("cached_tokens_details", {})
+                    .get("text_tokens", 0),
                     audio_tokens=usage.get("input_token_details", {})
-                        .get("cached_tokens_details", {}).get("audio_tokens", 0),
+                    .get("cached_tokens_details", {})
+                    .get("audio_tokens", 0),
                     image_tokens=usage.get("input_token_details", {})
-                        .get("cached_tokens_details", {}).get("image_tokens", 0),
+                    .get("cached_tokens_details", {})
+                    .get("image_tokens", 0),
                 ),
             ),
             output_token_details=RealtimeModelMetrics.OutputTokenDetails(
