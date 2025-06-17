f24c872e1ada94fab4407f866e1dfc82dd4c2c08
Denis Malimonov <dacool731@mail.ru>
2025-06-17 18:04:15 +0700

    update cached_tokens_details
    - update cached_tokens_details
    - disable ensure_ascii (so that Cyrillic characters are displayed in the log)

--- livekit-plugins/livekit-plugins-openai/livekit/plugins/openai/realtime/realtime_model.py
+++ livekit-plugins/livekit-plugins-openai/livekit/plugins/openai/realtime/realtime_model.py
@@ -1523,7 +1523,11 @@ class RealtimeSession(
                 audio_tokens=usage.get("input_token_details", {}).get("audio_tokens", 0),
                 cached_tokens=usage.get("input_token_details", {}).get("cached_tokens", 0),
                 text_tokens=usage.get("input_token_details", {}).get("text_tokens", 0),
-                cached_tokens_details=None,
+                cached_tokens_details=RealtimeModelMetrics.CachedTokenDetails(
+                    text_tokens=usage.get("input_token_details", {}).get("cached_tokens_details", {}).get("text_tokens", 0),
+                    audio_tokens=usage.get("input_token_details", {}).get("cached_tokens_details", {}).get("audio_tokens", 0),
+                    image_tokens=usage.get("input_token_details", {}).get("cached_tokens_details", {}).get("image_tokens", 0),
+                ),
                 image_tokens=0,
             ),
             output_token_details=RealtimeModelMetrics.OutputTokenDetails(
