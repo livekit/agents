5f4860b0ab775656efc59a6b041aeabdea14cc95
Long Chen <longch1024@gmail.com>
2025-06-17 15:28:23 +0800

    fix transcription delay when VAD false negative

--- livekit-agents/livekit/agents/metrics/base.py
+++ livekit-agents/livekit/agents/metrics/base.py
@@ -62,10 +62,14 @@ class EOUMetrics(BaseModel):
     type: Literal["eou_metrics"] = "eou_metrics"
     timestamp: float
     end_of_utterance_delay: float
-    """Amount of time between the end of speech from VAD and the decision to end the user's turn."""
+    """Amount of time between the end of speech from VAD and the decision to end the user's turn.
+    Negative value if the end of speech was not detected.
+    """
 
     transcription_delay: float
-    """Time taken to obtain the transcript after the end of the user's speech."""
+    """Time taken to obtain the transcript after the end of the user's speech.
+    Negative value if the end of speech was not detected.
+    """
 
     on_user_turn_completed_delay: float
     """Time taken to invoke the user's `Agent.on_user_turn_completed` callback."""
