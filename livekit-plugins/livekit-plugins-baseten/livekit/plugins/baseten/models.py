200470e7a242d3c2c620a1a0bc60824164fc3c06
David Zhao <dz@livekit.io>
2025-06-19 23:44:32 -0700

    feat(baseten): add LLM module (#2657)

---
+++ livekit-plugins/livekit-plugins-baseten/livekit/plugins/baseten/models.py
@@ -0,0 +1,8 @@
+from typing import Literal
+
+LLMModels = Literal[
+    "deepseek-ai/DeepSeek-R1",
+    "deepseek-ai/DeepSeek-V3-0324",
+    "meta-llama/Llama-4-Scout-17B-16E-Instruct",
+    "meta-llama/Llama-4-Maverick-17B-128E-Instruct",
+]
