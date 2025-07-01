7e38050452f83c06cab6bf0a184ede60345cf8fc
David Zhao <dz@livekit.io>
2025-07-01 16:00:15 -0700

    feat: ability to use remote EOT inference when deployed in Cloud

--- livekit-plugins/livekit-plugins-turn-detector/livekit/plugins/turn_detector/multilingual.py
+++ livekit-plugins/livekit-plugins-turn-detector/livekit/plugins/turn_detector/multilingual.py
@@ -1,8 +1,16 @@
 from __future__ import annotations
 
+import os
+
+import aiohttp
+
+from livekit.agents import llm, utils
 from livekit.agents.inference_runner import _InferenceRunner
 
 from .base import EOUModelBase, _EUORunnerBase
+from .log import logger
+
+REMOTE_INFERENCE_TIMEOUT = 2
 
 
 class _EUORunnerMultilingual(_EUORunnerBase):
@@ -14,10 +22,64 @@ class _EUORunnerMultilingual(_EUORunnerBase):
 
 class MultilingualModel(EOUModelBase):
     def __init__(self, *, unlikely_threshold: float | None = None):
-        super().__init__(model_type="multilingual", unlikely_threshold=unlikely_threshold)
+        super().__init__(
+            model_type="multilingual",
+            unlikely_threshold=unlikely_threshold,
+            load_languages=remote_inference_url() is None,
+        )
 
     def _inference_method(self) -> str:
         return _EUORunnerMultilingual.INFERENCE_METHOD
 
+    async def unlikely_threshold(self, language: str | None) -> float | None:
+        threshold = await super().unlikely_threshold(language)
+        if threshold is None:
+            if url := remote_inference_url():
+                async with utils.http_context.http_session().post(
+                    url=url,
+                    json={
+                        "language": language,
+                    },
+                    timeout=aiohttp.ClientTimeout(total=REMOTE_INFERENCE_TIMEOUT),
+                ) as resp:
+                    resp.raise_for_status()
+                    data = await resp.json()
+                    threshold = data.get("threshold")
+                    if threshold:
+                        self._languages[language] = {"threshold": data["threshold"]}
 
-_InferenceRunner.register_runner(_EUORunnerMultilingual)
+        return threshold
+
+    async def predict_end_of_turn(
+        self, chat_ctx: llm.ChatContext, *, timeout: float | None = 3
+    ) -> float:
+        url = remote_inference_url()
+        if not url:
+            return super().predict_end_of_turn(chat_ctx, timeout=timeout)
+
+        print(f"requesting eou: {chat_ctx.to_dict(exclude_function_call=True)}")
+        async with utils.http_context.http_session().post(
+            url=url,
+            json=chat_ctx.to_dict(exclude_function_call=True),
+            timeout=aiohttp.ClientTimeout(total=REMOTE_INFERENCE_TIMEOUT),
+        ) as resp:
+            resp.raise_for_status()
+            data = await resp.json()
+            probability = data.get("probability")
+            if probability:
+                logger.debug(
+                    "eou prediction",
+                    extra=data,
+                )
+                return probability
+            else:
+                # default to indicate no prediction
+                return 1
+
+
+def remote_inference_url() -> str | None:
+    return os.getenv("LIVEKIT_EOT_MULTI_URL")
+
+
+if not remote_inference_url():
+    _InferenceRunner.register_runner(_EUORunnerMultilingual)
