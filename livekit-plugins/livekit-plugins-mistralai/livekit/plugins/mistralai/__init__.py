74df57403e3a9385bf9e033fe6f5124b0578702e
Fab <151897294+fabitokki@users.noreply.github.com>
2025-07-02 13:42:55 +0200

    add livekit-plugins-mistralai (#2772)
    Co-authored-by: Théo Monnom <theo.monnom@outlook.com>

---
+++ livekit-plugins/livekit-plugins-mistralai/livekit/plugins/mistralai/__init__.py
@@ -0,0 +1,25 @@
+"""LiveKit plugin for Mistral AI models. Currently only supports ChatModels"""
+
+from livekit.agents import Plugin
+
+from .log import logger
+from .version import __version__
+
+__all__ = ["LLM", "LLMStream", "__version__"]
+
+
+class MistralAIPlugin(Plugin):
+    def __init__(self) -> None:
+        super().__init__(__name__, __version__, __package__, logger)
+
+
+Plugin.register_plugin(MistralAIPlugin())
+
+# Cleanup docs of unexported modules
+_module = dir()
+NOT_IN_ALL = [m for m in _module if m not in __all__]
+
+__pdoc__ = {}
+
+for n in NOT_IN_ALL:
+    __pdoc__[n] = False
