340d3a92be6ae8b95c561affd62592d32eb925af
Théo Monnom <theo.8bits@gmail.com>
2025-07-15 22:27:05 +0200

    Update _datastream_io.py

--- livekit-agents/livekit/agents/voice/avatar/_datastream_io.py
+++ livekit-agents/livekit/agents/voice/avatar/_datastream_io.py
@@ -48,7 +48,10 @@ class DataStreamAudioOutput(AudioOutput):
         self._started = False
         self._lock = asyncio.Lock()
 
-    async def start(self) -> None:
+        self._start_atask: asyncio.Task | None = None
+
+    @utils.log_exceptions(logger=logger)
+    async def _start_task(self) -> None:
         async with self._lock:
             if self._started:
                 return
@@ -83,7 +86,11 @@ class DataStreamAudioOutput(AudioOutput):
     async def capture_frame(self, frame: rtc.AudioFrame) -> None:
         """Capture and stream audio frame to remote worker"""
         if not self._started:
-            await self.start()
+            # TODO(theomonnom): this class should be encapsuled somewhere else to allow for a clean close
+            if self._start_atask is None:
+                self._start_atask = asyncio.create_task(self._start_task())
+
+            return  # ignore the frame
 
         await super().capture_frame(frame)
 
