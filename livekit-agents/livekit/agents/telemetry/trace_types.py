ac5094f102b3ef7cefe47b7515a2c5789eb6621a
ishanextreme <ishanmishra.work@outlook.com>
2025-07-24 20:32:50 +0530

    removed extra changes

--- livekit-agents/livekit/agents/telemetry/trace_types.py
+++ livekit-agents/livekit/agents/telemetry/trace_types.py
@@ -30,7 +30,6 @@ ATTR_FUNCTION_TOOL_OUTPUT = "lk.function_tool.output"
 ATTR_TTS_INPUT_TEXT = "input.value"
 ATTR_TTS_STREAMING = "lk.tts.streaming"
 ATTR_TTS_LABEL = "lk.tts.label"
-ATTR_TTS_BLOB_URL = "lk.tts.blob_url"
 
 # eou detection
 ATTR_EOU_PROBABILITY = "lk.eou.probability"
