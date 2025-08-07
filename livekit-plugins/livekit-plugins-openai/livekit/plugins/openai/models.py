311002fef7e28fc3ee2032a08a1dbdc861a75dcb
Mateusz Kulpa <mateusz.kulpa@outlook.com>
2025-08-07 23:02:33 +0200

    feat: add openai gpt-5 models (#3106)

--- livekit-plugins/livekit-plugins-openai/livekit/plugins/openai/models.py
+++ livekit-plugins/livekit-plugins-openai/livekit/plugins/openai/models.py
@@ -18,6 +18,9 @@ TTSVoices = Literal[
 ]
 DalleModels = Literal["dall-e-2", "dall-e-3"]
 ChatModels = Literal[
+    "gpt-5",
+    "gpt-5-mini",
+    "gpt-5-nano",
     "gpt-4.1",
     "gpt-4.1-mini",
     "gpt-4.1-nano",
