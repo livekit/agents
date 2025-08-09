f51c370008f20eb6040e394ed76dd56fde4f56ab
David Zhao <dz@livekit.io>
2025-08-09 13:38:36 -0700

    feat: cgroups v1 support, lower idle processes (#3117)

--- livekit-plugins/livekit-plugins-turn-detector/livekit/plugins/turn_detector/base.py
+++ livekit-plugins/livekit-plugins-turn-detector/livekit/plugins/turn_detector/base.py
@@ -91,7 +91,7 @@ class _EUORunnerBase(_InferenceRunner):
             )
             sess_options = ort.SessionOptions()
             sess_options.intra_op_num_threads = max(
-                1, math.ceil(hw.get_cpu_monitor().cpu_count()) // 2
+                1, min(math.ceil(hw.get_cpu_monitor().cpu_count()) // 2, 4)
             )
             sess_options.inter_op_num_threads = 1
             sess_options.add_session_config_entry("session.dynamic_block_base", "4")
