6b39e90615e04008b8aaa74f9f01188349c2e561
Long Chen <longch1024@gmail.com>
2025-08-13 15:32:27 +0800

    ignore interruption on session.say

--- livekit-agents/livekit/agents/voice/agent_activity.py
+++ livekit-agents/livekit/agents/voice/agent_activity.py
@@ -1432,7 +1432,9 @@ class AgentActivity(RecognitionHooks):
 
             if speech_handle._interrupted_by_user:
                 self._session._schedule_agent_false_interruption(
-                    AgentFalseInterruptionEvent(extra_instructions=None, message=msg)
+                    AgentFalseInterruptionEvent(
+                        extra_instructions=None, message=msg, speech_source="say"
+                    )
                 )
 
         if self._session.agent_state == "speaking":
@@ -1648,7 +1650,11 @@ class AgentActivity(RecognitionHooks):
 
             if speech_handle._interrupted_by_user:
                 self._session._schedule_agent_false_interruption(
-                    AgentFalseInterruptionEvent(extra_instructions=instructions, message=copy_msg)
+                    AgentFalseInterruptionEvent(
+                        extra_instructions=instructions,
+                        message=copy_msg,
+                        speech_source="generate_reply",
+                    )
                 )
 
             if self._session.agent_state == "speaking":
@@ -2028,7 +2034,11 @@ class AgentActivity(RecognitionHooks):
 
                 if speech_handle._interrupted_by_user:
                     self._session._schedule_agent_false_interruption(
-                        AgentFalseInterruptionEvent(extra_instructions=instructions, message=msg)
+                        AgentFalseInterruptionEvent(
+                            extra_instructions=instructions,
+                            message=msg,
+                            speech_source="generate_reply",
+                        )
                     )
 
             speech_handle._mark_generation_done()
