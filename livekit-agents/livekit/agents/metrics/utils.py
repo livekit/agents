1cbc202e86892e5333b021594adf74f6cd9f9e7a
Chenghao Mou <chenghao.mou@itramei.com>
2025-08-18 13:09:04 +0100

    fix mypy issue

--- livekit-agents/livekit/agents/metrics/utils.py
+++ livekit-agents/livekit/agents/metrics/utils.py
@@ -10,7 +10,7 @@ def log_metrics(metrics: AgentMetrics, *, logger: logging.Logger | None = None) 
     if logger is None:
         logger = default_logger
 
-    metadata: dict[str, str] = {}
+    metadata: dict[str, str | float] = {}
     if metrics.metadata:
         metadata |= {
             "model_name": metrics.metadata.model_name or "unknown",
@@ -20,51 +20,51 @@ def log_metrics(metrics: AgentMetrics, *, logger: logging.Logger | None = None) 
     if isinstance(metrics, LLMMetrics):
         logger.info(
             "LLM metrics",
-            extra={
+            extra=metadata
+            | {
                 "ttft": round(metrics.ttft, 2),
                 "prompt_tokens": metrics.prompt_tokens,
                 "prompt_cached_tokens": metrics.prompt_cached_tokens,
                 "completion_tokens": metrics.completion_tokens,
                 "tokens_per_second": round(metrics.tokens_per_second, 2),
-            }
-            | metadata,
+            },
         )
     elif isinstance(metrics, RealtimeModelMetrics):
         logger.info(
             "RealtimeModel metrics",
-            extra={
+            extra=metadata
+            | {
                 "ttft": round(metrics.ttft, 2),
                 "input_tokens": metrics.input_tokens,
                 "cached_input_tokens": metrics.input_token_details.cached_tokens,
                 "output_tokens": metrics.output_tokens,
                 "total_tokens": metrics.total_tokens,
                 "tokens_per_second": round(metrics.tokens_per_second, 2),
-            }
-            | metadata,
+            },
         )
     elif isinstance(metrics, TTSMetrics):
         logger.info(
             "TTS metrics",
-            extra={
+            extra=metadata
+            | {
                 "ttfb": metrics.ttfb,
                 "audio_duration": round(metrics.audio_duration, 2),
-            }
-            | metadata,
+            },
         )
     elif isinstance(metrics, EOUMetrics):
         logger.info(
             "EOU metrics",
-            extra={
+            extra=metadata
+            | {
                 "end_of_utterance_delay": round(metrics.end_of_utterance_delay, 2),
                 "transcription_delay": round(metrics.transcription_delay, 2),
-            }
-            | metadata,
+            },
         )
     elif isinstance(metrics, STTMetrics):
         logger.info(
             "STT metrics",
-            extra={
+            extra=metadata
+            | {
                 "audio_duration": round(metrics.audio_duration, 2),
-            }
-            | metadata,
+            },
         )
