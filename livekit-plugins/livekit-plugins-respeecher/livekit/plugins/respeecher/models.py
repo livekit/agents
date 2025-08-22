fe3dbb34557b87c09be4aee95e6a483fdc3e527a
Anna Mitrushchienkova <avoknenkova@gmail.com>
2025-08-22 16:15:20 +0300

    Add Respeecher TTS plugin

---
+++ livekit-plugins/livekit-plugins-respeecher/livekit/plugins/respeecher/models.py
@@ -0,0 +1,51 @@
+from typing import Literal, Optional
+from dataclasses import dataclass
+
+TTSModels = Literal[
+    # Respeecher's English model, multilanguage models will be added later
+    "v1/public/tts/en-rt",
+]
+
+TTSEncoding = Literal[
+    "pcm_s16le",
+    "pcm_f32le",
+]
+
+TTSLanguages = Literal[
+    "en",
+]
+
+TTSSampleRates = [
+    8000, 
+    11025, 
+    16000, 
+    22050, 
+    44100,  
+    48000,
+]
+
+@dataclass
+class SamplingParam:
+    """Check https://space.respeecher.com/docs/api/tts/sampling-params-guide for details"""
+    seed: Optional[int] = None
+    temparature: Optional[float] = None
+    top_k: Optional[int] = None
+    top_p: Optional[float] = None
+    min_p: Optional[float] = None
+    presence_penalty: Optional[float] = None
+    repetition_penalty: Optional[float] = None
+    frequency_penalty: Optional[float] = None
+
+@dataclass
+class VoiceSettings:
+    """Voice settings for Respeecher TTS"""
+    sampling_param: Optional[SamplingParam] = None
+    
+@dataclass  
+class Voice:
+    """Voice model for Respeecher"""
+    id: str
+    gender: Optional[str] = None
+    accent: Optional[str] = None
+    age: Optional[str] = None
+    sampling_param: Optional[SamplingParam] = None
