2e69eb4ab1249e5bc17934660212894183dce51b
Chenghao Mou <chenghao.mou@itramei.com>
2025-09-18 12:07:28 +0100

    add speech id to agent events

---
+++ test.py
@@ -0,0 +1,157 @@
+import logging
+import time
+from collections.abc import AsyncIterable
+
+from dotenv import load_dotenv
+from openai.types.beta.realtime.session import InputAudioTranscription
+
+from livekit import rtc
+from livekit.agents import (
+    Agent,
+    AgentSession,
+    JobContext,
+    JobProcess,
+    MetricsCollectedEvent,
+    ModelSettings,
+    RunContext,
+    WorkerOptions,
+    cli,
+    metrics,
+)
+from livekit.agents import AgentStateChangedEvent
+from livekit.agents import ConversationItemAddedEvent
+from livekit.agents import UserStateChangedEvent
+from livekit.agents.llm import function_tool
+from livekit.agents.voice.transcription.filters import filter_markdown
+from livekit.plugins import openai, silero
+
+# uncomment to enable Krisp background voice/noise cancellation
+# from livekit.plugins import noise_cancellation
+
+logger = logging.getLogger("basic-agent")
+
+load_dotenv()
+
+
+class MyAgent(Agent):
+    def __init__(self) -> None:
+        super().__init__(
+            instructions="Your name is Kelly. You would interact with users via voice."
+            "with that in mind keep your responses concise and to the point."
+            "do not use emojis, asterisks, markdown, or other special characters in your responses."
+            "You are curious and friendly, and have a sense of humor.",
+        )
+
+    async def on_enter(self):
+        # when the agent is added to the session, it'll generate a reply
+        # according to its instructions
+        self.session.generate_reply()
+
+    async def tts_node(
+        self, text: AsyncIterable[str], model_settings: ModelSettings
+    ) -> AsyncIterable[rtc.AudioFrame]:
+        # TTS node allows us to process the text before it's sent to the model
+        # here we'll strip out markdown
+        filtered_text = filter_markdown(text)
+        return super().tts_node(filtered_text, model_settings)
+
+    # all functions annotated with @function_tool will be passed to the LLM when this
+    # agent is active
+    @function_tool
+    async def lookup_weather(
+        self, context: RunContext, location: str, latitude: str, longitude: str
+    ):
+        """Called when the user asks for weather related information.
+        Ensure the user's location (city or region) is provided.
+        When given a location, please estimate the latitude and longitude of the location and
+        do not ask the user for them.
+
+        Args:
+            location: The location they are asking for
+            latitude: The latitude of the location, do not ask user for it
+            longitude: The longitude of the location, do not ask user for it
+        """
+
+        logger.info(f"Looking up weather for {location}")
+
+        return "sunny with a temperature of 70 degrees."
+
+
+def prewarm(proc: JobProcess):
+    proc.userdata["vad"] = silero.VAD.load()
+
+
+async def entrypoint(ctx: JobContext):
+    # each log entry will include these fields
+    ctx.log_context_fields = {
+        "room": ctx.room.name,
+    }
+
+    session = AgentSession(
+        vad=ctx.proc.userdata["vad"],
+        # any combination of STT, LLM, TTS, or realtime API can be used
+        # llm=openai.LLM(model="gpt-4o-mini"),
+        # stt=deepgram.STT(model="nova-3", language="multi"),
+        # tts=openai.TTS(voice="ash"),
+        llm=openai.realtime.RealtimeModel(
+            turn_detection=openai.realtime.realtime_model.TurnDetection(type="semantic_vad"),
+            input_audio_transcription=InputAudioTranscription(language="en", model="gpt-4o-mini-transcribe")
+        ),
+        # allow the LLM to generate a response while waiting for the end of turn
+        preemptive_generation=True,
+        # use LiveKit's turn detection model
+        # turn_detection=MultilingualModel(),
+    )
+
+    # log metrics as they are emitted, and total usage after session is over
+    usage_collector = metrics.UsageCollector()
+
+    @session.on("user_state_changed")  # type: ignore[misc]  # pyright: ignore[reportUnknownMemberType]
+    def on_user_state_changed(event: UserStateChangedEvent) -> None:  # pyright: ignore[reportUnusedFunction]
+
+        match (event.old_state, event.new_state):
+            case (_, "speaking"):
+                print(f"User started speaking: {time.time()}")
+            case ("speaking", _):
+                print(f"User stopped speaking: {time.time()}")
+
+    @session.on("agent_state_changed")  # type: ignore[misc]  # pyright: ignore[reportUnknownMemberType]
+    def on_agent_state_changed(event: AgentStateChangedEvent) -> None:  # pyright: ignore[reportUnusedFunction]
+
+        match (event.old_state, event.new_state):
+            case (_, "speaking"):
+                print(f"Agent started speaking: {event.speech_id} {time.time()}")
+            case ("speaking", _):
+                print(f"Agent stopped speaking: {event.speech_id} {time.time()}")
+            case _:
+                pass
+
+
+
+    @session.on("conversation_item_added")  # type: ignore[misc]  # pyright: ignore[reportUnknownMemberType]
+    def on_conversation_item_added(  # pyright: ignore[reportUnusedFunction]
+        event: ConversationItemAddedEvent,
+    ) -> None:
+        print(f"Message received: {event.item} {event.speech_id}")
+
+
+    @session.on("metrics_collected")
+    def _on_metrics_collected(ev: MetricsCollectedEvent):
+        metrics.log_metrics(ev.metrics)
+        usage_collector.collect(ev.metrics)
+
+    async def log_usage():
+        summary = usage_collector.get_summary()
+        logger.info(f"Usage: {summary}")
+
+    # shutdown callbacks are triggered when the session is over
+    ctx.add_shutdown_callback(log_usage)
+
+    await session.start(
+        agent=MyAgent(),
+        room=ctx.room,
+    )
+
+
+if __name__ == "__main__":
+    cli.run_app(WorkerOptions(entrypoint_fnc=entrypoint, prewarm_fnc=prewarm))
