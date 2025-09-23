e53edfd6463e66589b4bfb5f52bc91dc90a36381
Théo Monnom <theo.8bits@gmail.com>
2025-09-23 15:45:34 -0700

    Update job.py

--- livekit-agents/livekit/agents/job.py
+++ livekit-agents/livekit/agents/job.py
@@ -538,11 +538,7 @@ class JobRequest:
         return self._job.agent_name
 
     async def reject(self) -> None:
-        """Reject the job request. The job may be assigned to another worker"""
-        await self._on_reject(False)
-
-    async def terminate(self) -> None:
-        """Terminate the job request. The job will not be assigned to another worker"""
+        """Reject the job request. The job will not be assigned to another worker"""
         await self._on_reject(True)
 
     async def accept(
