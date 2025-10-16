8f3d19b328e643742326857ee862aefe863434be
Toubat <brian.yin@livekit.io>
2025-10-16 15:52:37 +0800

    Update dtmf_inputs.py

--- livekit-agents/livekit/agents/beta/workflows/dtmf_inputs.py
+++ livekit-agents/livekit/agents/beta/workflows/dtmf_inputs.py
@@ -59,7 +59,6 @@ class GetDtmfTask(AgentTask[GetDtmfResult]):
 
         self._curr_dtmf_inputs: list[DtmfEvent] = []
         self._dtmf_reply_running: bool = False
-        self._external_audio_enabled: bool = True
 
         @function_tool
         async def confirm_inputs(inputs: list[DtmfEvent]) -> None:
@@ -185,14 +184,11 @@ class GetDtmfTask(AgentTask[GetDtmfResult]):
 
     async def on_enter(self) -> None:
         ctx = get_job_context()
-        self._external_audio_enabled = self.session.input.audio_enabled
 
         ctx.room.on("sip_dtmf_received", self._on_sip_dtmf_received)
         self.session.on("agent_state_changed", self._on_user_state_changed)
         self.session.on("agent_state_changed", self._on_agent_state_changed)
-
-        handle = self.session.generate_reply()
-        handle.add_done_callback(lambda _: self.session.input.set_audio_enabled(True))
+        self.session.generate_reply()
 
     async def on_exit(self) -> None:
         ctx = get_job_context()
@@ -200,5 +196,4 @@ class GetDtmfTask(AgentTask[GetDtmfResult]):
         ctx.room.off("sip_dtmf_received", self._on_sip_dtmf_received)
         self.session.off("agent_state_changed", self._on_user_state_changed)
         self.session.off("agent_state_changed", self._on_agent_state_changed)
-        self.session.input.set_audio_enabled(self._external_audio_enabled)
         self._generate_dtmf_reply.cancel()
