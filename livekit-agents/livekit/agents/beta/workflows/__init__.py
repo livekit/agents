fa7bdb260af260d7414bab7ee861c3ead1b6b52a
Théo Monnom <theo.monnom@outlook.com>
2025-10-17 15:58:38 -0700

    wip

--- livekit-agents/livekit/agents/beta/workflows/__init__.py
+++ livekit-agents/livekit/agents/beta/workflows/__init__.py
@@ -1,5 +1,12 @@
 from .dtmf_inputs import GetDtmfResult, GetDtmfTask
 from .email_address import GetEmailResult, GetEmailTask
-from .task_orchestrator import Task, TaskOrchestrator
+from .task_group import TaskGroup, TaskGroupResult
 
-__all__ = ["GetEmailTask", "GetEmailResult", "GetDtmfTask", "GetDtmfResult", "TaskOrchestrator", "Task"]
+__all__ = [
+    "GetEmailTask",
+    "GetEmailResult",
+    "GetDtmfTask",
+    "GetDtmfResult",
+    "TaskGroup",
+    "TaskGroupResult",
+]
