5500c209555169f546a1bb2559b7dddbaa4b41ed
Chenghao Mou <mouchenghao@gmail.com>
2025-10-19 00:20:00 +0100

    Improve gcp vertex credential check (#2798)
    Co-authored-by: Chenghao Mou <chenghao.mou@itramei.com>

--- livekit-plugins/livekit-plugins-google/livekit/plugins/google/realtime/realtime_api.py
+++ livekit-plugins/livekit-plugins-google/livekit/plugins/google/realtime/realtime_api.py
@@ -10,6 +10,7 @@ from collections.abc import Iterator
 from dataclasses import dataclass, field
 from typing import Literal
 
+from google.auth._default_async import default_async
 from google.genai import Client as GenAIClient, types
 from google.genai.live import AsyncSession
 from livekit import rtc
@@ -235,6 +236,10 @@ class RealtimeModel(llm.RealtimeModel):
         )
 
         if use_vertexai:
+            if not gcp_project:
+                _, gcp_project = default_async(  # type: ignore
+                    scopes=["https://www.googleapis.com/auth/cloud-platform"]
+                )
             if not gcp_project or not gcp_location:
                 raise ValueError(
                     "Project is required for VertexAI via project kwarg or GOOGLE_CLOUD_PROJECT environment variable"  # noqa: E501
