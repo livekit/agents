e9612f29b78870dda239253db41809048bc6f825
Tina Nguyen <72938484+tinalenguyen@users.noreply.github.com>
2025-10-30 17:56:11 -0400

    update

--- examples/minimal_worker.py
+++ examples/minimal_worker.py
@@ -2,18 +2,20 @@ import logging
 
 from dotenv import load_dotenv
 
-from livekit.agents import JobContext, WorkerOptions, cli
+from livekit.agents import JobContext, AgentServer, cli
 
 logger = logging.getLogger("minimal-worker")
 logger.setLevel(logging.INFO)
 
 load_dotenv()
 
+server = AgentServer()
 
+@server.rtc_session()
 async def entrypoint(ctx: JobContext):
     await ctx.connect()
     logger.info(f"connected to the room {ctx.room.name}")
 
 
 if __name__ == "__main__":
-    cli.run_app(WorkerOptions(entrypoint_fnc=entrypoint))
+    cli.run_app(server)
