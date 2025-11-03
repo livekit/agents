491bc1958744f81012654a427246f657e825b724
Zaheer Abbas <zaheer.abbas@prodigaltech.com>
2025-11-03 10:48:16 +0530

    Add support for using a custom silero vad model file
    - This allows users to use a previous version of the silero vad model file.

--- livekit-plugins/livekit-plugins-silero/livekit/plugins/silero/vad.py
+++ livekit-plugins/livekit-plugins-silero/livekit/plugins/silero/vad.py
@@ -19,6 +19,7 @@ import time
 import weakref
 from concurrent.futures import ThreadPoolExecutor
 from dataclasses import dataclass
+from pathlib import Path
 from typing import Literal
 
 import numpy as np
@@ -66,6 +67,7 @@ class VAD(agents.vad.VAD):
         activation_threshold: float = 0.5,
         sample_rate: Literal[8000, 16000] = 16000,
         force_cpu: bool = True,
+        onnx_file_path: NotGivenOr[Path] = NOT_GIVEN,
         # deprecated
         padding_duration: NotGivenOr[float] = NOT_GIVEN,
     ) -> VAD:
@@ -102,6 +104,7 @@ class VAD(agents.vad.VAD):
             max_buffered_speech (float): Maximum duration of speech to keep in the buffer (in seconds).
             activation_threshold (float): Threshold to consider a frame as speech.
             sample_rate (Literal[8000, 16000]): Sample rate for the inference (only 8KHz and 16KHz are supported).
+            onnx_file_path (Path | None): Path to the ONNX model file. If not provided, the default model will be loaded. This can be helpful if you want to use a previous version of the silero model.
             force_cpu (bool): Force the use of CPU for inference.
             padding_duration (float | None): **Deprecated**. Use `prefix_padding_duration` instead.
 
@@ -120,7 +123,13 @@ class VAD(agents.vad.VAD):
             )
             prefix_padding_duration = padding_duration
 
-        session = onnx_model.new_inference_session(force_cpu)
+        resolved_onnx_path: Path | None
+        if is_given(onnx_file_path):
+            resolved_onnx_path = onnx_file_path
+        else:
+            resolved_onnx_path = None
+
+        session = onnx_model.new_inference_session(force_cpu, resolved_onnx_path)
         opts = _VADOptions(
             min_speech_duration=min_speech_duration,
             min_silence_duration=min_silence_duration,
