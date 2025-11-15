7ef97cf76190a16f6499925ab7c55d06cc152c3c
Dev Bhushan <dev.bhushan@enparadigm.com>
2025-11-16 00:52:06 +0530

    Added Support for gpt-5.1-chat-latest (#3932)

--- livekit-plugins/livekit-plugins-openai/livekit/plugins/openai/models.py
+++ livekit-plugins/livekit-plugins-openai/livekit/plugins/openai/models.py
@@ -20,6 +20,7 @@ TTSVoices = Literal[
 DalleModels = Literal["dall-e-2", "dall-e-3"]
 ChatModels = Literal[
     "gpt-5.1",
+    "gpt-5.1-chat-latest",
     "gpt-5",
     "gpt-5-chat-latest",
     "gpt-5-mini",
