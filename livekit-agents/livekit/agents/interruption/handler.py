11b77306cac2a0c5bc83f92ee1405b5236e7fe0b
chirag <chiragmiglani73@gmail.com>
2025-11-19 01:07:43 +0530

    style: Apply ruff format fixes using uv

--- livekit-agents/livekit/agents/interruption/handler.py
+++ livekit-agents/livekit/agents/interruption/handler.py
@@ -125,7 +125,9 @@ class IntelligentInterruptionHandler:
             return
 
         async with self._lock:
-            normalized_words = {word.lower() for word in words} # <-- Assume input parameter is 'words'
+            normalized_words = {
+                word.lower() for word in words
+            }  # <-- Assume input parameter is 'words'
 
             self._ignored_words = self._ignored_words.union(normalized_words)
             if append:
