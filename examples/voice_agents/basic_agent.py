b134e51a2b4d1a46fc54ed06a0d759bae05074c3
Chenghao Mou <chenghao.mou@livekit.io>
2025-11-20 11:40:02 +0000

    add barge plugin and examples

--- examples/voice_agents/basic_agent.py
+++ examples/voice_agents/basic_agent.py
@@ -16,6 +16,7 @@ from livekit.agents import (
 )
 from livekit.agents.llm import function_tool
 from livekit.plugins import silero
+from livekit.plugins.bargein_detector import BargeinDetector
 from livekit.plugins.turn_detector.multilingual import MultilingualModel
 
 # uncomment to enable Krisp background voice/noise cancellation
@@ -93,6 +94,7 @@ async def entrypoint(ctx: JobContext):
         # See more at https://docs.livekit.io/agents/build/turns
         turn_detection=MultilingualModel(),
         vad=ctx.proc.userdata["vad"],
+        bargein_detector=BargeinDetector(),
         # allow the LLM to generate a response while waiting for the end of turn
         # See more at https://docs.livekit.io/agents/build/audio/#preemptive-generation
         preemptive_generation=True,
@@ -126,6 +128,7 @@ async def entrypoint(ctx: JobContext):
                 # noise_cancellation=noise_cancellation.BVC(),
             ),
         ),
+        record=True,
     )
 
 
