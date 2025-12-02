7c967d0841a1fb8aa31def2ee63a380069e76fec
lukasIO <mail@lukasseiler.de>
2025-12-02 09:31:50 +0100

    Add support for audio frame processor

--- livekit-agents/livekit/agents/voice/room_io/_input.py
+++ livekit-agents/livekit/agents/voice/room_io/_input.py
@@ -29,6 +29,7 @@ class _ParticipantInputStream(Generic[T], ABC):
         room: rtc.Room,
         *,
         track_source: rtc.TrackSource.ValueType | list[rtc.TrackSource.ValueType],
+        processor: rtc.FrameProcessor[T] | None = None,
     ) -> None:
         self._room = room
         self._accepted_sources = (
@@ -48,6 +49,8 @@ class _ParticipantInputStream(Generic[T], ABC):
 
         self._room.on("track_subscribed", self._on_track_available)
         self._room.on("track_unpublished", self._on_track_unavailable)
+
+        self.processor = processor
 
     async def __anext__(self) -> T:
         return await self._data_ch.__anext__()
@@ -122,6 +125,8 @@ class _ParticipantInputStream(Generic[T], ABC):
 
         self._room.off("track_subscribed", self._on_track_available)
         self._data_ch.close()
+        if self.processor:
+            self.processor.close()
 
     @log_exceptions(logger=logger)
     async def _forward_task(
@@ -143,7 +148,10 @@ class _ParticipantInputStream(Generic[T], ABC):
             if not self._attached:
                 # drop frames if the stream is detached
                 continue
-            await self._data_ch.send(cast(T, event.frame))
+            frame = cast(T, event.frame)
+            if self.processor:
+                frame = self.processor.process(frame)
+            await self._data_ch.send(frame)
 
         logger.debug("stream closed", extra=extra)
 
@@ -174,6 +182,12 @@ class _ParticipantInputStream(Generic[T], ABC):
         self._close_stream()
         self._stream = self._create_stream(track)
         self._publication = publication
+        if self.processor:
+            self.processor.set_context(
+                room=self._room,
+                participant_identity=participant.identity,
+                publication_sid=publication.sid,
+            )
         self._forward_atask = asyncio.create_task(
             self._forward_task(self._forward_atask, self._stream, publication, participant)
         )
@@ -206,11 +220,20 @@ class _ParticipantAudioInputStream(_ParticipantInputStream[rtc.AudioFrame], Audi
         *,
         sample_rate: int,
         num_channels: int,
-        noise_cancellation: rtc.NoiseCancellationOptions | None,
+        noise_cancellation: rtc.NoiseCancellationOptions
+        | rtc.FrameProcessor[rtc.AudioFrame]
+        | None,
         pre_connect_audio_handler: PreConnectAudioHandler | None,
     ) -> None:
+        audio_processor: rtc.FrameProcessor[rtc.AudioFrame] | None = None
+        if isinstance(noise_cancellation, rtc.FrameProcessor):
+            audio_processor = noise_cancellation
+
         _ParticipantInputStream.__init__(
-            self, room=room, track_source=rtc.TrackSource.SOURCE_MICROPHONE
+            self,
+            room=room,
+            track_source=rtc.TrackSource.SOURCE_MICROPHONE,
+            processor=audio_processor,
         )
         AudioInput.__init__(self, label="RoomIO")
         self._sample_rate = sample_rate
@@ -251,7 +274,7 @@ class _ParticipantAudioInputStream(_ParticipantInputStream[rtc.AudioFrame], Audi
             try:
                 duration: float = 0
                 frames = await self._pre_connect_audio_handler.wait_for_data(publication.track.sid)
-                for frame in self._resample_frames(frames):
+                for frame in self._resample_frames(self._apply_audio_filter(frames)):
                     if self._attached:
                         await self._data_ch.send(frame)
                         duration += frame.duration
@@ -304,6 +327,13 @@ class _ParticipantAudioInputStream(_ParticipantInputStream[rtc.AudioFrame], Audi
 
         if resampler:
             yield from resampler.flush()
+
+    def _apply_audio_filter(self, frames: Iterable[rtc.AudioFrame]) -> Iterable[rtc.AudioFrame]:
+        for frame in frames:
+            if self.audio_filter is not None:
+                yield self.audio_filter.process(frame)
+            else:
+                yield frame
 
 
 class _ParticipantVideoInputStream(_ParticipantInputStream[rtc.VideoFrame], VideoInput):
