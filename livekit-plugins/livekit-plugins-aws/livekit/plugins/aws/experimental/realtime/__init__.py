834f63844554222c5e3435f1a4504709bc99065c
Jarrett Kachenmeister <kachenjr@gmail.com>
2025-12-09 16:51:34 -0500

    Addressing class vs literals comment, and moving new contants out to a types file

--- livekit-plugins/livekit-plugins-aws/livekit/plugins/aws/experimental/realtime/__init__.py
+++ livekit-plugins/livekit-plugins-aws/livekit/plugins/aws/experimental/realtime/__init__.py
@@ -1,18 +1,9 @@
-from .events import Sonic1Voices, Sonic2Voices
 from .realtime_model import RealtimeModel, RealtimeSession
+from .types import SONIC1_VOICES, SONIC2_VOICES
 
 __all__ = [
     "RealtimeSession",
     "RealtimeModel",
-    "Sonic1Voices",
-    "Sonic2Voices",
+    "SONIC1_VOICES",
+    "SONIC2_VOICES",
 ]
-
-# Cleanup docs of unexported modules
-_module = dir()
-NOT_IN_ALL = [m for m in _module if m not in __all__]
-
-__pdoc__ = {}
-
-for n in NOT_IN_ALL:
-    __pdoc__[n] = False
