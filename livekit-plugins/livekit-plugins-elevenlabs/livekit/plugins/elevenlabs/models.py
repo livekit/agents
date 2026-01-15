037ee1cbd80698cce26d1d649500a9b7b921a2d2
Rafał Leżanko <rafal.lezanko@zowie.ai>
2026-01-15 15:05:59 +0100

    #4481 Added Opus and PCM encoding to ElevenLabs TTS

--- livekit-plugins/livekit-plugins-elevenlabs/livekit/plugins/elevenlabs/models.py
+++ livekit-plugins/livekit-plugins-elevenlabs/livekit/plugins/elevenlabs/models.py
@@ -13,12 +13,25 @@ TTSModels = Literal[
 
 TTSEncoding = Literal[
     "mp3_22050_32",
+    "mp3_24000_48",
     "mp3_44100",
     "mp3_44100_32",
     "mp3_44100_64",
     "mp3_44100_96",
     "mp3_44100_128",
     "mp3_44100_192",
+    "opus_48000_32",
+    "opus_48000_64",
+    "opus_48000_96",
+    "opus_48000_128",
+    "opus_48000_192",
+    "pcm_8000",
+    "pcm_16000",
+    "pcm_22050",
+    "pcm_24000",
+    "pcm_32000",
+    "pcm_44100",
+    "pcm_48000",
 ]
 
 STTRealtimeSampleRates = Literal[
