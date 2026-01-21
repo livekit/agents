652cbbfe7f95c343cf19453ca7c8f800575fc8b8
Ashot <ashot.baghdasaryan@podcastle.ai>
2026-01-21 14:34:07 +0400

    Rebase on main and fix formatting issues

--- livekit-plugins/livekit-plugins-asyncai/livekit/plugins/asyncai/tts.py
+++ livekit-plugins/livekit-plugins-asyncai/livekit/plugins/asyncai/tts.py
@@ -241,7 +241,9 @@ class SynthesizeStream(tts.SynthesizeStream):
 
         sent_tokenizer_stream = self._tts._sentence_tokenizer.stream()
 
-        async def _sentence_stream_task(ws: aiohttp.ClientWebSocketResponse, asyncai_context_id: str) -> None:
+        async def _sentence_stream_task(
+            ws: aiohttp.ClientWebSocketResponse, asyncai_context_id: str
+        ) -> None:
             async for ev in sent_tokenizer_stream:
                 token_pkt = {}
                 token_pkt["transcript"] = ev.token + " "
