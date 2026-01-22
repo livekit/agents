50b243dde6f4047b45851b9555b95d0affe4a8a0
“tinalenguyen” <thetinalenguyen@gmail.com>
2026-01-22 18:28:38 -0500

    dobtask

--- livekit-agents/livekit/agents/beta/workflows/__init__.py
+++ livekit-agents/livekit/agents/beta/workflows/__init__.py
@@ -1,4 +1,5 @@
 from .address import GetAddressResult, GetAddressTask
+from .dob import GetDOBResult, GetDOBTask
 from .dtmf_inputs import GetDtmfResult, GetDtmfTask
 from .email_address import GetEmailResult, GetEmailTask
 from .name import GetNameResult, GetNameTask
@@ -11,6 +12,8 @@ __all__ = [
     "GetAddressTask",
     "GetAddressResult",
     "GetDtmfTask",
+    "GetDOBResult",
+    "GetDOBTask",
     "GetDtmfResult",
     "GetNameTask",
     "GetNameResult",
