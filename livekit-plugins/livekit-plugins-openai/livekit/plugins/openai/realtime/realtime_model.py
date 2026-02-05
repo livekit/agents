73cc5610832e81d74494489dc8e1d0f530df0c99
Denis Malimonov <dacool731@mail.ru>
2026-02-06 03:26:55 +0700

    drop ResponseOutputAudioTranscriptDoneEvent

--- livekit-plugins/livekit-plugins-openai/livekit/plugins/openai/realtime/realtime_model.py
+++ livekit-plugins/livekit-plugins-openai/livekit/plugins/openai/realtime/realtime_model.py
@@ -970,10 +970,6 @@ class RealtimeSession(
                         self._handle_response_audio_transcript_delta(event)
                     elif event["type"] in ("response.output_audio.delta", "response.audio.delta"):
                         self._handle_response_audio_delta(ResponseAudioDeltaEvent.construct(**event))
-                    elif event["type"] in ("response.output_audio_transcript.done", "response.audio_transcript.done"):
-                        self._handle_response_output_audio_transcript_done(
-                            ResponseOutputAudioTranscriptDoneEvent.construct(**event)
-                        )
                     elif event["type"] in ("response.output_audio.done", "response.audio.done"):
                         self._handle_response_audio_done(ResponseAudioDoneEvent.construct(**event))
                     elif event["type"] == "response.output_item.done":
