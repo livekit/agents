ddaa61a68f70647807377067f131084bdd7facf4
Ashot <ashot.baghdasaryan@podcastle.ai>
2026-02-19 14:51:51 +0400

    chore: update Async API URL and default model

--- livekit-plugins/livekit-plugins-asyncai/livekit/plugins/asyncai/models.py
+++ livekit-plugins/livekit-plugins-asyncai/livekit/plugins/asyncai/models.py
@@ -2,6 +2,6 @@ from typing import Literal
 
 TTSEncoding = Literal["pcm_s16le", "pcm_f32le", "pcm_mulaw"]
 
-TTSModels = Literal["asyncflow_multilingual_v1.0", "asyncflow_v2.0"]
-TTSLanguages = Literal["en", "de", "es", "fr", "it"]
+TTSModels = Literal["async_flash_v1.0"]
+TTSLanguages = Literal["en", "de", "es", "fr", "it", "pt", "ar", "ru", "ro", "ja", "he", "hy", "tr", "hi", "zh"]
 TTSDefaultVoiceId = "e0f39dc4-f691-4e78-bba5-5c636692cc04"
