b98e7140837a0bc97de720c89df41fa7a00527d5
fpt-hoangpn2 <hoangpn2@fpt.com>
2026-03-13 06:25:18 +0700

    blaze(py): fix python compatibility, timeout precedence, and tool-mode query flag

--- livekit-plugins/livekit-plugins-blaze/livekit/plugins/blaze/stt.py
+++ livekit-plugins/livekit-plugins-blaze/livekit/plugins/blaze/stt.py
@@ -93,7 +93,7 @@ class STT(stt.STT):
         self._language = language
         self._auth_token = auth_token or self._config.auth_token
         self._sample_rate = sample_rate
-        self._timeout = timeout or self._config.stt_timeout
+        self._timeout = timeout if timeout is not None else self._config.stt_timeout
         self._normalization_rules = normalization_rules
 
         # Build transcribe URL
