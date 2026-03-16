ae634a623ffe8e1d13f91ceee9f1990ad9e50f4e
Tina Nguyen <72938484+tinalenguyen@users.noreply.github.com>
2026-03-16 14:02:00 -0400

    (xai tts): add language parameter (#5122)

--- livekit-plugins/livekit-plugins-xai/livekit/plugins/xai/types.py
+++ livekit-plugins/livekit-plugins-xai/livekit/plugins/xai/types.py
@@ -7,3 +7,27 @@ GrokVoices = Literal[
     "Rex",
     "Sal",
 ]
+
+TTSLanguages = Literal[
+    "auto",
+    "en",
+    "ar-EG",
+    "ar-SA",
+    "ar-AE",
+    "bn",
+    "zh",
+    "fr",
+    "de",
+    "hi",
+    "id",
+    "it",
+    "ja",
+    "ko",
+    "pt-BR",
+    "pt-PT",
+    "ru",
+    "es-MX",
+    "es-ES",
+    "tr",
+    "vi",
+]
