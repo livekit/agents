d1eafc453a3ec16d49009a568684cef0c8132969
Chenghao Mou <chenghao.mou@livekit.io>
2026-03-30 15:17:11 +0100

    add a warning about user speaking state

--- livekit-agents/livekit/agents/voice/audio_recognition.py
+++ livekit-agents/livekit/agents/voice/audio_recognition.py
@@ -483,14 +483,18 @@ class AudioRecognition:
             with trace.use_span(self._ensure_user_turn_span()):
                 self._hooks.on_end_of_speech(None)
 
+            # reset VAD so that incorrect end of turn from STT can be corrected by VAD interruption
+            if self._vad:
+                if self._speaking:
+                    logger.warning(
+                        "stt end of speech received while user is speaking, resetting vad"
+                    )
+                self.update_vad(self._vad)
+
             self._speaking = False
             self._user_turn_committed = True
             if not self._vad or self._last_speaking_time is None:
                 self._last_speaking_time = time.time()
-
-            # reset VAD so that incorrect end of turn from STT can be corrected by VAD interruption
-            if self._vad:
-                self.update_vad(self._vad)
 
             chat_ctx = self._hooks.retrieve_chat_ctx().copy()
             self._run_eou_detection(chat_ctx)
