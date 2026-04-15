7ef0e66254fbbfa7917ecbbdaea35320036b5315
Tina Nguyen <thetinalenguyen@gmail.com>
2026-04-15 16:14:14 -0400

    add space

--- livekit-plugins/livekit-plugins-hedra/livekit/plugins/hedra/avatar.py
+++ livekit-plugins/livekit-plugins-hedra/livekit/plugins/hedra/avatar.py
@@ -10,6 +10,6 @@ class AvatarSession:
 
     def __init__(self, **kwargs: object) -> None:
         raise HedraException(
-            "The Hedra realtime avatar service has been disabled. This plugin no longer functions."
+            "The Hedra realtime avatar service has been disabled. This plugin no longer functions. "
             "Please browse our other avatar integrations instead at https://docs.livekit.io/agents/models/avatar/."
         )
